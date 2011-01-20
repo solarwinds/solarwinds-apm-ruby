ffedf38e21f4daf4d6a8f8ebc36cf04108d9df4a
Spiridon Eliopoulos <seliopou@gmail.com>
2011-01-20 22:33:30 +0000

    Add useful functions for setting up tracing

--- lib/util.rb
+++ lib/util.rb
@@ -4,14 +4,53 @@ module Oboe
   end
 
   def self.always?
-      Oboe::Config[:tracing_mode] == "always"
+    Oboe::Config[:tracing_mode] == "always"
   end
 
   def self.through?
-      Oboe::Config[:tracing_mode] == "through"
+    Oboe::Config[:tracing_mode] == "through"
   end
 
   def self.never?
-      Oboe::Config[:tracing_mode] == "never"
+    Oboe::Config[:tracing_mode] == "never"
+  end
+
+  def self.now?
+    Oboe::Context.isValid and not Oboe.never?
+  end
+
+  module Inst
+    def self.trace_agent_block(agent, opts) 
+      if Oboe.now?
+        Oboe::Context.log(agent, 'entry', opts)
+      end
+
+      begin
+        result = yield
+      ensure
+        if Oboe.now?
+          evt = Oboe::Context.createEvent()
+          evt.addInfo('Agent', agent)
+          evt.addInfo('Label', 'exit')
+          Oboe.reporter.sendReport(evt)
+        end
+      end
+
+      return result
+    end
+
+    def self.log(agent, label, opts)
+      if Oboe.now?
+        evt = Oboe::Context.createEvent
+        evt.addInfo('Agent', agent)
+        evt.addInfo('Label', label)
+
+        opts.each do |k, v|
+          evt.addInfo(k.to_s, v.to_s)
+        end
+
+        Oboe.reporter.sendReport(evt)
+      end
+    end
   end
 end
