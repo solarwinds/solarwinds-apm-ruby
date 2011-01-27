53c641f25effd0fe30c57dde4586b93c43cb0818
Spiridon Eliopoulos <seliopou@gmail.com>
2011-01-27 01:48:42 +0000

    Revert "Fix init problem, less relative require paths"
    This reverts commit 7e30357daa06377ecb664ecef708d6344d63306e.

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 begin
   require 'oboe'
-  require 'oboe_fu/lib/util'
-  require 'oboe_fu/lib/inst/action_controller'
-  require 'oboe_fu/lib/inst/active_record'
-  require 'oboe_fu/lib/inst/xmlrpc'
-  require 'oboe_fu/lib/inst/memcache'
+  require 'util'
+  require 'inst/action_controller'
+  require 'inst/active_record'
+  require 'inst/xmlrpc'
+  require 'inst/memcache'
 rescue Exception => e
 end
