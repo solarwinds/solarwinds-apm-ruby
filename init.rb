359c2071127ba056d124f07031fd3024a3703c82
Spiridon Eliopoulos <seliopou@gmail.com>
2011-01-27 02:07:17 +0000

    Fix import error init.rb, use absolute paths

--- init.rb
+++ init.rb
@@ -1,9 +1,7 @@
 begin
   require 'oboe'
-  require 'util'
-  require 'inst/action_controller'
-  require 'inst/active_record'
-  require 'inst/xmlrpc'
-  require 'inst/memcache'
+  %w{util inst/action_controller inst/active_record inst/xmlrpc inst/memcache}.each do |f|
+    require File.join(File.dirname(__FILE__), 'lib', f)
+  end
 rescue Exception => e
 end
