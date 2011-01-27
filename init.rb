6d7b57b63bff916eb9f8b637d7436d804b69c8e6
Spiridon Eliopoulos <seliopou@gmail.com>
2011-01-27 15:45:53 +0000

    rename util file to avoid name clashes

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 begin
   require 'oboe'
-  %w{util inst/action_controller inst/active_record inst/xmlrpc inst/memcache}.each do |f|
+  %w{oboe_fu_util inst/action_controller inst/active_record inst/xmlrpc inst/memcache}.each do |f|
     require File.join(File.dirname(__FILE__), 'lib', f)
   end
 rescue Exception => e
