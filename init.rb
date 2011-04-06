6294253442cf8d2c2f181e0be97ce272266ecceb
Spiridon Eliopoulos <seliopou@gmail.com>
2011-04-06 21:52:53 +0000

    [init] Load Rack middleware if middleware is supported

--- init.rb
+++ init.rb
@@ -1,8 +1,13 @@
 begin
   require 'oboe'
-  %w{oboe_fu_util inst/action_controller inst/active_record inst/xmlrpc inst/memcache}.each do |f|
+  %w{oboe_fu_util inst/action_controller inst/rack inst/active_record inst/xmlrpc inst/memcache}.each do |f|
     require File.join(File.dirname(__FILE__), 'lib', f)
   end
+  
+  if defined?(Rails.configuration.middleware)
+    Rails.configuration.middleware.use Oboe::Middleware
+  end
 rescue Exception => e
-    $stderr.puts "[oboe_fu] unable to init oboe_fu"
+  $stderr.puts "[oboe_fu] unable to init oboe_fu"
 end
+
