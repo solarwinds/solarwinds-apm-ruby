85be67920de4576e1875db45ecbf490ea9dc7f61
Rassi <jrassi@cs.brown.edu>
2011-07-05 22:56:34 +0000

    [memcached] report remote server hostname, instead of string representation of server object

--- lib/inst/memcached.rb
+++ lib/inst/memcached.rb
@@ -8,7 +8,18 @@ if defined?(Memcached)
         opts = { :KVOp => m }
         if args.length and args[0].class != Array
             opts[:KVKey] = args[0].to_s
-            opts[:RemoteHost] = server_by_key(args[0].to_s)
+            if defined?(Lib) and defined?(Lib.memcached_server_by_key) \
+                    and defined?(@struct) and defined?(is_unix_socket?)
+                server_as_array = Lib.memcached_server_by_key(@struct, args[0].to_s)
+                if server_as_array.is_a?(Array)
+                    server = server_as_array.first
+                    if is_unix_socket?(server)
+                        opts[:RemoteHost] = "localhost"
+                    elsif defined?(server.hostname)
+                        opts[:RemoteHost] = server.hostname
+                    end
+                end
+            end
         end
         Oboe::Inst.trace_agent_block('memcache', opts) do
           send("clean_#{m}", *args) 
