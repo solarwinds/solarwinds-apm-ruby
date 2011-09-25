4abc7fbf261d4f4fcd3fa0132b764a60ddee989b
Spiridon Eliopoulos <seliopou@gmail.com>
2011-09-25 17:40:45 +0000

    Instruemtation helper rewrite, add new block tracer

--- lib/oboe_fu_util.rb
+++ lib/oboe_fu_util.rb
@@ -1,36 +1,64 @@
 module Oboe
   module Inst
     def self.trace_layer_block(layer, opts) 
-      if Oboe.now?
-        Oboe::Context.log(layer, 'entry', opts)
-      end
+      Oboe::Context.log(layer, 'entry', opts)
 
       begin
         result = yield
+      rescue Exception => e
+        Oboe::Inst.log_exception(layer, e)
+        raise
       ensure
-        if Oboe.now?
-          evt = Oboe::Context.createEvent()
-          evt.addInfo('Layer', layer)
-          evt.addInfo('Label', 'exit')
-          Oboe.reporter.sendReport(evt)
-        end
+        Oboe::Inst.log(layer, 'exit')
       end
 
       return result
     end
 
-    def self.log(layer, label, opts)
-      if Oboe.now?
-        evt = Oboe::Context.createEvent
-        evt.addInfo('Layer', layer)
-        evt.addInfo('Label', label)
+    def self.trace_layer_block_ss(layer, method, *args)
+      opts = {}
 
-        opts.each do |k, v|
-          evt.addInfo(k.to_s, v.to_s)
+      Oboe::Context.log(layer, 'entry', opts)
+
+      if Oboe.now?
+        begin
+          result, opts = yield(*args)
+        rescue Exception => e
+          Oboe::Inst.log_exception(layer, e)
+          raise
         end
 
-        Oboe.reporter.sendReport(evt)
+        result
+      else
+        send("clean_#{method}", *args)
       end
+
+    ensure
+      Oboe::Inst.log(layer, 'exit', opts)
+    end
+
+    def self.log(layer, label, opts = {})
+      return unless Oboe.now?
+
+      evt = Oboe::Context.createEvent
+      evt.addInfo('Layer', layer)
+      evt.addInfo('Label', label)
+
+      opts.each do |k, v|
+        evt.addInfo(k.to_s, v.to_s)
+      end if opts and opts.size
+
+      Oboe.reporter.sendReport(evt)
+    end
+
+    def self.log_exception(layer, exn)
+      return unless Oboe.now?
+
+      Oboe::Context.log(layer, 'error', {
+          :ErrorClass => exn.class.name,
+          :Message => exn.message,
+          :ErrorBacktrace => exn.backtrace.join('\r\n')
+      })
     end
   end
 end
