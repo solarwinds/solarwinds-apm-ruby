e2b852f4b65b26a445f9db7fcb514c7bd115f599
Spiridon Eliopoulos <seliopou@gmail.com>
2011-10-04 19:08:29 +0000

    [rework] Use new helpers for entry point instrumentation

--- lib/oboefu/inst/action_controller.rb
+++ lib/oboefu/inst/action_controller.rb
@@ -4,58 +4,20 @@ if defined?(ActionController::Base)
     alias :old_rescue_action :rescue_action
 
     def perform_action(*arguments)
-      Oboe::Context.clear()
+      header = @_request.headers['X-Trace']
+      opts = @_request.path_parameters
 
-      hdr = @_request.headers['X-Trace']
-
-      if hdr and Oboe.passthrough?
-        Oboe::Context.fromString(hdr)
+      result, header = Oboe::Inst.trace_start_layer_block('rails', header, opts) do
+        old_perform_action(*arguments)
       end
 
-      if not (Oboe.start? or Oboe.continue?)
-        return old_perform_action(*arguments)
-      end
-
-      if Oboe.start?
-        entryEvt = Oboe::Context.startTrace()
-      elsif Oboe.continue?
-        entryEvt = Oboe::Context.createEvent()
-      end
-
-      entryEvt.addInfo("Layer", "rails")
-      entryEvt.addInfo("Label", "entry")
-      @_request.path_parameters.each_pair do |k, v|
-        entryEvt.addInfo(k.to_s.capitalize, v.to_s)
-      end
-      Oboe.reporter.sendReport(entryEvt)
-
-      exitEvt = Oboe::Context.createEvent()
-      @_response.headers['X-Trace'] = exitEvt.metadataString()
-
-      begin
-        result = old_perform_action(*arguments)
-      rescue Exception => e
-        Oboe::Inst.log_exception('rails', e)
-        raise
-      end
-    ensure
-      if Oboe::Context.isValid() and exitEvt
-        exitEvt.addEdge(Oboe::Context.get())
-        exitEvt.addInfo("Layer", "rails")
-        exitEvt.addInfo("Label", "exit")
-
-        @_request.path_parameters.each_pair do |k, v|
-          exitEvt.addInfo(k.to_s.capitalize, v.to_s)
-        end
-
-        Oboe::Context.clear()
-      end
+      @_request.headers['X-Trace'] = header if header
+      result
     end
 
     def rescue_action(exn)
       Oboe::Inst.log_exception('rails', exn)
       old_rescue_action(exn)
     end
-
   end
 end
--- lib/oboefu/inst/rack.rb
+++ lib/oboefu/inst/rack.rb
@@ -5,47 +5,15 @@ module Oboe
     end
 
     def call(env)
-      Oboe::Context.clear()
+      header = env['HTTP_X_TRACE']
 
-      hdr = env['HTTP_X_TRACE']
-
-      if hdr and Oboe.passthrough?
-        Oboe::Context.fromString(hdr)
+      result, header = Oboe::Inst.trace_start_layer_block('rack', header) do
+        env['HTTP_X_TRACE'] = Oboe::Context.toString()
+        @app.call(env)
       end
 
-
-      # short circuit-the rest
-      if not (Oboe.start? or Oboe.continue?)
-        return @app.call(env)
-      end
-
-      if Oboe.start?
-        entryEvt = Oboe::Context.startTrace()
-      elsif Oboe.continue?
-        entryEvt = Oboe::Context.createEvent()
-      end
-
-      entryEvt.addInfo('Layer', 'rack')
-      entryEvt.addInfo('Label', 'entry')
-      Oboe.reporter.sendReport(entryEvt)
-      exitEvt = Oboe::Context.createEvent()
-
-      begin
-        env['HTTP_X_TRACE'] = Oboe::Context.toString
-        status, headers, body = @app.call(env)
-
-        Oboe::Context.fromString(headers['X-Trace']) if headers['X-Trace']
-
-        exitEvt.addEdge(Oboe::Context.get())
-        headers['X-Trace'] = exitEvt.metadataString
-
-        return [status, headers, body]
-      ensure
-        exitEvt.addInfo('Layer', 'rack')
-        exitEvt.addInfo('Label', 'exit')
-        Oboe.reporter.sendReport(exitEvt)
-        Oboe::Context.clear()
-      end
+      env['HTTP_X_TRACE'] = header if header
+      result
     end
   end
 end
