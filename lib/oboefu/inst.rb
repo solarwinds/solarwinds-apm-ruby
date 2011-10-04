f89caf13038556215dbdd97e8e48cf444fd62531
Spiridon Eliopoulos <seliopou@gmail.com>
2011-10-04 19:07:46 +0000

    [rework] Add helpers for startring traces

--- lib/oboefu/inst.rb
+++ lib/oboefu/inst.rb
@@ -1,5 +1,35 @@
 module Oboe
   module Inst
+    def self.trace_start_layer_block(layer, header, opts={})
+      Oboe::Context.clear()
+
+      if header and Oboe.passthrogh?
+        Oboe::Context.fromString(header)
+      end
+
+      if not (Oboe.start? or Oboe.continue?)
+        return [yield, header]
+      end
+
+      entryEvent, exitEvent = Oboe::Inst.layer_sentinels(layer)
+
+      opts.each do |k, v|
+        entryEvent.addInfo(k.to_s, v.to_s)
+      end if opts and opts.size
+      Oboe.reporter.sendReport(entryEvent)
+
+      begin
+        result = yield
+        [result, exitEvent.metadataString()]
+      rescue => e
+        Oboe::Inst.log_exception(e)
+      ensure
+        exitEvent.addEdge(Oboe::Context.get())
+        Oboe.reporter.sendReport(exitEvent)
+        Oboe::Context.clear()
+      end
+    end
+
     def self.trace_layer_block(layer, opts) 
       Oboe::Context.log(layer, 'entry', opts)
 
@@ -59,6 +89,26 @@ module Oboe
           :Message => exn.message,
           :ErrorBacktrace => exn.backtrace.join('\r\n')
       })
+    end
+
+    def self.layer_sentinels(layer)
+      if Oboe.start?
+        entryEvent = Oboe::Context.startTrace
+      elsif Oboe.continue?
+        entryEvent = Oboe::Context.createEvent
+      else
+        return [nil, nil]
+      end
+
+      entryEvent.addInfo('Layer', layer)
+      entryEvent.addInfo('Label', 'entry')
+
+      exitEvent = Oboe::Context.createEvent
+      exitEvent.addInfo('Layer', layer)
+      exitEvent.addInfo('Label', 'exit')
+      exitEvent.addEdge(Oboe::Context.get())
+
+      [entryEvent, exitEvent]
     end
   end
 end
