6be4d697eaf48b0391688827ef9cff334014207c
Spiridon Eliopoulos <seliopou@gmail.com>
2011-10-04 15:16:17 +0000

    [rework] organize directory layout and loading

--- lib/oboefu/util.rb
+++ lib/oboefu/util.rb
@@ -1,9 +1,9 @@
 module OboeFu
   def self.require_instrumentation
     require 'oboe'
-    require 'oboe_fu_util'
+    require 'oboefu/inst'
 
-    pattern = File.join(File.dirname(__FILE__), 'lib', 'inst', '*.rb')
+    pattern = File.join(File.dirname(__FILE__), 'inst', '*.rb')
     Dir.glob(pattern) do |f|
       begin
         puts "[oboe_fu/loading] Instrumentation '#{f}'"
