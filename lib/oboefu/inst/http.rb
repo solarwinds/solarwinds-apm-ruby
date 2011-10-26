3f47b7c29f82a2facf69539f407afdf137073562
Spiridon Eliopoulos <seliopou@gmail.com>
2011-10-26 15:58:08 +0000

    [inst/http] New net/http instrumentation

---
+++ lib/oboefu/inst/http.rb
@@ -0,0 +1,34 @@
+require 'net/http'
+
+module Net
+  class HTTP
+    alias clean_request request
+
+    define_method(:request) do |*args|
+      unless started?
+        return send(:clean_request, *args)
+      end
+
+      Oboe::Inst.trace_layer_block_ss('http', self, 'request', *args) do
+        opts = {}
+
+        if args.length and args[0]
+          req = args[0]
+          req['X-Trace'] = Oboe::Context.toString()
+
+          opts['IsService'] = 1
+          opts['RemoteProtocol'] = 'http'
+          opts['RemoteHost'] = addr_port
+          opts['Method'] = req.method
+        end
+
+        resp = self.send(:clean_request, *args)
+
+        xtrace = resp.get_fields('X-Trace')
+        Oboe::Context.fromString(xtrace[0]) if xtrace and xtrace.size
+
+        next [resp, opts]
+      end
+    end
+  end
+end
