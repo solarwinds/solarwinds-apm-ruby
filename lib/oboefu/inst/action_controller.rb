cbac5474a7019951f57fae6aed136e968bfb77aa
Spiridon Eliopoulos <seliopou@gmail.com>
2011-11-25 15:44:31 +0000

    [rails3] fill out methods to be instrumented
    still commented out, hasn't been run yet

--- lib/oboefu/inst/action_controller.rb
+++ lib/oboefu/inst/action_controller.rb
@@ -34,8 +34,26 @@ if defined?(ActionController::Base) and Rails::VERSION::MAJOR == 3
     alias :old_render :render
     alias :old_rescue_action :rescue_action
 
-    def process; end
-    def process_action; end
+    def process(*args)
+      header = request.headers['X-Trace']
+      result, header = Oboe::Inst.trace_start_layer_block('rails', header) do
+        old_process(request, response)
+      end
+
+      response.headers['X-Trace'] = header if header
+      result
+    end
+
+    def process_action(*args)
+      opts = {
+        :Controller => self.controller.name,
+        :Action => self.action_name,
+      }
+
+      Oboe::Inst.log('rails', 'info', opts)
+      old_process_action(*args)
+    end
+
     def render; end
     def rescue_action; end
   end
