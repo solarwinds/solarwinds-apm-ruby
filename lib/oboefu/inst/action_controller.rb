cb770ef5c908907d0121fcecb2c51851e6ae7a6d
Ubuntu <ubuntu@seliopou>
2012-02-20 23:16:51 +0000

    [controler] Change intrumentation strategy for rails3
    On first glance this fixes the issues that both OurStage and weheartit have
    been experiencing. It really seems as though to get this right, we need to do
    NewRelic version detection.
    
    Conflicts:
    
    	lib/oboefu/inst/action_controller.rb

--- lib/oboefu/inst/action_controller.rb
+++ lib/oboefu/inst/action_controller.rb
@@ -1,72 +1,70 @@
-if defined?(ActionController::Base) and Rails::VERSION::MAJOR == 2
-  ActionController::Base.class_eval do
-    alias :old_perform_action :perform_action
-    alias :old_rescue_action :rescue_action
-    alias :old_process :process
+module OboeFu
+  module Inst
+    module Rails3ActionController
+      def process(*args)
+        header = request.headers['X-Trace']
+        result, header = Oboe::Inst.trace_start_layer_block('rails', header) do |exitEvent|
+          response.headers['X-Trace'] = exitEvent.metadataString() if exitEvent
+          super
+        end
 
-    def process(request, response)
-      header = request.headers['X-Trace']
-      result, header = Oboe::Inst.trace_start_layer_block('rails', header) do |exitEvent|
-        response.headers['X-Trace'] = exitEvent.metadataString() if exitEvent
-        old_process(request, response)
+        result
       end
 
-      result
-    end
+      def process_action(*args)
+        opts = {
+          :Controller => self.class.name,
+          :Action => self.action_name,
+        }
 
-    def perform_action(*arguments)
-      opts = {
-          'Controller' => @_request.path_parameters['controller'],
-          'Action' => @_request.path_parameters['action']
-      }
+        Oboe::Inst.log('rails', 'info', opts)
+        super
+      end
 
-      Oboe::Inst.log('rails', 'info', opts)
-      old_perform_action(*arguments)
-    end
-
-    def rescue_action(exn)
-      Oboe::Inst.log_exception('rails', exn)
-      old_rescue_action(exn)
+      def render(*args)
+        Oboe::Inst.trace_layer_block('render', {}) do
+          super
+        end
+      end
     end
   end
 end
 
-if defined?(ActionController::Base) and Rails::VERSION::MAJOR == 3
-  ActionController::Base.class_eval do
-    alias :old_process_action :process_action
-    alias :old_process :process
-    alias :old_render :render
-    alias :old_rescue_with_handler :rescue_with_handler
-
-    def process(*args)
-      header = request.headers['X-Trace']
-      result, header = Oboe::Inst.trace_start_layer_block('rails', header) do |exitEvent|
-        response.headers['X-Trace'] = exitEvent.metadataString() if exitEvent
-        old_process(*args)
-      end
-
-      result
+if defined?(ActionController::Base)
+  if Rails::VERSION::MAJOR == 3
+    class ActionController::Base
+      include OboeFu::Inst::Rails3ActionController
     end
+  elsif Rails::VERSION::MAJOR == 2
+    ActionController::Base.class_eval do
+      alias :old_perform_action :perform_action                                                          
+      alias :old_rescue_action :rescue_action                                                            
+      alias :old_process :process                                                                        
 
-    def process_action(*args)
-      opts = {
-        :Controller => self.class.name,
-        :Action => self.action_name,
-      }
+      def process(request, response)
+        header = request.headers['X-Trace']
+        result, header = Oboe::Inst.trace_start_layer_block('rails', header) do |exitEvent|
+          response.headers['X-Trace'] = exitEvent.metadataString() if exitEvent
+          old_process(request, response)                                                                 
+        end                                                                                              
+      
+        result                                                                                           
+      end                                                                                                
 
-      Oboe::Inst.log('rails', 'info', opts)
-      old_process_action(*args)
-    end
+      def perform_action(*arguments)                                                                     
+        opts = {
+            'Controller' => @_request.path_parameters['controller'],                                     
+            'Action' => @_request.path_parameters['action']                                              
+        }                                                                                                
+        
+        Oboe::Inst.log('rails', 'info', opts)                                                            
+        old_perform_action(*arguments)                                                                   
+      end                                                                                                
 
-    def render(*args)
-      Oboe::Inst.trace_layer_block('render', {}) do
-        old_render(*args)
-      end
-    end
-
-    def rescue_with_handler(exn)
-      Oboe::Inst.log_exception('rails', exn)
-      old_rescue_with_handler(exn)
-    end
+      def rescue_action(exn)
+        Oboe::Inst.log_exception('rails', exn)                                                           
+        old_rescue_action(exn)                                                                           
+      end                                                                                                
+    end                                                                                                  
   end
 end
