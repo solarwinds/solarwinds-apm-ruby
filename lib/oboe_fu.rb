93a5893e4145ce0e5aa773789bb105e5af5d1d8c
Ubuntu <ubuntu@seliopou>
2012-02-20 23:00:20 +0000

    [init] Change oboe_fu loading for rails3 (use Railtie)

--- lib/oboe_fu.rb
+++ lib/oboe_fu.rb
@@ -1,3 +1,13 @@
 require 'oboefu/util'
 
-OboeFu.require_instrumentation
+if defined?(Rails) and Rails::VERSION::MAJOR == "3"
+  module OboeFu
+    class Railtie < Rails::Railtie
+      initializer "oboe_fu.start" do |app|
+        OboeFu.require_instrumentation
+      end
+    end
+  end
+else
+  OboeFu.require_instrumentation
+end
