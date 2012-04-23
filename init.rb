b9405271c0e9ce06ce08f47b65415c31fe83d41b
Spiridon Eliopoulos <seliopou@gmail.com>
2012-04-23 00:23:22 +0000

    [init] Plugin and gem init code should be the same

--- init.rb
+++ init.rb
@@ -2,4 +2,14 @@
 # All rights reserved.
 require 'oboefu/util'
 
-OboeFu.require_instrumentation
+if defined?(Rails) and Rails::VERSION::MAJOR == 3
+  module OboeFu
+    class Railtie < Rails::Railtie
+      initializer "oboe_fu.start" do |app|
+        OboeFu.require_instrumentation
+      end
+    end
+  end
+else
+  OboeFu.require_instrumentation
+end
