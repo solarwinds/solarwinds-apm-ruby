97aa1f9ddd2058ec2225089f03e190697ab29bb8
Spiros Eliopoulos <seliopou@gmail.com>
2012-05-03 15:46:44 -0400

    [loading] move util module into loading module

--- init.rb
+++ init.rb
@@ -1,15 +1,15 @@
 # Copyright (c) 2012 by Tracelytics, Inc.
 # All rights reserved.
-require 'oboefu/util'
+require 'oboefu/loading'
 
 if defined?(Rails) and Rails::VERSION::MAJOR == 3
   module OboeFu
     class Railtie < Rails::Railtie
       initializer "oboe_fu.start" do |app|
-        OboeFu.require_instrumentation
+        OboeFu::Loading.require_instrumentation
       end
     end
   end
 else
-  OboeFu.require_instrumentation
+  OboeFu::Loading.require_instrumentation
 end
--- lib/oboe_fu.rb
+++ lib/oboe_fu.rb
@@ -7,10 +7,10 @@ if defined?(Rails) and Rails::VERSION::MAJOR == 3
   module OboeFu
     class Railtie < Rails::Railtie
       initializer "oboe_fu.start" do |app|
-        OboeFu.require_instrumentation
+        OboeFu::Loading.require_instrumentation
       end
     end
   end
 else
-  OboeFu.require_instrumentation
+  OboeFu::Loading.require_instrumentation
 end
