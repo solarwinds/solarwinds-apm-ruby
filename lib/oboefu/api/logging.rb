73553e254ab9d5420a2793c30f92e7d1fa6d3b59
Spiros Eliopoulos <seliopou@gmail.com>
2012-05-10 17:29:48 -0400

    [api/logging] Correct condition for starting a trace

--- lib/oboefu/api/logging.rb
+++ lib/oboefu/api/logging.rb
@@ -25,7 +25,7 @@ module Oboe
   
         if Oboe::Config.tracing?
           self.log_entry(layer, opts)
-        elsif Oboe::Config.start? and Oboe::Config.sample?
+        elsif Oboe::Config.always? or Oboe::Config.sample?
           self.log_event(layer, 'entry', Oboe::Context.startTrace, opts)
         end
       end
