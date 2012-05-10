ed0543d8f28847ca6240ad50a11b1f6b7493be3d
Spiros Eliopoulos <seliopou@gmail.com>
2012-05-10 17:58:44 -0400

    [util] Add backtraces util function
    Collect a backtrace but trim from the top if it is too large.

--- lib/oboefu/api/util.rb
+++ lib/oboefu/api/util.rb
@@ -4,6 +4,7 @@
 module Oboe
   module API
     module Util
+      BACKTRACE_CUTOFF = 100
 
       # Returns false is the key passed in is reserved for use by the
       # Tracelytics data-proessing pipeline. Some of these keys may not be
@@ -11,6 +12,19 @@ module Oboe
       # (i.e., liboboe).
       def valid_key?(k)
         not ['Label', 'Layer', 'Edge', 'Timestamp', 'Timestamp_u'].include? k.to_s
+      end
+
+      # Returns the current backtrace, ignoring the secified number of frames
+      # at the bottom of the backtrace, and dropping all frames
+      # BACKTRACE_CUTOFF beyond the last valid frame.
+      def backtrace(ignore=1)
+        frames = Kernel.caller
+        frames_len = Kernel.caller.size
+        if frames_len - ignore > BACKTRACE_CUTOFF
+          frames[ignore, BACKTRACE_CUTOFF + ignore].unshift("...")
+        else
+          frames.drop(ignore)
+        end.join("\r\n")
       end
     end
   end
