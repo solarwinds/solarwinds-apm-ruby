b97ab366a3e0b42371327dce1c406e986ea46cab
Spiros Eliopoulos <seliopou@gmail.com>
2012-05-11 12:11:49 -0400

    [api] Load LayerInit API module

--- lib/oboefu/api.rb
+++ lib/oboefu/api.rb
@@ -6,11 +6,13 @@ module Oboe
     def self.extend_with_tracing
       extend Oboe::API::Logging
       extend Oboe::API::Tracing
+      extend Oboe::API::LayerInit
     end
 
     def self.extend_with_noop
       extend Oboe::API::LoggingNoop
       extend Oboe::API::TracingNoop
+      extend Oboe::API::LayerInitNoop
     end
     
     extend Oboe::API::Util
