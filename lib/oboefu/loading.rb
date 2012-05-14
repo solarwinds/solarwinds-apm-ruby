180f232b1ce206f82de8f4e2332c1d2db43855aa
Spiros Eliopoulos <seliopou@gmail.com>
2012-05-14 10:01:46 -0400

    [loading] Remove per-instrumentation file output
    As per customer feedback. Keeping exception logging code, though.

--- lib/oboefu/loading.rb
+++ lib/oboefu/loading.rb
@@ -3,7 +3,9 @@
 
 module OboeFu
   module Loading
+
     def self.require_api
+      puts "[oboe_fu] loading ..."
       require 'oboefu/config'
 
       pattern = File.join(File.dirname(__FILE__), 'api', '*.rb')
@@ -26,7 +28,6 @@ module OboeFu
       pattern = File.join(File.dirname(__FILE__), 'inst', '*.rb')
       Dir.glob(pattern) do |f|
         begin
-          puts "[oboe_fu/loading] Instrumentation '#{f}'"
           require f
         rescue => e
           $stderr.puts "[oboe_fu/loading] Error loading insrumentation file '#{f}' : #{e}"
