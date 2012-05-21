09467bbd0747642e3fced6dde2de159c5b3a21cd
Spiros Eliopoulos <seliopou@gmail.com>
2012-05-21 13:23:01 -0400

    [api/layer] Force a trace when reporting layer init

--- lib/oboefu/api/layerinit.rb
+++ lib/oboefu/api/layerinit.rb
@@ -2,17 +2,31 @@ module Oboe
   module API
     module LayerInit
 
-      # Report that instrumentation for the given layer has been installed, as
-      # well as the version of instrumentation and version of layer.
+      # Internal: Report that instrumentation for the given layer has been
+      # installed, as well as the version of instrumentation and version of
+      # layer.
       #
       def report_init(layer)
-        start_trace(layer, { '__Init' => 1, 'Version' => Oboe::Version::STRING }) do
+        force_trace do
+          start_trace(layer, { '__Init' => 1, 'Version' => Oboe::Version::STRING }) { }
         end
+      end
+
+      def force_trace
+        saved_mode = Oboe::Config[:tracing_mode]
+        Oboe::Config[:tracing_mode] = 'always'
+        result = yield
+        Oboe::Config[:tracing_mode] = saved_mode
+        result
       end
     end
 
     module LayerInitNoop
       def report_init(layer)
+      end
+
+      def force_trace
+        yield
       end
     end
   end
