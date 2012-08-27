b762a616e4cf1fb7047526d00b6e3a8ab85628f5
Peter Giacomo Lombardo <pglombardo@gmail.com>
2012-08-27 11:54:34 -0400

    Fail gem install if base OS packages aren't installed.

--- oboe_ext/extconf.rb
+++ oboe_ext/extconf.rb
@@ -3,17 +3,25 @@
 
 require 'mkmf'
 
-if `uname -a`.match(/Darwin/)
-    $stderr.puts "Warning: native extension disabled on OS X. This will not work."
-    `printf "all:\n\ninstall:\n\nclean:\n\n" > Makefile`
-else
-    $libs = append_library($libs, "oboe")
-    $libs = append_library($libs, "stdc++")
-
-    $CFLAGS << " #{ENV["CFLAGS"]}"
-    $CPPFLAGS << " #{ENV["CPPFLAGS"]}"
-    $LIBS << " #{ENV["LIBS"]}"
-
-    cpp_command('g++') if RUBY_VERSION < '1.9'
-    create_makefile('oboe_ext')
+if RUBY_PLATFORM =~ /darwin/
+  $stderr.puts "Error: native extension disabled on OS X. This will not work."
+  exit 1
 end
+
+unless RUBY_PLATFORM =~ /linux/
+  $stderr.puts "Error: The oboe gem will only run under linux currently."
+  exit 1
+end
+
+exit 1 unless have_library('liboboe')
+
+$libs = append_library($libs, "oboe")
+$libs = append_library($libs, "stdc++")
+
+$CFLAGS << " #{ENV["CFLAGS"]}"
+$CPPFLAGS << " #{ENV["CPPFLAGS"]}"
+$LIBS << " #{ENV["LIBS"]}"
+
+cpp_command('g++') if RUBY_VERSION < '1.9'
+create_makefile('oboe_ext')
+
