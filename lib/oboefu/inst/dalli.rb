3a2f95ba0a665a254c4c65c61e6bd85c0491aa1c
Peter Giacomo Lombardo <pglombardo@gmail.com>
2012-08-31 12:48:23 -0400

    Initial dalli instrumentation implementation.

---
+++ lib/oboefu/inst/dalli.rb
@@ -0,0 +1,47 @@
+# Copyright (c) 2012 by Tracelytics, Inc.
+# All rights reserved.
+
+module Oboe
+  module Inst
+    module Dalli
+      def self.included(cls)
+        cls.class_eval do
+          puts "[oboe_fu/loading] Instrumenting Dalli" if Oboe::Config[:verbose]
+          if ::Dalli::Client.private_method_defined? :perform
+            alias perform_without_oboe perform
+            alias perform perform_with_oboe
+          else puts "[oboe_fu/loading] Couldn't properly instrument Dalli.  Partial traces may occur."
+          end
+        end
+      end
+
+      def perform_with_oboe(op, key, *args)
+        if Oboe::Config.tracing?
+          opts = {}
+          opts[:KVOp] = op
+          opts[:KVKey] = key 
+
+          Oboe::API.trace('dalli', opts || {}) do
+            perform_without_oboe(op, key, *args)
+          end
+        else
+          perform_without_oboe(op, key, *args)
+        end
+      end
+    end
+  end
+end
+
+if defined?(Dalli)
+  if Rails::VERSION::MAJOR == 3
+    Rails.configuration.after_initialize do
+      Dalli::Client.module_eval do
+        include Oboe::Inst::Dalli
+      end
+    end
+  else
+    Dalli::Client.module_eval do
+      include Oboe::Inst::Dalli
+    end
+  end
+end
