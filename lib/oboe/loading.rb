fe18145213e3383ba36000fe6e076a8742065715
Peter Giacomo Lombardo <pglombardo@gmail.com>
2012-09-17 18:47:49 -0400

    Move Rails initlzr to rails part.  Load frameworks with glob.

--- lib/oboe/loading.rb
+++ lib/oboe/loading.rb
@@ -12,36 +12,28 @@ module Oboe
       require 'oboe/api'
 
       begin
-        # Force load the tracelytics user initializer if there is one
-        tr_initializer = "#{Rails.root}/config/initializers/tracelytics.rb"
-        require tr_initializer if File.exists?(tr_initializer)
-      
-        puts "[oboe] loading ..." if Oboe::Config[:verbose]
-
         Oboe::API.extend_with_tracing
       rescue LoadError => e
         Oboe::API.extend_with_noop
       end
-
+      
       require 'oboe/config'
     end
 
-    def self.load_rails_instrumentation
-      pattern = File.join(File.dirname(__FILE__), 'frameworks/rails/inst', '*.rb')
+    def self.load_framework_instrumentation
+      pattern = File.join(File.dirname(__FILE__), 'frameworks/*/', '*.rb')
       Dir.glob(pattern) do |f|
         begin
           require f
         rescue => e
-          $stderr.puts "[oboe/loading] Error loading insrumentation file '#{f}' : #{e}"
+          $stderr.puts "[oboe/loading] Error loading framework file '#{f}' : #{e}"
         end
       end
-    end
-    
-    def self.instrument_rails
-      if defined?(Rails) 
-        Oboe::Loading.load_rails_instrumentation
-      end
-    end
 
+    end
   end
 end
+
+Oboe::Loading.require_api
+Oboe::Loading.load_framework_instrumentation
+
