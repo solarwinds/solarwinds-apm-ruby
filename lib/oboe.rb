602cff08b1ea52b25d626ed58fb86d2b4b44a3ba
Peter Giacomo Lombardo <pglombardo@gmail.com>
2012-09-17 11:09:37 -0400

    Updated initalizer and startup routines.

--- lib/oboe.rb
+++ lib/oboe.rb
@@ -1,16 +1,21 @@
 # Copyright (c) 2012 by Tracelytics, Inc.
 # All rights reserved.
-
-require 'oboe/loading'
-
-if defined?(Rails) and Rails::VERSION::MAJOR == 3
-  module Oboe
-    class Railtie < Rails::Railtie
-      initializer "oboe.start" do |app|
-        Oboe::Loading.require_instrumentation
-      end
+          
+module Oboe
+  class Railtie < Rails::Railtie
+    initializer "oboe.start" do |app|
+      Oboe::Loading.require_api
+      Oboe::Loading.instrument_rails if defined?(Rails)
     end
   end
-else
-  Oboe::Loading.require_instrumentation
+end
+
+begin
+  require 'oboe_metal.so'
+  require 'rbconfig'
+  require 'oboe_metal'
+  require 'oboe/loading'
+
+rescue LoadError
+  puts "Unsupported Tracelytics environment (no libs).  Going No-op."
 end
