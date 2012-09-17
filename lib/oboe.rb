bdce4fc9904fb1e0105b558c1196edfa36df63dd
Peter Giacomo Lombardo <pglombardo@gmail.com>
2012-09-17 18:59:06 -0400

    Move Rails initialization code to rails section.

--- lib/oboe.rb
+++ lib/oboe.rb
@@ -1,19 +1,11 @@
 # Copyright (c) 2012 by Tracelytics, Inc.
 # All rights reserved.
           
-module Oboe
-  class Railtie < Rails::Railtie
-    initializer "oboe.start" do |app|
-      Oboe::Loading.require_api
-      Oboe::Loading.instrument_rails if defined?(Rails)
-    end
-  end
-end
-
 begin
   require 'oboe_metal.so'
   require 'rbconfig'
   require 'oboe_metal'
+  require 'oboe/config'
   require 'oboe/loading'
 
 rescue LoadError
