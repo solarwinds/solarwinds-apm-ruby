02b353dba656b657d1d609b5504dbc9eb97c7871
Peter Giacomo Lombardo <pglombardo@gmail.com>
2012-09-21 18:15:14 -0400

    Fix init function name

--- ext/oboe_metal/noop/noop.c
+++ ext/oboe_metal/noop/noop.c
@@ -1,7 +1,7 @@
 #include <ruby.h>
 
 /* ruby calls this to load the extension */
-void Init_hola(void) {
+void Init_oboe_noop(void) {
   /* assume we haven't yet defined Hola */
   VALUE klass = rb_define_class("OboeNoop", rb_cObject);
 }
