4152aa04b6bec8963ee4b8af00ac51bbe3a06dd6
Peter Giacomo Lombardo <pglombardo@gmail.com>
2012-09-30 17:48:07 -0400

    Remove Inst wrapper for this module.

--- lib/oboe/frameworks/rails/rails.rb
+++ lib/oboe/frameworks/rails/rails.rb
@@ -1,50 +1,54 @@
 
 module Oboe
-  module Inst
-    module Rails
+  module Rails
 
-      module Helpers
-        extend ActiveSupport::Concern
+    module Helpers
+      extend ActiveSupport::Concern
 
-        def rum_header
-          render :file => File.dirname(__FILE__) + '/helpers/rum/rum_header', :formats => [:js]
+      def self.rum_header
+        return unless Oboe::Config.has_key?(:access_key) and Oboe::Config.has_key?(:rum_id)
+        if Oboe::Config.tracing?
+          render :partial => File.dirname(__FILE__) + '/helpers/rum/rum_header'
         end
+      end
 
-        def rum_footer
+      def self.rum_footer
+        return unless Oboe::Config.has_key?(:access_key) and Oboe::Config.has_key?(:rum_id)
+        if Oboe::Config.tracing?
           render :file => File.dirname(__FILE__) + '/helpers/rum/rum_footer', :formats => [:js]
         end
-      end # Helpers
-
-      def self.load_initializer
-        # Force load the tracelytics Rails initializer if there is one
-        tr_initializer = "#{::Rails.root}/config/initializers/tracelytics.rb"
-        require tr_initializer if File.exists?(tr_initializer)
       end
+    end # Helpers
 
-      def self.load_instrumentation
-        pattern = File.join(File.dirname(__FILE__), 'inst', '*.rb')
-        Dir.glob(pattern) do |f|
-          begin
-            require f
-          rescue => e
-            $stderr.puts "[oboe/loading] Error loading rails insrumentation file '#{f}' : #{e}"
-          end
+    def self.load_initializer
+      # Force load the tracelytics Rails initializer if there is one
+      tr_initializer = "#{::Rails.root}/config/initializers/tracelytics.rb"
+      require tr_initializer if File.exists?(tr_initializer)
+    end
+
+    def self.load_instrumentation
+      pattern = File.join(File.dirname(__FILE__), 'inst', '*.rb')
+      Dir.glob(pattern) do |f|
+        begin
+          require f
+        rescue => e
+          $stderr.puts "[oboe/loading] Error loading rails insrumentation file '#{f}' : #{e}"
         end
       end
+    end
 
-      def self.include_helpers
-        # TBD: This would make the helpers available to controllers which is occasionally desired.
-        # ActiveSupport.on_load(:action_controller) do
-        #   include Oboe::Inst::Rails::Helpers
-        # end
-        
-        ActiveSupport.on_load(:action_view) do
-          include Oboe::Inst::Rails::Helpers
-        end
+    def self.include_helpers
+      # TBD: This would make the helpers available to controllers which is occasionally desired.
+      # ActiveSupport.on_load(:action_controller) do
+      #   include Oboe::Rails::Helpers
+      # end
+      
+      ActiveSupport.on_load(:action_view) do
+        include Oboe::Rails::Helpers
       end
+    end
 
-    end # Rails
-  end # Inst
+  end # Rails
 end # Oboe
 
 if defined?(::Rails)
@@ -53,19 +57,19 @@ if defined?(::Rails)
       class Railtie < ::Rails::Railtie
         
         initializer 'oboe.helpers' do
-          Oboe::Inst::Rails.include_helpers        
+          Oboe::Rails.include_helpers        
         end
 
         config.after_initialize do
-          Oboe::Inst::Rails.load_instrumentation
+          Oboe::Rails.load_instrumentation
         end
       end
     end
-    Oboe::Inst::Rails.load_initializer
+    Oboe::Rails.load_initializer
   else
-    Oboe::Inst::Rails.load_initializer
-    Oboe::Inst::Rails.load_instrumentation
-    Oboe::Inst::Rails::Helpers.include_helpers        
+    Oboe::Rails.load_initializer
+    Oboe::Rails.load_instrumentation
+    Oboe::Rails::Helpers.include_helpers        
   end
 end
 
