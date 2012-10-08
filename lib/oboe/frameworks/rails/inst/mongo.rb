c62698f7dcc3827905f6eb191519d916cb262cf0
Peter Giacomo Lombardo <pglombardo@gmail.com>
2012-10-08 16:40:09 -0400

    Better initialization, report flavor and db connection info

--- lib/oboe/frameworks/rails/inst/mongo.rb
+++ lib/oboe/frameworks/rails/inst/mongo.rb
@@ -5,20 +5,6 @@ module Oboe
   module Inst
     module Mongo
       OPERATIONS = [ :find, :update, :insert, :remove, :drop, :index, :group, :distinct, :find_and_modify ]
-    
-      OPERATIONS.reject { |m| not method_defined?(m) }.each do |m|
-        define_method("#{m}_with_oboe") do |*args|
-          opts = { :KVOp => m }
-          opts[:KVKey] = args[0].to_s if args.length and args[0].class == Hash
-
-          Oboe::API.trace('mongo', opts) do
-            send("#{m}_without_oboe", *args)
-          end
-        end
-
-        class_eval "alias #{m}_without_oboe #{m}"
-        class_eval "alias #{m} #{m}_with_oboe"
-      end
     end
   end
 end
@@ -27,6 +13,28 @@ if defined?(::Mongo::Collection)
   module ::Mongo
     class Collection
       include Oboe::Inst::Mongo
+      
+      Oboe::Inst::Mongo::OPERATIONS.reject { |m| not method_defined?(m) }.each do |m|
+        define_method("#{m}_with_oboe") do |*args|
+          report_kvs = {}
+         
+          report_kvs[:Flavor] = 'mongodb'
+
+          report_kvs[:Database] = @db.name
+          report_kvs[:RemoteHost] = @db.connection.host
+          report_kvs[:RemotePort] = @db.connection.port
+          
+          report_kvs[:KVOp] = m 
+          report_kvs[:KVKey] = args[0].to_json if args.length and args[0].class == Hash
+
+          Oboe::API.trace('mongo', report_kvs) do
+            send("#{m}_without_oboe", *args)
+          end
+        end
+
+        class_eval "alias #{m}_without_oboe #{m}"
+        class_eval "alias #{m} #{m}_with_oboe"
+      end
     end
   end
   puts "[oboe/loading] Instrumenting mongo" if Oboe::Config[:verbose]
