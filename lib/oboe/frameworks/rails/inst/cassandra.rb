e8c1f3e49efc917f95f538af29157cbace1b1f18
Peter Giacomo Lombardo <pglombardo@gmail.com>
2012-11-01 14:35:26 -0400

    Also trace 5 more operations: :create_index, :drop_index, :add_column_family, :drop_column_family, :add_keyspace, :drop_keyspace

--- lib/oboe/frameworks/rails/inst/cassandra.rb
+++ lib/oboe/frameworks/rails/inst/cassandra.rb
@@ -184,6 +184,86 @@ module Oboe
           send :get_indexed_slices_without_oboe, *args
         end
       end
+
+      def create_index_with_oboe(keyspace, column_family, column_name, validation_class)
+        if Oboe::Config.tracing?
+          report_kvs = extract_trace_details(:create_index, column_family, nil, nil)
+          report_kvs[:keyspace] = keyspace.to_s
+          report_kvs[:column_name] = column_name.to_s
+          report_kvs[:validation_class] = validation_class.to_s
+
+          Oboe::API.trace('cassandra', report_kvs) do
+            create_index_without_oboe(keyspace, column_family, column_name, validation_class)
+          end
+        else
+          create_index_without_oboe(keyspace, column_family, column_name, validation_class)
+        end
+      end
+
+      def drop_index_with_oboe(keyspace, column_family, column_name)
+        if Oboe::Config.tracing?
+          report_kvs = extract_trace_details(:drop_index, column_family, nil, nil)
+          report_kvs[:keyspace] = keyspace.to_s
+          report_kvs[:column_name] = column_name.to_s
+
+          Oboe::API.trace('cassandra', report_kvs) do
+            drop_index_without_oboe(keyspace, column_family, column_name)
+          end
+        else
+          drop_index_without_oboe(keyspace, column_family, column_name)
+        end
+      end
+
+      def add_column_family_with_oboe(cf_def)
+        if Oboe::Config.tracing?
+          report_kvs = extract_trace_details(:add_column_family, nil, nil, nil)
+          report_kvs[:name] = cf_def[:name] if cf_def.is_a?(Hash) and cf_def.has_key?(:name)
+
+          Oboe::API.trace('cassandra', report_kvs) do
+            add_column_family_without_oboe(cf_def)
+          end
+        else
+          add_column_family_without_oboe(cf_def)
+        end
+      end
+      
+      def drop_column_family_with_oboe(column_family)
+        if Oboe::Config.tracing?
+          report_kvs = extract_trace_details(:drop_column_family, column_family, nil, nil)
+
+          Oboe::API.trace('cassandra', report_kvs) do
+            drop_column_family_without_oboe(column_family)
+          end
+        else
+          drop_column_family_without_oboe(column_family)
+        end
+      end
+      
+      def add_keyspace_with_oboe(ks_def)
+        if Oboe::Config.tracing?
+          report_kvs = extract_trace_details(:add_keyspace, nil, nil, nil)
+          report_kvs[:name] = ks_def[:name] if ks_def.is_a?(Hash) and ks_def.has_key?(:name)
+
+          Oboe::API.trace('cassandra', report_kvs) do
+            add_keyspace_without_oboe(ks_def)
+          end
+        else
+          add_keyspace_without_oboe(ks_def)
+        end
+      end
+      
+      def drop_keyspace_with_oboe(keyspace)
+        if Oboe::Config.tracing?
+          report_kvs = extract_trace_details(:drop_keyspace, nil, nil, nil)
+          report_kvs[:name] = keyspace.to_s
+
+          Oboe::API.trace('cassandra', report_kvs) do
+            drop_keyspace_without_oboe(keyspace)
+          end
+        else
+          drop_keyspace_without_oboe(keyspace)
+        end
+      end
     end
   end
 end
@@ -194,11 +274,13 @@ if defined?(::Cassandra)
     include Oboe::Inst::Cassandra
 
     [ :insert, :remove, :count_columns, :get_columns, :multi_get_columns, :get, 
-      :multi_get, :get_range_single, :get_range_batch, :get_indexed_slices].each do |m|
+      :multi_get, :get_range_single, :get_range_batch, :get_indexed_slices,
+      :create_index, :drop_index, :add_column_family, :drop_column_family,
+      :add_keyspace, :drop_keyspace].each do |m|
       if method_defined?(m)
         class_eval "alias #{m}_without_oboe #{m}"
         class_eval "alias #{m} #{m}_with_oboe"
-      else puts "[oboe/loading] Couldn't properly instrument Cassandra.  Partial traces may occur."
+      else puts "[oboe/loading] Couldn't properly instrument Cassandra (#{m}).  Partial traces may occur."
       end
     end
 
@@ -206,7 +288,7 @@ if defined?(::Cassandra)
     if method_defined?(:exists?)
       alias exists_without_oboe? exists?
       alias exists? exists_with_oboe?
-    else puts "[oboe/loading] Couldn't properly instrument Cassandra.  Partial traces may occur."
+    else puts "[oboe/loading] Couldn't properly instrument Cassandra (exists?).  Partial traces may occur."
     end
   end # class Cassandra
 end
