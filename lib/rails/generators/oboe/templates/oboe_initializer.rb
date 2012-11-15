446bfeba741f0d1f752a0f3f57b20efd95b7de84
Peter Giacomo Lombardo <pglombardo@gmail.com>
2012-11-15 09:14:06 +0100

    Replace UTF8 quotes with single quotes

--- lib/rails/generators/oboe/templates/oboe_initializer.rb
+++ lib/rails/generators/oboe/templates/oboe_initializer.rb
@@ -1,9 +1,9 @@
 if defined?(::Oboe::Config) 
-  # When traces should be initiated for incoming requests. Valid options are “always,” 
-  # “through” (when the request is initiated with a tracing header from upstream) and “never”. 
-  # You must set this directive to “always” in order to initiate tracing when there
+  # When traces should be initiated for incoming requests. Valid options are 'always', 
+  # 'through' (when the request is initiated with a tracing header from upstream) and 'never'. 
+  # You must set this directive to 'always' in order to initiate tracing when there
   # is no front-end webserver initiating traces.
-  Oboe::Config[:tracing_mode] = "<%= @tracing_mode %>"
+  Oboe::Config[:tracing_mode] = '<%= @tracing_mode %>'
 <% if ['through', 'never'].include?(@tracing_mode) %>
   # sample_rate is a value from 0 - 1m indicating the fraction of requests per million to trace
   # Oboe::Config[:sample_rate] = <%= @sampling_rate %>
