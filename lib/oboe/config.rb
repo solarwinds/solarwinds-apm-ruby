45a0eda3519884353294b489ec7c40f962b98224
Peter Giacomo Lombardo <pglombardo@gmail.com>
2013-02-12 15:00:07 +0100

    Rebuilt Oboe::Config to support complex configurations

--- lib/oboe/config.rb
+++ lib/oboe/config.rb
@@ -2,38 +2,87 @@
 # All rights reserved.
 
 module Oboe
-  # The following is done for compatability with older versions of oboe and
-  # oboe_fu (0.2.x)
-  if not defined?(Oboe::Config)
-    Config = {
-        :tracing_mode => "through",
-        :reporter_host => "127.0.0.1",
-        :sample_rate => 3e5
-    }
+  
+  def self.always?
+    Oboe::Config[:tracing_mode].to_s == "always"
+  end
+  
+  def self.continue?
+    Oboe::Context.isValid and not Oboe.never?
+  end
+  
+  def self.log(layer, label, options = {})
+    Context.log(layer, label, options = options)
   end
 
-  class << Config 
-    def always?
-      self[:tracing_mode].to_s == "always"
-    end
+  def self.never?
+    Oboe::Config[:tracing_mode].to_s == "never"
+  end
+
+  def self.now?
+    Oboe::Context.isValid and not Oboe.never?
+  end
   
-    def never?
-      self[:tracing_mode].to_s == "never"
-    end
+  def self.passthrough?
+    ["always", "through"].include?(Oboe::Config[:tracing_mode])
+  end
+    
+  def self.sample?
+    # Note that this the only point in the code that currently does and
+    # should ever read the sample rate. When autopilot is released, modify
+    # the line below and that line only.
+    Oboe::Config[:sample_rate].to_i < rand(1e6)
+  end
+
+  def self.start?
+    not Oboe::Context.isValid and Oboe.always?
+  end
   
-    def tracing?
-      Oboe::Context.isValid and not never?
-    end
-  
-    def start?
-      not Oboe::Context.isValid and always?
+  def self.through?
+    Oboe::Config[:tracing_mode] == "through"
+  end
+    
+  def self.tracing?
+    Oboe::Context.isValid and not Oboe.never?
+  end
+
+  ############################
+  # Oboe Configuration Module
+  ############################
+  module Config
+    extend self
+
+    @config = {
+      :tracing_mode => "through",
+      :reporter_host => "127.0.0.1",
+      :sample_rate => 1000000
+    }
+
+    def initialize(data={})
+      @config = {}
+      update!(data)
     end
 
-    def sample?
-      # Note that this the only point in the code that currently does and
-      # should ever read the sample rate. When autopilot is released, modify
-      # the line below and that line only.
-      self[:sample_rate].to_i < rand(1e6)
+    def update!(data)
+      data.each do |key, value|
+        self[key] = value
+      end
+    end
+
+    def [](key)
+      @config[key.to_sym]
+    end
+
+    def []=(key, value)
+      @config[key.to_sym] = value
+    end
+
+    def method_missing(sym, *args)
+      if sym.to_s =~ /(.+)=$/
+        self[$1] = args.first
+      else
+        self[sym]
+      end
     end
   end
 end
