98ae473aabc65588f0664609dc4e4b9878c50c02
Peter Giacomo Lombardo <pglombardo@gmail.com>
2013-02-21 23:20:07 +0100

    Add comment documentation of the new Config options to the initializer template.

--- lib/rails/generators/oboe/templates/oboe_initializer.rb
+++ lib/rails/generators/oboe/templates/oboe_initializer.rb
@@ -13,4 +13,36 @@ if defined?(::Oboe::Config)
 <% end %>
   # Verbose output of instrumentation initialization
   # Oboe::Config[:verbose] = <%= @verbose %>
+
+  #
+  # Resque Options
+  #
+  # :link_workers - associates Resque enqueue operations with the jobs they queue by piggybacking
+  #                 an additional argument on the Redis queue that is stripped prior to job 
+  #                 processing 
+  #                 !!! Note: Make sure both the enqueue side and the Resque workers are instrumented
+  #                 before enabling this or jobs will fail !!!
+  #                 (Default: false)
+  # Oboe::Config[:resque][:link_workers] = false
+  #
+  # Set to true to disable Resque argument logging (Default: false)
+  # Oboe::Config[:resque][:log_args] = false
+ 
+  #
+  # Enabling/Disabling Instrumentation
+  #
+  # If you're having trouble with one of the instrumentation libraries, they
+  # can be individually disabled here by setting the :enabled
+  # value to false:
+  #
+  # Oboe::Config[:action_controller][:enabled] = true
+  # Oboe::Config[:active_record][:enabled] = true
+  # Oboe::Config[:action_view][:enabled] = true
+  # Oboe::Config[:cassandra][:enabled] = true
+  # Oboe::Config[:dalli][:enabled] = true
+  # Oboe::Config[:memcache][:enabled] = true
+  # Oboe::Config[:memcached][:enabled] = true
+  # Oboe::Config[:mongo][:enabled] = true
+  # Oboe::Config[:moped][:enabled] = true
+  # Oboe::Config[:resque][:enabled] = true
 end
