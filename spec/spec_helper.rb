c0ad4063501e81469770f235a3e61fd5136303df
Peter Giacomo Lombardo <pglombardo@gmail.com>
2013-02-27 17:06:34 +0100

    Setup the test environment gems with Bundler

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,12 @@
-require 'rspec'
-require 'oboe'
+require 'rubygems'
+require 'bundler'
+
+Bundler.require(:default, :test)
 
 RSpec.configure do |config|
   config.color_enabled = true
   config.formatter     = 'documentation'
 end
+
+Oboe::Ruby.initialize
 
