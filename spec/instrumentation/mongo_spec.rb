0ac37f105ee726eeb878b0c195f089c5d7b242a2
Peter Giacomo Lombardo <pglombardo@gmail.com>
2013-02-27 18:13:48 +0100

    Initial Mongo rspec tests.

---
+++ spec/instrumentation/mongo_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe Oboe::Inst::Mongo do
+  it 'Stock Mongo should be loaded, defined and ready' do
+    defined?(::Mongo).should_not == nil 
+    defined?(::Mongo::DB).should_not == nil
+    defined?(::Mongo::Cursor).should_not == nil
+    defined?(::Mongo::Collection).should_not == nil
+  end
+
+  it 'Mongo should have oboe methods defined' do
+    Oboe::Inst::Mongo::DB_OPS.each do |m|
+      ::Mongo::DB.method_defined?(m).should == true
+    end
+    Oboe::Inst::Mongo::CURSOR_OPS.each do |m|
+      ::Mongo::Cursor.method_defined?(m).should == true
+    end
+    Oboe::Inst::Mongo::COLL_WRITE_OPS.each do |m|
+      ::Mongo::Collection.method_defined?(m).should == true
+    end
+    Oboe::Inst::Mongo::COLL_QUERY_OPS.each do |m|
+      ::Mongo::Collection.method_defined?(m).should == true
+    end
+    Oboe::Inst::Mongo::COLL_INDEX_OPS.each do |m|
+      ::Mongo::Collection.method_defined?(m).should == true
+    end
+    ::Mongo::Collection.method_defined?(:oboe_collect).should == true
+  end
+end
