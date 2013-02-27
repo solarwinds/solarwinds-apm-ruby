3efb21e913a589df593e739097577bc82231bcef
Peter Giacomo Lombardo <pglombardo@gmail.com>
2013-02-27 18:30:28 +0100

    Fix the tested method names

--- spec/instrumentation/mongo_spec.rb
+++ spec/instrumentation/mongo_spec.rb
@@ -10,19 +10,19 @@ describe Oboe::Inst::Mongo do
 
   it 'Mongo should have oboe methods defined' do
     Oboe::Inst::Mongo::DB_OPS.each do |m|
-      ::Mongo::DB.method_defined?(m).should == true
+      ::Mongo::DB.method_defined?("#{m}_with_oboe").should == true
     end
     Oboe::Inst::Mongo::CURSOR_OPS.each do |m|
-      ::Mongo::Cursor.method_defined?(m).should == true
+      ::Mongo::Cursor.method_defined?("#{m}_with_oboe").should == true
     end
     Oboe::Inst::Mongo::COLL_WRITE_OPS.each do |m|
-      ::Mongo::Collection.method_defined?(m).should == true
+      ::Mongo::Collection.method_defined?("#{m}_with_oboe").should == true
     end
     Oboe::Inst::Mongo::COLL_QUERY_OPS.each do |m|
-      ::Mongo::Collection.method_defined?(m).should == true
+      ::Mongo::Collection.method_defined?("#{m}_with_oboe").should == true
     end
     Oboe::Inst::Mongo::COLL_INDEX_OPS.each do |m|
-      ::Mongo::Collection.method_defined?(m).should == true
+      ::Mongo::Collection.method_defined?("#{m}_with_oboe").should == true
     end
     ::Mongo::Collection.method_defined?(:oboe_collect).should == true
   end
