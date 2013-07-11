6df12e53f4c10e8a9751c42772181989e349fed9
Peter Giacomo Lombardo <pglombardo@gmail.com>
2013-07-11 17:14:21 +0200

    Don't insert X-Trace header for blacklisted Net::HTTP requests

--- lib/oboe/inst/http.rb
+++ lib/oboe/inst/http.rb
@@ -5,9 +5,12 @@ require 'net/http'
 
 Net::HTTP.class_eval do
   def request_with_oboe(*args, &block)
-    if not started? or Oboe::API.blacklisted?(addr_port)
+    unless started? 
       return request_without_oboe(*args, &block) 
     end
+
+    # Avoid cross host tracing for blacklisted domains
+    blacklisted = Oboe::API.blacklisted?(addr_port)
 
     Oboe::API.trace('net-http') do
       opts = {}
@@ -19,16 +22,19 @@ Net::HTTP.class_eval do
         opts['RemoteHost'] = addr_port
         opts['ServiceArg'] = req.path
         opts['Method'] = req.method
+        opts['Blacklisted'] = true if blacklisted
       
         Oboe::API.log('net-http', 'info', opts)
-        req['X-Trace'] = Oboe::Context.toString()
+        req['X-Trace'] = Oboe::Context.toString() unless blacklisted
       end
 
       resp = request_without_oboe(*args, &block)
 
-      xtrace = resp.get_fields('X-Trace')
-      if xtrace and xtrace.size and Oboe.tracing?
-        Oboe::Context.fromString(xtrace[0])
+      unless blacklisted
+        xtrace = resp.get_fields('X-Trace')
+        if xtrace and xtrace.size and Oboe.tracing? 
+          Oboe::Context.fromString(xtrace[0])
+        end
       end
       next resp
     end
