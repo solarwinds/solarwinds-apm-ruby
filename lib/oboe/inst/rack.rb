0711cd516c4132d789a4554a382903b7a3a5c1bc
Peter Giacomo Lombardo <pglombardo@gmail.com>
2013-08-19 18:24:37 +0200

    Updated KVs and retrieval method

--- lib/oboe/inst/rack.rb
+++ lib/oboe/inst/rack.rb
@@ -15,16 +15,22 @@ module Oboe
 
       begin
         xtrace = env['HTTP_X_TRACE'] if env.is_a?(Hash)
-
+        
         req = ::Rack::Request.new(env)
-        report_kvs[:SampleRate]      = Oboe::Config[:sample_rate]
-        report_kvs['HTTP-Host']      = req.host
-        report_kvs['HTTP-Port']      = req.port
-        report_kvs['Query-String']   = req.query_string unless req.query_string.blank?
-        report_kvs[:URL]             = req.path
-        report_kvs[:Method]          = req.request_method
-        report_kvs['AJAX']           = true if req.xhr?
-        report_kvs['X-TV-Meta']      = req.headers['X-TV-Meta'] if req.headers.has_key?('X-TV-Meta')
+        report_kvs[:SampleRate]        = Oboe::Config[:sample_rate]
+        report_kvs['HTTP-Host']        = req.host
+        report_kvs['Port']             = req.port
+        report_kvs['Query-String']     = req.query_string unless req.query_string.blank?
+        report_kvs[:URL]               = req.path
+        report_kvs[:Method]            = req.request_method
+        report_kvs['AJAX']             = true if req.xhr?
+         
+        report_kvs['TV-Meta']          = env['X-TV-Meta']          if env.has_key?('X-TV-Meta')
+        report_kvs['ClientIP']         = env['REMOTE_ADDR']        if env.has_key?('REMOTE_ADDR')
+        report_kvs['Forwarded-For']    = env['X-Forwarded-For']    if env.has_key?('X-Forwarded-For')
+        report_kvs['Forwarded-Host']   = env['X-Forwarded-Host']   if env.has_key?('X-Forwarded-Host')
+        report_kvs['Forwarded-Proto']  = env['X-Forwarded-Proto']  if env.has_key?('X-Forwarded-Proto')
+        report_kvs['Forwarded-Port']   = env['X-Forwarded-Port']   if env.has_key?('X-Forwarded-Port')
       rescue
         # Discard any potential exceptions. Report whatever we can.
       end
