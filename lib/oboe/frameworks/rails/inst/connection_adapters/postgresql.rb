9baa4c76e370c3e45826f28e8048bc47fd217612
Peter Giacomo Lombardo <pglombardo@gmail.com>
2013-08-31 16:33:55 +0200

    Simplified postgresql adapter tracing.

--- lib/oboe/frameworks/rails/inst/connection_adapters/postgresql.rb
+++ lib/oboe/frameworks/rails/inst/connection_adapters/postgresql.rb
@@ -4,54 +4,45 @@
 module Oboe
   module Inst
     module ConnectionAdapters
-
-      module PostgreSQLAdapter
-        include Oboe::Inst::ConnectionAdapters::Utils
-      
-        def self.included(cls)
-          cls.class_eval do
-            if ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::method_defined? :exec_query
-              alias exec_query_without_oboe exec_query
-              alias exec_query exec_query_with_oboe
-            else Oboe.logger.warn "[oboe/loading] Couldn't properly instrument activerecord layer.  Partial traces may occur."
-            end
-              
-            if ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::method_defined? :exec_delete
-              alias exec_delete_without_oboe exec_delete
-              alias exec_delete exec_delete_with_oboe
-            else Oboe.logger.warn "[oboe/loading] Couldn't properly instrument activerecord layer.  Partial traces may occur."
-            end
-          end
-        end
-      end # PostgreSQLAdapter
-
-      module LegacyPostgreSQLAdapter
-        include Oboe::Inst::ConnectionAdapters::Utils
-      
-        def self.included(cls)
-          cls.class_eval do
-            if ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::method_defined? :execute
-              alias execute_without_oboe execute
-              alias execute execute_with_oboe
-            else Oboe.logger.warn "[oboe/loading] Couldn't properly instrument activerecord layer.  Partial traces may occur."
-            end
-          end
-        end
-      end # LegacyPostgreSQLAdapter
-
       module FlavorInitializers
         def self.postgresql
+
           Oboe.logger.info "[oboe/loading] Instrumenting activerecord postgresqladapter" if Oboe::Config[:verbose]
+
           ::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.module_eval do
+            include Oboe::Inst::ConnectionAdapters::Utils
+
             if (::Rails::VERSION::MAJOR == 3 and ::Rails::VERSION::MINOR > 0) or ::Rails::VERSION::MAJOR == 4
-              include Oboe::Inst::ConnectionAdapters::PostgreSQLAdapter
+
+              # ActiveRecord 3.1 and up
+              if method_defined?(:exec_query)
+                alias_method :exec_query_without_oboe, :exec_query
+                alias_method :exec_query, :exec_query_with_oboe
+              else
+                Oboe.logger.debug "[oboe/loading] Couldn't instrument PostgreSQLAdapter:exec_query"
+              end
+                
+              if method_defined?(:exec_delete)
+                alias_method :exec_delete_without_oboe, :exec_delete
+                alias_method :exec_delete, :exec_delete_with_oboe
+              else
+                Oboe.logger.debug "[oboe/loading] Couldn't instrument PostgreSQLAdapter:exec_delete"
+              end
+
             else
-              include Oboe::Inst::ConnectionAdapters::LegacyPostgreSQLAdapter
+
+              # ActiveRecord 3.0 and prior
+              if method_defined?(:execute)
+                alias_method :execute_without_oboe, :execute
+                alias_method :execute, :execute_with_oboe
+              else
+                Oboe.logger.debug "[oboe/loading] Couldn't instrument PostgreSQLAdapter:execute"
+              end
+
             end
           end if defined?(::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
         end
       end
-
     end
   end
 end
