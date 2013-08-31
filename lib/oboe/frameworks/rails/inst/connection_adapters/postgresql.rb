726a3dde02e78e3a09e5d7d03d9ee1a1fbce380f
Peter Giacomo Lombardo <pglombardo@gmail.com>
2013-08-31 17:23:57 +0200

    Further simplify postgres adapter with centralized utility methods

--- lib/oboe/frameworks/rails/inst/connection_adapters/postgresql.rb
+++ lib/oboe/frameworks/rails/inst/connection_adapters/postgresql.rb
@@ -9,38 +9,20 @@ module Oboe
 
           Oboe.logger.info "[oboe/loading] Instrumenting activerecord postgresqladapter" if Oboe::Config[:verbose]
 
-          ::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.module_eval do
-            include Oboe::Inst::ConnectionAdapters::Utils
+          Oboe::Util.send_include(::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter,
+                                    Oboe::Inst::ConnectionAdapters::Utils)
 
-            if (::Rails::VERSION::MAJOR == 3 and ::Rails::VERSION::MINOR > 0) or ::Rails::VERSION::MAJOR == 4
+          if (::ActiveRecord::VERSION::MAJOR == 3 and ::ActiveRecord::VERSION::MINOR > 0) or 
+                ::ActiveRecord::VERSION::MAJOR == 4
 
-              # ActiveRecord 3.1 and up
-              if method_defined?(:exec_query)
-                alias_method :exec_query_without_oboe, :exec_query
-                alias_method :exec_query, :exec_query_with_oboe
-              else
-                Oboe.logger.debug "[oboe/loading] Couldn't instrument PostgreSQLAdapter.exec_query"
-              end
-                
-              if method_defined?(:exec_delete)
-                alias_method :exec_delete_without_oboe, :exec_delete
-                alias_method :exec_delete, :exec_delete_with_oboe
-              else
-                Oboe.logger.debug "[oboe/loading] Couldn't instrument PostgreSQLAdapter.exec_delete"
-              end
+            # ActiveRecord 3.1 and up
+            Oboe::Util.method_alias(::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter, :exec_query)
+            Oboe::Util.method_alias(::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter, :exec_delete)
 
-            else
-
-              # ActiveRecord 3.0 and prior
-              if method_defined?(:execute)
-                alias_method :execute_without_oboe, :execute
-                alias_method :execute, :execute_with_oboe
-              else
-                Oboe.logger.debug "[oboe/loading] Couldn't instrument PostgreSQLAdapter.execute"
-              end
-
-            end
-          end if defined?(::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+          else
+            # ActiveRecord 3.0 and prior
+            Oboe::Util.method_alias(::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter, :execute)
+          end
         end
       end
     end
