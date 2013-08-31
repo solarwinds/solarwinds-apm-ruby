6df69699df45b6168636e308e7fea095181e023f
Peter Giacomo Lombardo <pglombardo@gmail.com>
2013-08-31 17:39:59 +0200

    Use utility methods for method alias'ing

--- lib/oboe/frameworks/rails/inst/connection_adapters/mysql.rb
+++ lib/oboe/frameworks/rails/inst/connection_adapters/mysql.rb
@@ -9,69 +9,31 @@ module Oboe
           Oboe.logger.info "[oboe/loading] Instrumenting activerecord mysqladapter" if Oboe::Config[:verbose]
 
           # ActiveRecord 3.2 and higher
-          if (::Rails::VERSION::MAJOR == 3 and ::Rails::VERSION::MINOR >= 2) or ::Rails::VERSION::MAJOR == 4
+          if (::ActiveRecord::VERSION::MAJOR == 3 and ::ActiveRecord::VERSION::MINOR >= 2) or 
+              ::ActiveRecord::VERSION::MAJOR == 4
 
-            ::ActiveRecord::ConnectionAdapters::AbstractMysqlAdapter.module_eval do
-              include Oboe::Inst::ConnectionAdapters::Utils
+            # AbstractMysqlAdapter
+            Oboe::Util.send_include(::ActiveRecord::ConnectionAdapters::AbstractMysqlAdapter,
+                                      Oboe::Inst::ConnectionAdapters::Utils)
+            Oboe::Util.method_alias(::ActiveRecord::ConnectionAdapters::AbstractMysqlAdapter, :execute)
 
-              if method_defined?(:execute)
-                alias_method :execute_without_oboe, :execute
-                alias_method :execute, :execute_with_oboe
-              else
-                Oboe.logger.debug "[oboe/loading] Couldn't instrument AbstractMysqlAdapter.execute"
-              end
-            end
-
-            ::ActiveRecord::ConnectionAdapters::MysqlAdapter.module_eval do
-              include Oboe::Inst::ConnectionAdapters::Utils
-              
-              if method_defined?(:execute)
-                alias_method :exec_query_without_oboe, :exec_query
-                alias_method :exec_query, :exec_query_with_oboe
-              else
-                Oboe.logger.debug "[oboe/loading] Couldn't instrument MysqlAdapter.exec_query"
-              end
-            end
+            # MysqlAdapter
+            Oboe::Util.send_include(::ActiveRecord::ConnectionAdapters::MysqlAdapter,
+                                      Oboe::Inst::ConnectionAdapters::Utils)
+            Oboe::Util.method_alias(::ActiveRecord::ConnectionAdapters::MysqlAdapter, :exec_query)
 
           else
             # ActiveRecord 3.1 and below
-            ::ActiveRecord::ConnectionAdapters::MysqlAdapter.module_eval do
-              include Oboe::Inst::ConnectionAdapters::Utils
+            
+            # MysqlAdapter
+            Oboe::Util.send_include(::ActiveRecord::ConnectionAdapters::MysqlAdapter,
+                                      Oboe::Inst::ConnectionAdapters::Utils)
+
+            Oboe::Util.method_alias(::ActiveRecord::ConnectionAdapters::MysqlAdapter, :execute)
               
-              if method_defined?(:execute)
-                alias_method :execute_without_oboe, :execute
-                alias_method :execute, :execute_with_oboe
-              else
-                Oboe.logger.debug "[oboe/loading] Couldn't instrument MysqlAdapter.execute"
-              end
-                
-              if ::Rails::VERSION::MAJOR == 3 and ::Rails::VERSION::MINOR == 1
-
-                if method_defined?(:begin_db_transaction)
-                  alias_method :begin_db_transaction_without_oboe, :begin_db_transaction
-                  alias_method :begin_db_transaction, :begin_db_transaction_with_oboe
-                else
-                  Oboe.logger.debug "[oboe/loading] Couldn't instrument MysqlAdapter.begin_db_transaction"
-                end
-              end
-              
-              if ::Rails::VERSION::MAJOR == 3 and ::Rails::VERSION::MINOR > 0 
-
-                if method_defined?(:exec_query)
-                  alias_method :exec_query_without_oboe, :exec_query
-                  alias_method :exec_query, :exec_query_with_oboe
-                else
-                  Oboe.logger.debug "[oboe/loading] Couldn't instrument MysqlAdapter.exec_query"
-                end
-
-                if method_defined?(:exec_delete)
-                  alias_method :exec_delete_without_oboe, :exec_delete
-                  alias_method :exec_delete, :exec_delete_with_oboe
-                else
-                  Oboe.logger.debug "[oboe/loading] Couldn't instrument MysqlAdapter.exec_delete"
-                end
-              end
-
+            if ::ActiveRecord::VERSION::MAJOR == 3 and ::ActiveRecord::VERSION::MINOR == 1
+              Oboe::Util.method_alias(::ActiveRecord::ConnectionAdapters::MysqlAdapter, :begin_db_transaction)
+              Oboe::Util.method_alias(::ActiveRecord::ConnectionAdapters::MysqlAdapter, :exec_delete)
             end
           end
         end
