9510551b11ec6f3f957048fe3a0d4f9c4d1951ce
MikeA- <malexander@appneta.com>
2013-09-03 13:56:02 -0700

    Up version to 2.2.0

--- lib/oboe/version.rb
+++ lib/oboe/version.rb
@@ -1,8 +1,8 @@
 module Oboe
   module Version
     MAJOR = 2
-    MINOR = 1
-    PATCH = 4
+    MINOR = 2
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
