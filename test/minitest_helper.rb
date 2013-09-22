5c4bc363194f76fed5922b95e3550246ffdede78
Peter Giacomo Lombardo <pglombardo@gmail.com>
2013-09-22 22:55:32 +0200

    Helpers for trace processing.

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,7 +1,7 @@
 require "minitest/autorun"
 require "minitest/reporters"
 
-ENV["RAILS_ENV"] = "test"
+ENV["RACK_ENV"] = "test"
 MiniTest::Reporters.use!
 
 require 'rubygems'
@@ -12,5 +12,32 @@ Bundler.require(:default, :test)
 # Preload memcache-client
 require 'memcache'
 
+$trace_file = File.dirname(__FILE__) + "/../tmp/trace_output.bson"
+
+# Configure Oboe
+Oboe::Config[:tracing_mode] = "always"
+Oboe::Config[:sample_rate] = 1000000
 Oboe::Ruby.initialize
 
+##
+# clear_all_traces
+#
+# Truncates the trace output file to zero
+#
+def clear_all_traces
+  File.truncate($trace_file, 0)
+end
+
+##
+# get_all_traces
+#
+# Retrieves all traces written to the trace file
+#
+def get_all_traces
+  traces = []
+  f = File.open($trace_file)
+  until f.eof?
+    traces << BSON.read_bson_document(f)
+  end
+  traces
+end
