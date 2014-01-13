1f3975b870db470f042c2055417602fef8247540
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-01-13 20:33:16 +0100

    Group test suite into groups of Redis ops

--- test/instrumentation/redis_keys_test.rb
+++ test/instrumentation/redis_keys_test.rb
@@ -1,7 +1,7 @@
 require 'minitest_helper'
 require "redis"
     
-describe Oboe::Inst::Redis do
+describe Oboe::Inst::Redis, :keys do
   attr_reader :entry_kvs, :exit_kvs, :redis
 
   def min_server_version(version)
@@ -22,138 +22,6 @@ describe Oboe::Inst::Redis do
 
   it 'Stock Redis should be loaded, defined and ready' do
     defined?(::Redis).wont_match nil 
-  end
-
-  it "should trace basic set" do
-    Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.set("one",   "hello")
-    end
-
-    traces = get_all_traces
-    traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "set"
-    traces[1]['KVKey'].must_equal "one"
-  end
-  
-  it "should trace set with options hash (>= v2.6)" do
-    min_server_version(2.6)
-    unless (@redis.info["redis_version"] =~ /2.6/) == 0
-      skip "supported only redis-server 2.6.0" 
-    end
-
-    Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.set("one",   "hello")
-      @redis.set("two",   "hello", { :ex => 60 })
-      @redis.set("three", "hello", { :px => 1000 })
-      @redis.set("four",  "hello", { :nx => true })
-      @redis.set("five",  "hello", { :xx => true })
-    end
-
-    traces = get_all_traces
-    traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "set"
-    traces[1]['KVKey'].must_equal "one"
-    traces[3]['KVKey'].must_equal "two"
-    traces[3]['EX'].must_equal "60"
-    traces[5]['KVKey'].must_equal "three"
-    traces[5]['PX'].must_equal "1000"
-    traces[7]['KVKey'].must_equal "four"
-    traces[7]['NX'].must_equal "true"
-    traces[9]['KVKey'].must_equal "five"
-    traces[9]['XX'].must_equal "true"
-  end
-  
-  it "should trace setex" do
-    Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.setex("one", 60, "hello")
-    end
-
-    traces = get_all_traces
-    traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "setex"
-    traces[1]['KVKey'].must_equal "one"
-    traces[1]['ttl'].must_equal "60"
-  end
-  
-  it "should trace setnx" do
-    Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.setnx("one", "hello")
-    end
-
-    traces = get_all_traces
-    traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "setnx"
-    traces[1]['KVKey'].must_equal "one"
-  end
-
-  it "should trace psetex (>= v2.6)" do
-    unless (@redis.info["redis_version"] =~ /2.6/) == 0
-      skip "supported only redis-server 2.6.0" 
-    end
-
-    Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.psetex("one", 60, "hello")
-    end
-
-    traces = get_all_traces
-    traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "psetex"
-    traces[1]['KVKey'].must_equal "one"
-    traces[1]['ttl'].must_equal "60"
-  end
-  
-  it "should trace append" do
-    @redis.set("yourkey", "test")
-
-    Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.append("yourkey", "blah")
-    end
-
-    traces = get_all_traces
-    traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "append"
-    traces[1]['KVKey'].must_equal "yourkey"
-  end
-  
-  it "should trace bitcount (>=2.6)" do
-    
-    min_server_version("2.6")
-
-    Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.bitcount("yourkey")
-    end
-
-    traces = get_all_traces
-    traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "bitcount"
-    traces[1]['start'].must_equal "0"
-    traces[1]['stop'].must_equal "-1"
-  end
-  
-  it "should trace bitop (>=2.6)" do
-    
-    min_server_version("2.6")
-
-    Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.bitop("not", "bitopkey", "yourkey")
-    end
-
-    traces = get_all_traces
-    traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "bitop"
-    traces[1]['operation'].must_equal "not"
-    traces[1]['destkey'].must_equal "bitopkey"
-  end
-
-  it "should trace get" do
-    Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.get("yourkey")
-    end
-
-    traces = get_all_traces
-    traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "get"
-    traces[1]['KVKey'].must_equal "yourkey"
   end
   
   it "should trace del" do
@@ -181,7 +49,7 @@ describe Oboe::Inst::Redis do
     traces[1]['KVOp'].must_equal "del"
     traces[1].has_key?('KVKey').must_equal false
   end
-  
+
   it "should trace dump" do
     min_server_version(2.6)
 
@@ -196,7 +64,7 @@ describe Oboe::Inst::Redis do
     traces[1]['KVOp'].must_equal "dump"
     traces[1]['KVKey'].must_equal "del_test"
   end
-  
+
   it "should trace exists" do
     @redis.setex("talking_heads", 60, "burning down the house")
 
@@ -247,6 +115,26 @@ describe Oboe::Inst::Redis do
     traces[1]['pattern'].must_equal "del*"
   end
   
+  it "should trace migrate" do
+    skip "not supported"
+  end
+  
+  it "should trace basic move" do
+    Oboe::API.start_trace('redis_test', '', {}) do
+      @redis.move("piano", "anotherdb2")
+    end
+
+    traces = get_all_traces
+    traces.count.must_equal 4
+    traces[1]['KVOp'].must_equal "move"
+    traces[1]['KVKey'].must_equal "piano"
+    traces[1]['db'].must_equal "anotherdb"
+  end
+  
+  it "should trace object" do
+    skip "not supported"
+  end
+  
   it "should trace persist" do
     min_server_version(2.2)
 
@@ -261,7 +149,7 @@ describe Oboe::Inst::Redis do
     traces[1]['KVOp'].must_equal "persist"
     traces[1]['KVKey'].must_equal "mine"
   end
-
+  
   it "should trace pexpire" do
     min_server_version(2.6)
 
@@ -364,123 +252,65 @@ describe Oboe::Inst::Redis do
     traces[1]['KVKey'].must_equal "blue"
     traces[1]['ttl'].must_equal "0"
   end
-
-  it "should trace strlen" do
+  
+  it "should trace sort" do
     min_server_version(2.2)
 
-    @redis.setex("talking_heads", 60, "burning down the house")
+    @redis.rpush("penguin", "one")
+    @redis.rpush("penguin", "two")
+    @redis.rpush("penguin", "three")
+    @redis.rpush("penguin", "four")
 
     Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.strlen("talking_heads")
+      @redis.sort("penguin", { :limit => [0,5], :alpha => true, :desc => true })
     end
 
     traces = get_all_traces
     traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "strlen"
-    traces[1]['KVKey'].must_equal "talking_heads"
+    traces[1]['KVOp'].must_equal "sort"
+    traces[1]['KVKey'].must_equal "penguin"
   end
   
-  it "should trace getbit" do
-    min_server_version(2.2)
-
-    Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.getbit("yourkey", 3)
-    end
-
-    traces = get_all_traces
-    traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "getbit"
-    traces[1]['KVKey'].must_equal "yourkey"
-    traces[1]['offset'].must_equal "3"
-  end
-  
-  it "should trace setbit" do
-    min_server_version(2.2)
-
-    Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.setbit("yourkey", 3, 0)
-    end
-
-    traces = get_all_traces
-    traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "setbit"
-    traces[1]['KVKey'].must_equal "yourkey"
-    traces[1]['offset'].must_equal "3"
-  end
-  
-  it "should trace getrange" do
-    min_server_version(2.2)
-
-    Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.getrange("yourkey", 0, 3)
-    end
-
-    traces = get_all_traces
-    traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "getrange"
-    traces[1]['KVKey'].must_equal "yourkey"
-    traces[1]['start'].must_equal "0"
-    traces[1]['end'].must_equal "3"
-  end
-  
-  it "should trace setrange" do
-    min_server_version(2.2)
-
-    @redis.setex("spandau_ballet", 60, "XXXXXXXXXXXXXXX")
-
-    Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.setrange("yourkey", 2, "ok")
-    end
-
-    traces = get_all_traces
-    traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "setrange"
-    traces[1]['KVKey'].must_equal "yourkey"
-    traces[1]['offset'].must_equal "2"
-  end
-  
-  it "should trace decrby" do
-    @redis.setex("decr", 60, 0)
-
-    Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.decrby("decr", 1)
-    end
-
-    traces = get_all_traces
-    traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "decrby"
-    traces[1]['KVKey'].must_equal "decr"
-    traces[1]['decrement'].must_equal "1"
-  end
-  
-  it "should trace incrby" do
-    @redis.setex("incr", 60, 0)
-
-    Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.incrby("incr", 1)
-    end
-
-    traces = get_all_traces
-    traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "incrby"
-    traces[1]['KVKey'].must_equal "incr"
-    traces[1]['increment'].must_equal "1"
-  end
-  
-  it "should trace incrbyfloat" do
+  it "should trace ttl" do
     min_server_version(2.6)
 
-    @redis.setex("incrfloat", 60, 0.0)
+    @redis.setex("sand", 120, "blah")
 
     Oboe::API.start_trace('redis_test', '', {}) do
-      @redis.incrby("incrfloat", 1.01)
+      @redis.ttl("sand")
     end
 
     traces = get_all_traces
     traces.count.must_equal 4
-    traces[1]['KVOp'].must_equal "incrbyfloat"
-    traces[1]['KVKey'].must_equal "incrfloat"
-    traces[1]['increment'].must_equal "1.01"
+    traces[1]['KVOp'].must_equal "ttl"
+    traces[1]['KVKey'].must_equal "sand"
+  end
+  
+  it "should trace type" do
+    min_server_version(2.6)
+
+    @redis.setex("sand", 120, "blah")
+
+    Oboe::API.start_trace('redis_test', '', {}) do
+      @redis.type("sand")
+    end
+
+    traces = get_all_traces
+    traces.count.must_equal 4
+    traces[1]['KVOp'].must_equal "type"
+    traces[1]['KVKey'].must_equal "sand"
+  end
+  
+  it "should trace scan" do
+    min_server_version(2.8)
+
+    Oboe::API.start_trace('redis_test', '', {}) do
+      @redis.scan(0)
+    end
+
+    traces = get_all_traces
+    traces.count.must_equal 4
+    traces[1]['KVOp'].must_equal "scan"
   end
 end
 
