c5c96583085d4ffa4eaeb1559c965b6bbb2da135
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-01-22 19:55:42 +0100

    Tests for eval, evalsha and script

--- test/instrumentation/redis_pipelined_test.rb
+++ test/instrumentation/redis_pipelined_test.rb
@@ -64,5 +64,63 @@ describe Oboe::Inst::Redis, :sortedsets do
     traces[2]['KVOps'].must_equal "multi, zadd, zadd, zadd, lpush, lpush, lpush, exec"
   end
   
+  it "should trace eval" do
+    min_server_version(2.6)
+
+    Oboe::API.start_trace('redis_test', '', {}) do
+      @redis.eval("return 1")
+      @redis.eval("return { KEYS, ARGV }", ["k1", "k2"], ["a1", "a2"])
+      @redis.eval("return { KEYS, ARGV }", :keys => ["k1", "k2"], :argv => ["a1", "a2"])
+    end
+
+    traces = get_all_traces
+    traces.count.must_equal 8
+    traces[2]['KVOp'].must_equal "eval"
+    traces[2]['script'].must_equal "return 1"
+    traces[4]['KVOp'].must_equal "eval"
+    traces[4]['script'].must_equal "return { KEYS, ARGV }"
+    traces[6]['KVOp'].must_equal "eval"
+    traces[6]['script'].must_equal "return { KEYS, ARGV }"
+  end
+  
+  it "should trace evalsha" do
+    min_server_version(2.6)
+      
+    sha = @redis.script(:load, "return 1")
+
+    Oboe::API.start_trace('redis_test', '', {}) do
+      @redis.evalsha(sha)
+    end
+
+    traces = get_all_traces
+    traces.count.must_equal 4
+    traces[2]['KVOp'].must_equal "evalsha"
+    traces[2]['sha'].must_equal sha
+  end
+  
+  it "should trace script" do
+    min_server_version(2.6)
+      
+
+    Oboe::API.start_trace('redis_test', '', {}) do
+      sha = @redis.script(:load, "return 1")
+      @redis.script(:exists, sha)
+      @redis.script(:exists, [sha, "other_sha"])
+      @redis.script(:flush)
+    end
+
+    traces = get_all_traces
+    traces.count.must_equal 10
+    debugger
+    traces[2]['KVOp'].must_equal "script"
+    traces[2]['subcommand'].must_equal "load"
+    traces[2]['script'].must_equal "return 1"
+    traces[4]['KVOp'].must_equal "script"
+    traces[4]['subcommand'].must_equal "exists"
+    traces[6]['KVOp'].must_equal "script"
+    traces[6]['subcommand'].must_equal "exists"
+    traces[8]['KVOp'].must_equal "script"
+    traces[8]['subcommand'].must_equal "flush"
+  end
 end
 
