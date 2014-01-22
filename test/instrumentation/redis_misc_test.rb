73ce2fa3e1bdc36de1b0253fd82f12918536f2a3
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-01-22 20:02:35 +0100

    Merge all pipelined tests into the misc file

--- test/instrumentation/redis_misc_test.rb
+++ test/instrumentation/redis_misc_test.rb
@@ -49,5 +49,103 @@ describe Oboe::Inst::Redis, :misc do
     traces[2]['db'].must_equal "2"
   end
   
+  it "should trace pipelined operations" do
+    min_server_version(1.2)
+
+    Oboe::API.start_trace('redis_test', '', {}) do
+      @redis.pipelined do
+        @redis.zadd("staff", 0, "waiter")
+        @redis.zadd("staff", 1, "busser")
+        @redis.zadd("staff", 2, "chef")
+    
+        @redis.lpush("fringe", "bishop")
+        @redis.lpush("fringe", "dunham")
+        @redis.lpush("fringe", "broyles")
+      end
+    end
+
+    traces = get_all_traces
+    traces.count.must_equal 4
+    traces[2]['KVOpCount'].must_equal "6"
+    traces[2]['KVOps'].must_equal "zadd, zadd, zadd, lpush, lpush, lpush"
+  end
+  
+  it "should trace multi with block" do
+    min_server_version(1.2)
+
+    Oboe::API.start_trace('redis_test', '', {}) do
+      @redis.multi do
+        @redis.zadd("presidents", 0, "Lincoln")
+        @redis.zadd("presidents", 1, "Adams")
+        @redis.zadd("presidents", 2, "Reagan")
+    
+        @redis.lpush("hair", "blue")
+        @redis.lpush("hair", "gray")
+        @redis.lpush("hair", "yellow")
+      end
+    end
+
+    traces = get_all_traces
+    traces.count.must_equal 4
+    traces[2]['KVOpCount'].must_equal "8"
+    traces[2]['KVOps'].must_equal "multi, zadd, zadd, zadd, lpush, lpush, lpush, exec"
+  end
+  
+  it "should trace eval" do
+    min_server_version(2.6)
+
+    Oboe::API.start_trace('redis_test', '', {}) do
+      @redis.eval("return 1")
+      @redis.eval("return { KEYS, ARGV }", ["k1", "k2"], ["a1", "a2"])
+      @redis.eval("return { KEYS, ARGV }", :keys => ["k1", "k2"], :argv => ["a1", "a2"])
+    end
+
+    traces = get_all_traces
+    traces.count.must_equal 8
+    traces[2]['KVOp'].must_equal "eval"
+    traces[2]['script'].must_equal "return 1"
+    traces[4]['KVOp'].must_equal "eval"
+    traces[4]['script'].must_equal "return { KEYS, ARGV }"
+    traces[6]['KVOp'].must_equal "eval"
+    traces[6]['script'].must_equal "return { KEYS, ARGV }"
+  end
+  
+  it "should trace evalsha" do
+    min_server_version(2.6)
+      
+    sha = @redis.script(:load, "return 1")
+
+    Oboe::API.start_trace('redis_test', '', {}) do
+      @redis.evalsha(sha)
+    end
+
+    traces = get_all_traces
+    traces.count.must_equal 4
+    traces[2]['KVOp'].must_equal "evalsha"
+    traces[2]['sha'].must_equal sha
+  end
+  
+  it "should trace script" do
+    min_server_version(2.6)
+      
+    Oboe::API.start_trace('redis_test', '', {}) do
+      sha = @redis.script(:load, "return 1")
+      @redis.script(:exists, sha)
+      @redis.script(:exists, [sha, "other_sha"])
+      @redis.script(:flush)
+    end
+
+    traces = get_all_traces
+    traces.count.must_equal 10
+    traces[2]['KVOp'].must_equal "script"
+    traces[2]['subcommand'].must_equal "load"
+    traces[2]['script'].must_equal "return 1"
+    traces[4]['KVOp'].must_equal "script"
+    traces[4]['subcommand'].must_equal "exists"
+    traces[6]['KVOp'].must_equal "script"
+    traces[6]['subcommand'].must_equal "exists"
+    traces[8]['KVOp'].must_equal "script"
+    traces[8]['subcommand'].must_equal "flush"
+  end
 end
 
