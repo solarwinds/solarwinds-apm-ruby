c95424165396d2868eda9666ed5cc1d210323547
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-02-03 21:58:00 +0100

    Use method profiling for partial/sub-view rendering

--- lib/oboe/frameworks/sinatra/templates.rb
+++ lib/oboe/frameworks/sinatra/templates.rb
@@ -15,19 +15,40 @@ module Oboe
           report_kvs[:engine] = engine
           report_kvs[:template] = data
 
-          # Fall back to the raw tracing API so we can pass KVs
-          # back on exit (a limitation of the Oboe::API.trace
-          # block method) This removes the need for an info
-          # event to send additonal KVs
-          ::Oboe::API.log_entry('render', {})
+          if Oboe::Context.tracing_layer_op?('render')
+            # For recursive calls to :render (for sub-partials and layouts), 
+            # use method profiling.
+            begin
+              name = data
+              report_kvs[:FunctionName] = :render
+              report_kvs[:Class]        = :Templates
+              report_kvs[:Module]       = 'Sinatra::Templates'
+              report_kvs[:File]         = __FILE__
+              report_kvs[:LineNumber]   = __LINE__
+            rescue StandardError => e
+              ::Oboe.logger.debug e.message
+              ::Oboe.logger.debug e.backtrace.join(", ")
+            end
 
-          begin
-            render_without_oboe(engine, data, options, locals, &block)
-          ensure
-            ::Oboe::API.log_exit('render', report_kvs)
+            Oboe::API.profile(name, report_kvs, false) do
+              render_without_oboe(engine, data, options, locals, &block)
+            end
+
+          else
+            # Fall back to the raw tracing API so we can pass KVs
+            # back on exit (a limitation of the Oboe::API.trace
+            # block method) This removes the need for an info
+            # event to send additonal KVs
+            ::Oboe::API.log_entry('render', {}, 'render')
+
+            begin
+              render_without_oboe(engine, data, options, locals, &block)
+            ensure
+              ::Oboe::API.log_exit('render', report_kvs)
+            end
           end
         else
-          render_without_oboe
+          render_without_oboe(engine, data, options, locals, &block)
         end
       end
     end
