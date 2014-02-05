9bbf7d512de84f1adf42c4d0f8d0d2a055eab7f0
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-02-05 20:00:41 +0100

    Instrument Padrino's enhanced routing with Controller/Action KV reporting

--- lib/oboe/frameworks/padrino.rb
+++ lib/oboe/frameworks/padrino.rb
@@ -1,13 +1,56 @@
-# Copyright (c) 2013 AppNeta, Inc.
+# Copyright (c) 2014 AppNeta, Inc.
 # All rights reserved.
 
-if defined?(::Padrino)
-  # This instrumentation is a superset of the Sinatra similar to how Padrino
-  # is a superset of Sinatra itself.
-  Oboe.logger.info "[oboe/loading] Instrumenting Padrino" if Oboe::Config[:verbose]
+module Oboe
+  module PadrinoInst
+    module Routing
+      def self.included(klass)
+        ::Oboe::Util.method_alias(klass, :dispatch!, ::Padrino::Routing)
+      end
 
-  Oboe.logger = ::Padrino.logger if ::Padrino.logger
-  Oboe::Loading.load_access_key
-  Oboe::Inst.load_instrumentation
+      def dispatch_with_oboe
+        if Oboe.tracing?
+          report_kvs = {}
+
+          # Fall back to the raw tracing API so we can pass KVs
+          # back on exit (a limitation of the Oboe::API.trace
+          # block method) This removes the need for an info
+          # event to send additonal KVs
+          ::Oboe::API.log_entry('padrino', {})
+
+          begin
+            dispatch_without_oboe
+
+            # Report Controller/Action as best possible
+            if request.controller and not request.controller.empty?
+              report_kvs[:Controller] = request.controller
+            else
+              report_kvs[:Controller] = self.class
+            end
+         
+            report_kvs[:Action] = request.action
+          ensure
+            ::Oboe::API.log_exit('padrino', report_kvs)
+          end
+        else
+          dispatch_without_oboe
+        end
+      end
+    end
+  end
+end
+
+if defined?(::Padrino)
+  # This instrumentation is a superset of the Sinatra instrumentation similar 
+  # to how Padrino is a superset of Sinatra itself.
+  Oboe.logger.info "[oboe/loading] Instrumenting Padrino" if Oboe::Config[:verbose]
+  
+  Padrino.after_load do
+    Oboe.logger = ::Padrino.logger if ::Padrino.respond_to?(:logger)
+    Oboe::Loading.load_access_key
+    Oboe::Inst.load_instrumentation
+
+    ::Oboe::Util.send_include(::Padrino::Routing::InstanceMethods, ::Oboe::PadrinoInst::Routing)
+  end
 end
 
