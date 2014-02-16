aed8db8e58ef0cac201a35d887b97968b945926c
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-02-16 18:26:14 +0100

    Sinatra framework tests

---
+++ test/frameworks/test_sinatra.rb
@@ -0,0 +1,26 @@
+require "minitest_helper"
+require File.expand_path(File.dirname(__FILE__) + '/apps/sinatra_simple')
+
+describe Sinatra do
+  before do
+    clear_all_traces 
+  end
+
+  it "should trace a request to a simple sinatra stack" do
+    @app = SinatraSimple
+    
+    r = get "/render"
+    
+    traces = get_all_traces
+    traces.count.must_equal 9
+
+    validate_outer_layers(traces, 'rack')
+
+    traces[1]['Layer'].must_equal "sinatra"
+    traces[3]['Label'].must_equal "profile_entry"
+    traces[5]['Layer'].must_equal "render"
+    traces[6]['Controller'].must_equal "SinatraSimple"
+    traces[7]['Label'].must_equal "info"
+  end
+end
+
