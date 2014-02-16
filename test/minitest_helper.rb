5667865015862dd8fb5275af7a36f1fe60531ba7
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-02-16 16:38:17 +0100

    Include Sinatra/Padrino test helpers

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -110,3 +110,30 @@ def layer_doesnt_have_key(traces, layer, key)
   has_key.must_equal false
 end
 
+##
+# Sinatra and Padrino Related Helpers
+# 
+# Taken from padrino-core gem
+#
+
+class Sinatra::Base
+  # Allow assertions in request context
+  include MiniTest::Assertions
+end
+
+
+class MiniTest::Spec
+  include Rack::Test::Methods
+
+  # Sets up a Sinatra::Base subclass defined with the block
+  # given. Used in setup or individual spec methods to establish
+  # the application.
+  def mock_app(base=Padrino::Application, &block)
+    @app = Sinatra.new(base, &block)
+  end
+
+  def app
+    Rack::Lint.new(@app)
+  end
+end
+
