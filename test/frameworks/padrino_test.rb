2a67bda1ca51fb2f577c9201e5902020c7c34e11
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-03-10 19:45:22 +0100

    Don't run Padrino tests under Ruby 1.8

--- test/frameworks/padrino_test.rb
+++ test/frameworks/padrino_test.rb
@@ -1,24 +1,25 @@
-require "minitest_helper"
-require File.expand_path(File.dirname(__FILE__) + '/apps/padrino_simple')
+unless (RUBY_VERSION =~ /^1.8/) == 0
+  require "minitest_helper"
+  require File.expand_path(File.dirname(__FILE__) + '/apps/padrino_simple')
 
-describe Padrino do
-  before do
-    clear_all_traces 
-  end
+  describe Padrino do
+    before do
+      clear_all_traces 
+    end
 
-  it "should trace a request to a simple padrino stack" do
-    @app = SimpleDemo
-    
-    r = get "/render"
-    
-    traces = get_all_traces
-    traces.count.must_equal 9
+    it "should trace a request to a simple padrino stack" do
+      @app = SimpleDemo
+      
+      r = get "/render"
+      
+      traces = get_all_traces
+      traces.count.must_equal 9
 
-    validate_outer_layers(traces, 'rack')
+      validate_outer_layers(traces, 'rack')
 
-    traces[1]['Layer'].must_equal "padrino"
-    traces[6]['Controller'].must_equal "SimpleDemo"
-    traces[7]['Label'].must_equal "info"
+      traces[1]['Layer'].must_equal "padrino"
+      traces[6]['Controller'].must_equal "SimpleDemo"
+      traces[7]['Label'].must_equal "info"
+    end
   end
 end
-
