61f7de16081bf342068cb9ff23962bdb9818501a
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-03-31 20:16:58 +0200

    Add tests to validate tracing modes and sample_rate

--- test/support/srv1_localset_test.rb
+++ test/support/srv1_localset_test.rb
@@ -3,10 +3,7 @@ require 'rack/test'
 require 'rack/lobster'
 require 'oboe/inst/rack'
 
-Oboe::Config[:tracing_mode] = 'always'
-Oboe::Config[:sample_rate] = 1e6
-    
-class RackTestApp < MiniTest::Unit::TestCase
+class RackTestApp < Minitest::Test
   include Rack::Test::Methods
 
   def app
@@ -18,10 +15,13 @@ class RackTestApp < MiniTest::Unit::TestCase
         use Rack::Lint
         run Rack::Lobster.new
       end
+      run lambda { |env| [200, {'Content-Type' => 'text/plain'}, ['OK']] }
     }
   end
 
   def test_localset_sample_source
+    Oboe::Config[:tracing_mode] = 'always'
+    Oboe::Config[:sample_rate] = 1e6
     clear_all_traces 
 
     get "/lobster"
@@ -35,7 +35,43 @@ class RackTestApp < MiniTest::Unit::TestCase
     kvs["SampleRate"] = "1000000"
     kvs["SampleSource"] = OBOE_SAMPLE_RATE_SOURCE_FILE.to_s
     validate_event_keys(traces[1], kvs)
+  end
+  
+  def test_sample_rate
+    Oboe::Config[:tracing_mode] = 'always'
+    Oboe::Config[:sample_rate] = 500000
+    clear_all_traces 
 
+    10.times do
+      get "/"
+    end
+
+    traces = get_all_traces
+    traces.count.between?(4, 6).must_equal true
+  end
+  
+  def test_tracing_mode_never
+    Oboe::Config[:tracing_mode] = 'never'
+    clear_all_traces 
+
+    10.times do
+      get "/"
+    end
+
+    traces = get_all_traces
+    traces.count.must_equal 0
+  end
+  
+  def test_tracing_mode_through
+    Oboe::Config[:tracing_mode] = 'through'
+    clear_all_traces 
+
+    10.times do
+      get "/"
+    end
+
+    traces = get_all_traces
+    traces.count.must_equal 0
   end
 end
 
