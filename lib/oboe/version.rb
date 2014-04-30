74fa19eb18f32be3e67be6d49207c9e406a684de
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-04-30 17:41:16 +0200

    Bump version to 2.6.4.1

--- lib/oboe/version.rb
+++ lib/oboe/version.rb
@@ -5,8 +5,8 @@ module Oboe
   module Version
     MAJOR = 2
     MINOR = 6 
-    PATCH = 3
-    BUILD = 0
+    PATCH = 4
+    BUILD = 1
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
