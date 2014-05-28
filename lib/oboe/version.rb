d32cb78c32086bec898d4a9d72af86dc0b2d63b0
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-05-29 00:07:25 +0200

    Bump gem version to 2.6.5

--- lib/oboe/version.rb
+++ lib/oboe/version.rb
@@ -5,8 +5,8 @@ module Oboe
   module Version
     MAJOR = 2
     MINOR = 6 
-    PATCH = 4
-    BUILD = 1
+    PATCH = 5
+    BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
