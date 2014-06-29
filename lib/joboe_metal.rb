af96ddc4fe60652fd8c6a095771029d48502319b
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-06-29 20:47:29 +0200

    Clean up OboeBase, c-extension and JRuby interfaces

--- lib/joboe_metal.rb
+++ lib/joboe_metal.rb
@@ -15,30 +15,6 @@ module Oboe_metal
 
   class Context
     class << self
-      attr_accessor :layer_op
-      
-      def log(layer, label, options = {}, with_backtrace = false)
-        evt = Oboe::Context.createEvent()
-        evt.addInfo("Layer", layer.to_s)
-        evt.addInfo("Label", label.to_s)
-
-        options.each_pair do |k, v|
-          evt.addInfo(k.to_s, v.to_s)
-        end
-
-        evt.addInfo("Backtrace", Oboe::API.backtrace) if with_backtrace
-
-        Oboe.reporter.sendReport(evt)
-      end
-       
-      def tracing_layer_op?(operation)
-        if operation.is_a?(Array)
-          return operation.include?(@layer_op)
-        else
-          return @layer_op == operation
-        end
-      end
-      
       def toString
         md = getMetadata.toString
       end
@@ -52,7 +28,7 @@ module Oboe_metal
       end
     end
   end
-  
+
   class Event
     def self.metadataString(evt)
       evt.getMetadata.toHexString
@@ -62,11 +38,11 @@ module Oboe_metal
   def UdpReporter
     Java::ComTracelyticsJoboe
   end
-  
+
   module Metadata
     Java::ComTracelyticsJoboeMetaData
   end
-  
+
   module Reporter
     ##
     # Initialize the Oboe Context, reporter and report the initialization
@@ -75,7 +51,7 @@ module Oboe_metal
       begin
         return unless Oboe.loaded
 
-        Oboe_metal::Context.init() 
+        Oboe_metal::Context.init()
 
         if ENV['RACK_ENV'] == "test"
           Oboe.reporter = Oboe::FileReporter.new("/tmp/trace_output.bson")
@@ -88,23 +64,23 @@ module Oboe_metal
         unless defined?(::Rails) or defined?(::Sinatra) or defined?(::Padrino) or defined?(::Grape)
           Oboe::API.report_init
         end
-      
+
       rescue Exception => e
         $stderr.puts e.message
         raise
       end
     end
-    
+
     def self.sendReport(evt)
       evt.report
     end
   end
 end
 
-module Oboe 
+module Oboe
   extend OboeBase
   include Oboe_metal
-  
+
   class << self
     def sample?(opts = {})
       # Assure defaults since SWIG enforces Strings
@@ -113,11 +89,11 @@ module Oboe
       opts['X-TV-Meta']   ||= ''
       Java::ComTracelyticsJoboeSettingsReader.shouldTraceRequest(opts[:layer], opts[:xtrace], opts['X-TV-Meta'])
     end
-    
+
     def set_tracing_mode(mode)
       # FIXME: TBD
     end
-    
+
     def set_sample_rate(rate)
       # FIXME: TBD
     end
