f31dd50197100fed136f54464de95c7a0da38dd3
Diogo Benicá <diogo.pereira@abril.com.br>
2014-07-07 10:24:48 -0300

    Attaches instrumentation directly to method instead of overwriting headers callback.

--- lib/oboe/inst/em-http-request.rb
+++ lib/oboe/inst/em-http-request.rb
@@ -10,17 +10,21 @@ module Oboe
           client = setup_request_without_oboe(*args, &block)
           client.req.headers["X-Trace"] = Oboe::Context.toString()
           ::Oboe::API.log(nil, 'info', report_kvs)
-          client.headers do |hash|
-            ::Oboe::API.log_exit('em-http-request', { Async: 1 })
-          end
           client
+        end
+      end
+
+      module HttpClient
+        def parse_response_header_with_oboe(*args, &block)
+          ::Oboe::API.log_exit('em-http-request', { Async: 1 })
+          parse_response_header_without_oboe(*args, &block)
         end
       end
     end
   end
 end
 
-if defined?(::EventMachine::HttpConnection) and Oboe::Config[:em_http_request][:enabled]
+if defined?(::EventMachine::HttpConnection) defined?(::EventMachine::HttpClient) and Oboe::Config[:em_http_request][:enabled]
   Oboe.logger.info "[oboe/loading] Instrumenting em-http-request" if Oboe::Config[:verbose]
 
   class ::EventMachine::HttpConnection
@@ -29,8 +33,19 @@ if defined?(::EventMachine::HttpConnection) and Oboe::Config[:em_http_request][:
     if method_defined?(:setup_request)
       class_eval "alias :setup_request_without_oboe :setup_request"
       class_eval "alias :setup_request :setup_request_with_oboe"
-    else 
+    else
       Oboe.logger.warn "[oboe/loading] Couldn't properly instrument em-http-request (:setup_request).  Partial traces may occur."
+    end
+  end
+
+  class ::EventMachine::HttpClient
+    include Oboe::Inst::EventMachine::HttpClient
+
+    if method_defined?(:parse_response_header)
+      class_eval "alias :parse_response_header_without_oboe :parse_response_header"
+      class_eval "alias :parse_response_header :parse_response_header_with_oboe"
+    else
+      Oboe.logger.warn "[oboe/loading] Couldn't properly instrument em-http-request (:parse_response_header).  Partial traces may occur."
     end
   end
 end
