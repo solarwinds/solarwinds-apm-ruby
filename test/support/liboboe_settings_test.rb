05ee8f8b1cdb4ddc03cbeaec99795bc030093ab3
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-07-27 18:55:28 +0200

    Don't test liboboe specific stuff under JRuby

--- test/support/liboboe_settings_test.rb
+++ test/support/liboboe_settings_test.rb
@@ -3,39 +3,40 @@ require 'rack/test'
 require 'rack/lobster'
 require 'oboe/inst/rack'
 
-Oboe::Config[:tracing_mode] = 'always'
-Oboe::Config[:sample_rate] = 1e6
+unless defined?(JRUBY_VERSION)
+  Oboe::Config[:tracing_mode] = 'always'
+  Oboe::Config[:sample_rate] = 1e6
 
-class RackTestApp < Minitest::Test
-  include Rack::Test::Methods
+  class RackTestApp < Minitest::Test
+    include Rack::Test::Methods
 
-  def app
-    @app = Rack::Builder.new {
-      use Rack::CommonLogger
-      use Rack::ShowExceptions
-      use Oboe::Rack
-      map "/lobster" do
-        use Rack::Lint
-        run Rack::Lobster.new
-      end
-    }
-  end
+    def app
+      @app = Rack::Builder.new {
+        use Rack::CommonLogger
+        use Rack::ShowExceptions
+        use Oboe::Rack
+        map "/lobster" do
+          use Rack::Lint
+          run Rack::Lobster.new
+        end
+      }
+    end
 
-  def test_localset_sample_source
-    clear_all_traces
+    def test_localset_sample_source
+      clear_all_traces
 
-    get "/lobster"
+      get "/lobster"
 
-    traces = get_all_traces
-    traces.count.must_equal 3
+      traces = get_all_traces
+      traces.count.must_equal 3
 
-    validate_outer_layers(traces, 'rack')
+      validate_outer_layers(traces, 'rack')
 
-    kvs = {}
-    kvs["SampleRate"] = "1000000"
-    kvs["SampleSource"] = OBOE_SAMPLE_RATE_SOURCE_FILE.to_s
-    validate_event_keys(traces[0], kvs)
+      kvs = {}
+      kvs["SampleRate"] = "1000000"
+      kvs["SampleSource"] = OBOE_SAMPLE_RATE_SOURCE_FILE.to_s
+      validate_event_keys(traces[0], kvs)
 
+    end
   end
 end
-
