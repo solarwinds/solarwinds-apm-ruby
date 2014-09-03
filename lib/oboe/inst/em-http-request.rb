75efeaa58e65016cae3fbcc2c86d7370e2a3c022
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-09-03 19:57:40 +0200

    Lock EventMachine and EM::Synchrony to >= Ruby 1.9

--- lib/oboe/inst/em-http-request.rb
+++ lib/oboe/inst/em-http-request.rb
@@ -44,28 +44,30 @@ module Oboe
   end
 end
 
-if defined?(::EventMachine::HttpConnection) and defined?(::EventMachine::HttpClient) and Oboe::Config[:em_http_request][:enabled]
-  Oboe.logger.info "[oboe/loading] Instrumenting em-http-request" if Oboe::Config[:verbose]
+if RUBY_VERSION >= '1.9'
+  if defined?(::EventMachine::HttpConnection) and defined?(::EventMachine::HttpClient) and Oboe::Config[:em_http_request][:enabled]
+    Oboe.logger.info "[oboe/loading] Instrumenting em-http-request" if Oboe::Config[:verbose]
 
-  class ::EventMachine::HttpConnection
-    include Oboe::Inst::EventMachine::HttpConnection
+    class ::EventMachine::HttpConnection
+      include Oboe::Inst::EventMachine::HttpConnection
 
-    if method_defined?(:setup_request)
-      class_eval "alias :setup_request_without_oboe :setup_request"
-      class_eval "alias :setup_request :setup_request_with_oboe"
-    else
-      Oboe.logger.warn "[oboe/loading] Couldn't properly instrument em-http-request (:setup_request).  Partial traces may occur."
+      if method_defined?(:setup_request)
+        class_eval "alias :setup_request_without_oboe :setup_request"
+        class_eval "alias :setup_request :setup_request_with_oboe"
+      else
+        Oboe.logger.warn "[oboe/loading] Couldn't properly instrument em-http-request (:setup_request).  Partial traces may occur."
+      end
     end
-  end
 
-  class ::EventMachine::HttpClient
-    include Oboe::Inst::EventMachine::HttpClient
+    class ::EventMachine::HttpClient
+      include Oboe::Inst::EventMachine::HttpClient
 
-    if method_defined?(:parse_response_header)
-      class_eval "alias :parse_response_header_without_oboe :parse_response_header"
-      class_eval "alias :parse_response_header :parse_response_header_with_oboe"
-    else
-      Oboe.logger.warn "[oboe/loading] Couldn't properly instrument em-http-request (:parse_response_header).  Partial traces may occur."
+      if method_defined?(:parse_response_header)
+        class_eval "alias :parse_response_header_without_oboe :parse_response_header"
+        class_eval "alias :parse_response_header :parse_response_header_with_oboe"
+      else
+        Oboe.logger.warn "[oboe/loading] Couldn't properly instrument em-http-request (:parse_response_header).  Partial traces may occur."
+      end
     end
   end
 end
