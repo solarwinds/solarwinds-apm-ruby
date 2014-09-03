8ce2cded32271c68a4d8779f321dd407c2d8e88d
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-09-04 00:18:58 +0200

    Support blacklist and handle returned X-Trace IDs

--- lib/oboe/inst/em-http-request.rb
+++ lib/oboe/inst/em-http-request.rb
@@ -4,12 +4,14 @@ module Oboe
       module HttpConnection
         def setup_request_with_oboe(*args, &block)
           report_kvs = {}
+          context = Oboe::Context.toString()
+          blacklisted = Oboe::API.blacklisted?(@uri)
 
           begin
             report_kvs["IsService"] = 1
             report_kvs["RemoteURL"] = @uri
             report_kvs["HTTPMethod"] = args[0]
-            report_kvs["Blacklisted"] = true if Oboe::API.blacklisted?(@uri)
+            report_kvs["Blacklisted"] = true if blacklisted
 
             if Oboe::Config[:em_http_request][:collect_backtraces]
               report_kvs[:Backtrace] = Oboe::API.backtrace
@@ -19,8 +21,11 @@ module Oboe
           end
 
           ::Oboe::API.log_entry("em-http-request", report_kvs)
+
           client = setup_request_without_oboe(*args, &block)
-          client.req.headers["X-Trace"] = Oboe::Context.toString()
+
+          client.req.headers["X-Trace"] = context unless blacklisted
+
           client
         end
       end
@@ -28,6 +33,8 @@ module Oboe
       module HttpClient
         def parse_response_header_with_oboe(*args, &block)
           report_kvs = {}
+          xtrace = nil
+          blacklisted = Oboe::API.blacklisted?(@uri)
 
           begin
             report_kvs[:HTTPStatus] = args[2]
@@ -37,6 +44,28 @@ module Oboe
           end
 
           parse_response_header_without_oboe(*args, &block)
+
+          unless blacklisted
+            headers = args[0]
+            context = Oboe::Context.toString()
+            task_id = Oboe::XTrace.task_id(context)
+
+            if headers.is_a?(Hash) and headers.has_key?("X-Trace")
+              xtrace = headers["X-Trace"]
+            end
+
+            if Oboe::XTrace.valid?(xtrace) and Oboe.tracing?
+
+              # Assure that we received back a valid X-Trace with the same task_id
+              if task_id == Oboe::XTrace.task_id(xtrace)
+                Oboe::Context.fromString(xtrace)
+              else
+                Oboe.logger.debug "Mismatched returned X-Trace ID : #{xtrace}"
+              end
+            end
+
+          end
+
           ::Oboe::API.log_exit("em-http-request", report_kvs)
         end
       end
