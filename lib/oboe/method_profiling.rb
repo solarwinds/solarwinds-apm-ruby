44bcda8a4f7ad2ddeaee0ef62b8d5005c6c74b88
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-09-10 19:25:13 +0200

    Documentation and lint improvments

--- lib/oboe/method_profiling.rb
+++ lib/oboe/method_profiling.rb
@@ -1,23 +1,37 @@
 # Copyright (c) 2013 AppNeta, Inc.
 # All rights reserved.
 
+##
+# Provides the methods necessary for method profiling.  Profiling
+# results are sent to the TraceView dashboard.
+#
+# Example usage:
+# class MyApp
+#   include OboeMethodProfiling
+#
+#   def process_request()
+#     # The hard work
+#   end
+#
+#   # call syntax: profile_method <method>, <profile_name>
+#   profile_method :process_request, 'request_processor'
+# end
 module OboeMethodProfiling
-  def self.included klass
+  def self.included(klass)
     klass.extend ClassMethods
   end
 
   module ClassMethods
-    def profile_method(method_name, profile_name, store_args=false, store_return=false, profile=false)
+    def profile_method(method_name, profile_name, store_args = false, store_return = false, *_)
       begin
         # this only gets file and line where profiling is turned on, presumably
         # right after the function definition. ruby 1.9 and 2.0 has nice introspection (Method.source_location)
         # but its appears no such luck for ruby 1.8
-        version = RbConfig::CONFIG['ruby_version']
         file = ''
         line = ''
-        if version and (version.match(/^1.9/) or version.match(/^2.0/))
-          info = self.instance_method(method_name).source_location
-          if !info.nil?
+        if RUBY_VERSION >= '1.9'
+          info = instance_method(method_name).source_location
+          unless info.nil?
             file = info[0].to_s
             line = info[1].to_s
           end
@@ -54,7 +68,7 @@ module OboeMethodProfiling
                   Oboe::API.log(nil, 'profile_exit', exit_kvs)
                   ret
                 end"
-      rescue Exception => e
+      rescue => e
         Oboe.logger.warn "[oboe/warn] profile_method: #{e.inspect}"
       end
 
@@ -62,7 +76,7 @@ module OboeMethodProfiling
         class_eval code, __FILE__, __LINE__
         alias_method "_oboe_orig_#{method_name}", method_name
         alias_method method_name, "_oboe_profiled_#{method_name}"
-      rescue Exception => e
+      rescue => e
         Oboe.logger.warn "[oboe/warn] Fatal error profiling method (#{method_name}): #{e.inspect}" if Oboe::Config[:verbose]
       end
     end
