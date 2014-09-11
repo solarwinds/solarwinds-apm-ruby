4a7d7f52388217c4913e926c476c56ff444fd2a1
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-09-11 17:35:13 +0200

    Lint improvements: single quotes, operators

--- ext/oboe_metal/extconf.rb
+++ ext/oboe_metal/extconf.rb
@@ -7,7 +7,7 @@ require 'rbconfig'
 # Check if we're running in JRuby
 jruby = defined?(JRUBY_VERSION) ? true : false
 
-openshift = ENV.has_key?('OPENSHIFT_TRACEVIEW_DIR')
+openshift = ENV.key?('OPENSHIFT_TRACEVIEW_DIR')
 
 # When on OpenShift, set the mkmf lib paths so we have no issues linking to
 # the TraceView libs.
@@ -23,7 +23,7 @@ else
   dir_config('oboe')
 end
 
-if jruby or ENV.has_key?('TRACEVIEW_URL')
+if jruby || ENV.key?('TRACEVIEW_URL')
   # Build the noop extension under JRuby and Heroku.
   # The oboe-heroku gem builds it's own c extension which links to
   # libs specific to a Heroku dyno
@@ -32,30 +32,30 @@ if jruby or ENV.has_key?('TRACEVIEW_URL')
 
 elsif have_library('oboe', 'oboe_config_get_revision', 'oboe/oboe.h')
 
-  $libs = append_library($libs, "oboe")
-  $libs = append_library($libs, "stdc++")
+  $libs = append_library($libs, 'oboe')
+  $libs = append_library($libs, 'stdc++')
 
-  $CFLAGS << " #{ENV["CFLAGS"]}"
-  $CPPFLAGS << " #{ENV["CPPFLAGS"]}"
-  $LIBS << " #{ENV["LIBS"]}"
+  $CFLAGS << " #{ENV['CFLAGS']}"
+  $CPPFLAGS << " #{ENV['CPPFLAGS']}"
+  $LIBS << " #{ENV['LIBS']}"
 
   # On OpenShift user rpath to point out the TraceView libraries
   if openshift
-    $LDFLAGS << " #{ENV["LDFLAGS"]} -Wl,-rpath=#{tv_lib64},--rpath=#{tv_tlyzer}"
+    $LDFLAGS << " #{ENV['LDFLAGS']} -Wl,-rpath=#{tv_lib64},--rpath=#{tv_tlyzer}"
   end
 
   if RUBY_VERSION < '1.9'
     cpp_command('g++')
-    $CPPFLAGS << "-I./src/"
+    $CPPFLAGS << '-I./src/'
   end
   create_makefile('oboe_metal', 'src')
 
 else
   if have_library('oboe')
-    $stderr.puts "Error: The oboe gem requires an updated liboboe.  Please update your liboboe packages."
+    $stderr.puts 'Error: The oboe gem requires an updated liboboe.  Please update your liboboe packages.'
   end
 
-  $stderr.puts "Error: Could not find the base liboboe libraries.  No tracing will occur."
+  $stderr.puts 'Error: Could not find the base liboboe libraries.  No tracing will occur.'
   create_makefile('oboe_noop', 'noop')
 end
 
