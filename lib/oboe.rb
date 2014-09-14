5bf3cf39d86ac4f610113a3f06109e0590e63fb7
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-09-14 22:03:50 +0200

    Load the Ruby module last

--- lib/oboe.rb
+++ lib/oboe.rb
@@ -39,14 +39,17 @@ begin
     require 'oboe/loading'
     require 'oboe/method_profiling'
     require 'oboe/instrumentation'
-    require 'oboe/ruby'
 
     # Frameworks
-    require 'oboe/frameworks/rails'   if defined?(::Rails)
-    require 'oboe/frameworks/sinatra' if defined?(::Sinatra)
-    require 'oboe/frameworks/padrino' if defined?(::Padrino)
-    require 'oboe/frameworks/grape'   if defined?(::Grape)
+    require 'oboe/frameworks/rails'
+    require 'oboe/frameworks/sinatra'
+    require 'oboe/frameworks/padrino'
+    require 'oboe/frameworks/grape'
   end
+
+  # Load Ruby module last.  If there is no framework detected,
+  # it will load all of the Ruby instrumentation
+  require 'oboe/ruby'
 rescue => e
   $stderr.puts '[oboe/error] Problem loading: #{e.inspect}'
   $stderr.puts e.backtrace
