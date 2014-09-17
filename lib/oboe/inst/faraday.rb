6fd8f08d3bf2787b6b1410c17eb775077f2bf587
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-09-17 15:53:05 +0200

    Log exceptions + better logging mechanism

--- lib/oboe/inst/faraday.rb
+++ lib/oboe/inst/faraday.rb
@@ -7,54 +7,57 @@ module Oboe
 
       def run_request_with_oboe(method, url, body, headers)
         kvs = {}
+        kvs['Middleware'] = @builder.handlers
+        kvs['Backtrace'] = Oboe::API.backtrace if Oboe::Config[:faraday][:collect_backtraces]
+
+        # Only send service KVs if we're not using the Net::HTTP adapter
+        # Otherwise, the Net::HTTP instrumentation will send the service KVs
         handle_service = !@builder.handlers.include?(Faraday::Adapter::NetHttp)
 
-        Oboe::API.trace('faraday', kvs) do
-          kvs['Middleware'] = @builder.handlers
-          kvs['Backtrace'] = Oboe::API.backtrace if Oboe::Config[:faraday][:collect_backtraces]
+        if handle_service
+          blacklisted = Oboe::API.blacklisted?(@url_prefix.to_s)
+          context = Oboe::Context.toString
+          task_id = Oboe::XTrace.task_id(context)
 
-          # Only send service KVs if we're not using the Net::HTTP adapter
-          if handle_service
-            blacklisted = Oboe::API.blacklisted?(@url_prefix.to_s)
-            context = Oboe::Context.toString
-            task_id = Oboe::XTrace.task_id(context)
+          # Avoid cross host tracing for blacklisted domains
+          # Conditionally add the X-Trace header to the outgoing request
+          @headers['X-Trace'] = context unless blacklisted
 
-            # Avoid cross host tracing for blacklisted domains
-            # Conditionally add the X-Trace header to the outgoing request
-            @headers['X-Trace'] = context unless blacklisted
+          kvs['IsService'] = 1
+          kvs['RemoteProtocol'] = (@url_prefix.scheme == 'https') ? 'HTTPS' : 'HTTP'
+          kvs['RemoteHost'] = @url_prefix.host
+          kvs['RemotePort'] = @url_prefix.port
+          kvs['ServiceArg'] = url
+          kvs['HTTPMethod'] = method
+          kvs['Blacklisted'] = true if blacklisted
+        end
 
-            kvs['IsService'] = 1
-            kvs['RemoteProtocol'] = (@url_prefix.scheme == 'https') ? 'HTTPS' : 'HTTP'
-            kvs['RemoteHost'] = @url_prefix.host
-            kvs['RemotePort'] = @url_prefix.port
-            kvs['ServiceArg'] = url
-            kvs['HTTPMethod'] = method
-            kvs['Blacklisted'] = true if blacklisted
-          end
+        Oboe::API.log_entry('faraday', kvs)
+        result = run_request_without_oboe(method, url, body, headers)
 
-          result = run_request_without_oboe(method, url, body, headers)
+        # Re-attach net::http edge unless it's blacklisted or if we don't have a
+        # valid X-Trace header
+        if handle_service && !blacklisted
+          xtrace = result.headers['X-Trace']
 
-          # Re-attach net::http edge unless it's blacklisted or if we don't have a
-          # valid X-Trace header
-          if handle_service && !blacklisted
-            xtrace = result.headers['X-Trace']
+          if Oboe::XTrace.valid?(xtrace) && Oboe.tracing?
 
-            if Oboe::XTrace.valid?(xtrace) && Oboe.tracing?
-
-              # Assure that we received back a valid X-Trace with the same task_id
-              if task_id == Oboe::XTrace.task_id(xtrace)
-                Oboe::Context.fromString(xtrace)
-              else
-                Oboe.logger.debug "Mismatched returned X-Trace ID: #{xtrace}"
-              end
+            # Assure that we received back a valid X-Trace with the same task_id
+            if task_id == Oboe::XTrace.task_id(xtrace)
+              Oboe::Context.fromString(xtrace)
+            else
+              Oboe.logger.debug "Mismatched returned X-Trace ID: #{xtrace}"
             end
           end
-
-          kvs['HTTPStatus'] = result.status
-          result
         end
+
+        Oboe::API.log('faraday', 'info', :HTTPStatus => result.status) if handle_service
+        result
+      rescue => e
+        Oboe::API.log_exception('faraday', e)
+        raise e
       ensure
-        Oboe::API.log('faraday', 'info', kvs) if handle_service
+        Oboe::API.log_exit('faraday', kvs)
       end
     end
   end
