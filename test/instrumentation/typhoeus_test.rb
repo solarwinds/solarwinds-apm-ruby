208bdc06de5b9821db675f24122a2fc04de186dd
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-11-15 23:02:09 +0100

    Tests to validate hydra client tracing

--- test/instrumentation/typhoeus_test.rb
+++ test/instrumentation/typhoeus_test.rb
@@ -86,29 +86,23 @@ describe Oboe::Inst::TyphoeusRequestOps do
       second_request = Typhoeus::Request.new("www.appneta.com/products/")
       third_request  = Typhoeus::Request.new("www.curlmyip.com")
 
+      hydra.queue first_request
+      hydra.queue second_request
+      hydra.queue third_request
+
       hydra.run
     end
 
     traces = get_all_traces
-    traces.count.must_equal 5
+    traces.count.must_equal 4
 
     validate_outer_layers(traces, 'typhoeus_test')
 
-    traces[1]['Layer'].must_equal 'typhoeus'
-    traces[1].key?('Backtrace').must_equal Oboe::Config[:typhoeus][:collect_backtraces]
+    traces[1]['Layer'].must_equal 'typhoeus_hydra'
+    traces[1]['Label'].must_equal 'entry'
 
-    traces[2]['Layer'].must_equal 'typhoeus'
-    traces[2]['Label'].must_equal 'info'
-    traces[2]['IsService'].must_equal '1'
-    traces[2]['RemoteProtocol'].must_equal 'http'
-    traces[2]['RemoteHost'].must_equal 'www.appneta.com'
-    traces[2]['ServiceArg'].must_equal '/products/traceview/'
-    traces[2]['HTTPMethod'].must_equal 'get'
-    traces[2]['HTTPStatus'].must_equal '200'
-
-    traces[3]['Layer'].must_equal 'typhoeus'
-    traces[3]['Async'].must_equal 'true'
-    traces[3]['Label'].must_equal 'exit'
+    traces[2]['Layer'].must_equal 'typhoeus_hydra'
+    traces[2]['Label'].must_equal 'exit'
   end
 
   it 'should obey :collect_backtraces setting when true' do
