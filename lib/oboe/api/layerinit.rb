1754d75cfd52c8e8ef9ad049d6806e6dbcd0b226
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-11-25 17:04:39 +0100

    If already tracing, save and restore the context for __Init

--- lib/oboe/api/layerinit.rb
+++ lib/oboe/api/layerinit.rb
@@ -18,7 +18,17 @@ module Oboe
         return unless Oboe.loaded
 
         platform_info = Oboe::Util.build_init_report
+
+        # If already tracing, save and clear the context.  Restore it after
+        # the __Init is sent
+        if Oboe.tracing?
+          context = Oboe::Context.toString
+          Oboe::Context.clear
+        end
+
         start_trace(layer, nil, platform_info.merge('Force' => true)) {}
+
+        Oboe::Context.fromString(context) if context
       end
 
       ##
