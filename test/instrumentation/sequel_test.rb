800ea4426a33ec1484ac783ebc6ee648eb9bfa77
Peter Giacomo Lombardo <pglombardo@gmail.com>
2014-12-07 18:46:14 +0100

    More Sequel tests; updates

--- test/instrumentation/sequel_test.rb
+++ test/instrumentation/sequel_test.rb
@@ -1,6 +1,7 @@
 require 'minitest_helper'
 
 if ENV.key?('TRAVIS_PSQL_PASS')
+  debugger
   DB = Sequel.connect("postgres://postgres:#{ENV['TRAVIS_PSQL_PASS']}@127.0.0.1:5432/travis_ci_test")
 else
   DB = Sequel.connect('postgres://postgres@127.0.0.1:5432/travis_ci_test')
@@ -41,35 +42,14 @@ describe Oboe::Inst::Sequel do
   it 'sequel should have oboe methods defined' do
     #::Sequel::Database
     ::Sequel::Database.method_defined?(:extract_trace_details).must_equal true
-    ::Sequel::Database.method_defined?(:run_with_oboe).must_equal true
     ::Sequel::Database.method_defined?(:get_with_oboe).must_equal true
     ::Sequel::Database.method_defined?(:execute_dui_with_oboe).must_equal true
-  end
-
-  it 'should trace DB.run' do
-    Oboe::API.start_trace('sequel_test', '', {}) do
-      DB.run("select 1")
-    end
-
-    traces = get_all_traces
-    traces.count.must_equal 4
-    validate_outer_layers(traces, 'sequel_test')
-
-    validate_event_keys(traces[1], @entry_kvs)
-    traces[1]['Query'].must_equal "select 1"
-    traces[1].has_key?('Backtrace').must_equal Oboe::Config[:sequel][:collect_backtraces]
-    validate_event_keys(traces[2], @exit_kvs)
   end
 
   it "should obey :collect_backtraces setting when true" do
     Oboe::Config[:sequel][:collect_backtraces] = true
 
     Oboe::API.start_trace('sequel_test', '', {}) do
-      if ENV.key?('TRAVIS_PSQL_PASS')
-        DB = Sequel.connect("postgres://postgres:#{ENV['TRAVIS_PSQL_PASS']}@127.0.0.1:5432/travis_ci_test")
-      else
-        DB = Sequel.connect('postgres://postgres@127.0.0.1:5432/travis_ci_test')
-      end
       DB.run('select 1')
     end
 
@@ -81,15 +61,79 @@ describe Oboe::Inst::Sequel do
     Oboe::Config[:sequel][:collect_backtraces] = false
 
     Oboe::API.start_trace('sequel_test', '', {}) do
-      if ENV.key?('TRAVIS_PSQL_PASS')
-        DB = Sequel.connect("postgres://postgres:#{ENV['TRAVIS_PSQL_PASS']}@127.0.0.1:5432/travis_ci_test")
-      else
-        DB = Sequel.connect('postgres://postgres@127.0.0.1:5432/travis_ci_test')
-      end
       DB.run('select 1')
     end
 
     traces = get_all_traces
     layer_doesnt_have_key(traces, 'sequel', 'Backtrace')
   end
+
+  it 'should trace DB.run select' do
+    Oboe::API.start_trace('sequel_test', '', {}) do
+      DB.run("insert into items (name, price) values ('blah', '12')")
+    end
+
+    traces = get_all_traces
+
+    traces.count.must_equal 4
+    validate_outer_layers(traces, 'sequel_test')
+
+    validate_event_keys(traces[1], @entry_kvs)
+    traces[1]['Query'].must_equal "insert into items (name, price) values ('blah', '12')"
+    traces[1].has_key?('Backtrace').must_equal Oboe::Config[:sequel][:collect_backtraces]
+    validate_event_keys(traces[2], @exit_kvs)
+  end
+
+  it 'should trace DB.run insert' do
+    Oboe::API.start_trace('sequel_test', '', {}) do
+      DB.run("select 1")
+    end
+
+    traces = get_all_traces
+
+    traces.count.must_equal 4
+    validate_outer_layers(traces, 'sequel_test')
+
+    validate_event_keys(traces[1], @entry_kvs)
+    traces[1]['Query'].must_equal "select 1"
+    traces[1].has_key?('Backtrace').must_equal Oboe::Config[:sequel][:collect_backtraces]
+    validate_event_keys(traces[2], @exit_kvs)
+  end
+
+  it 'should trace a dataset insert' do
+    Oboe::API.start_trace('sequel_test', '', {}) do
+      items = DB[:items]
+      items.insert(:name => 'abc', :price => rand * 100)
+      x = items.count
+      puts x
+    end
+
+    traces = get_all_traces
+    debugger
+
+    traces.count.must_equal 4
+    validate_outer_layers(traces, 'sequel_test')
+
+    validate_event_keys(traces[1], @entry_kvs)
+    traces[1]['Query'].must_equal "select 1"
+    traces[1].has_key?('Backtrace').must_equal Oboe::Config[:sequel][:collect_backtraces]
+    validate_event_keys(traces[2], @exit_kvs)
+  end
+
+  it 'should capture and report exceptions' do
+    Oboe::API.start_trace('sequel_test', '', {}) do
+      DB.run("this is bad sql")
+    end
+
+    traces = get_all_traces
+
+    traces.count.must_equal 4
+    validate_outer_layers(traces, 'sequel_test')
+
+    validate_event_keys(traces[1], @entry_kvs)
+    traces[1]['Query'].must_equal "select 1"
+    traces[1].has_key?('Backtrace').must_equal Oboe::Config[:sequel][:collect_backtraces]
+    validate_event_keys(traces[2], @exit_kvs)
+  end
+
 end
