95219e78d60436a96acfe59b6bf332bf1f7b64f1
Peter Giacomo Lombardo <pglombardo@gmail.com>
2015-01-27 20:37:00 +0100

    Updated and extended tests to follow rack changes

--- test/frameworks/apps/grape_simple.rb
+++ test/frameworks/apps/grape_simple.rb
@@ -1,9 +1,20 @@
 require 'grape'
 
 class GrapeSimple < Grape::API
+  rescue_from :all do |e|
+    error_response({ message: "rescued from #{e.class.name}" })
+  end
 
   get '/json_endpoint' do
     present({ :test => true })
+  end
+
+  get "/break" do
+    raise Exception.new("This should have http status code 500!")
+  end
+
+  get "/breakstring" do
+    raise "This should have http status code 500!"
   end
 end
 
