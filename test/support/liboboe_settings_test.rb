6c51c73a0271fc65017636d43b61b19754e3e8a7
Peter Giacomo Lombardo <pglombardo@gmail.com>
2015-02-25 17:25:16 +0100

    Tests to validate ruby datatypes reporting support

--- test/support/liboboe_settings_test.rb
+++ test/support/liboboe_settings_test.rb
@@ -40,13 +40,62 @@ unless defined?(JRUBY_VERSION)
       kvs["SampleRate"] = 1000000
       kvs["SampleSource"] = OBOE_SAMPLE_RATE_SOURCE_FILE
       validate_event_keys(traces[0], kvs)
+    end
 
+    # Test logging of all Ruby datatypes against the SWIG wrapper
+    # of addInfo which only has four overloads.
+    def test_swig_datatypes_conversion
+      event = Oboe::Context.createEvent
+      report_kvs = {}
+
+      # Array
+      report_kvs[:TestData] = [0, 1, 2, 5, 7.0]
+      result = Oboe::API.log_event('test_layer', 'entry', event, report_kvs)
+
+      # Class
+      report_kvs[:TestData] = Minitest
+      result = Oboe::API.log_event('test_layer', 'entry', event, report_kvs)
+
+      # FalseClass
+      report_kvs[:TestData] = false
+      result = Oboe::API.log_event('test_layer', 'entry', event, report_kvs)
+
+      # Fixnum
+      report_kvs[:TestData] = 1_873_293_293
+      result = Oboe::API.log_event('test_layer', 'entry', event, report_kvs)
+
+      # Float
+      report_kvs[:TestData] = 1.0001
+      result = Oboe::API.log_event('test_layer', 'entry', event, report_kvs)
+
+      # Hash
+      report_kvs[:TestData] = Hash.new
+      result = Oboe::API.log_event('test_layer', 'entry', event, report_kvs)
+
+      # Integer
+      report_kvs[:TestData] = 1
+      result = Oboe::API.log_event('test_layer', 'entry', event, report_kvs)
+
+      # NilClass
+      report_kvs[:TestData] = nil
+      result = Oboe::API.log_event('test_layer', 'entry', event, report_kvs)
+
+      # Set
+      report_kvs[:TestData] = Set.new(1..10)
+      result = Oboe::API.log_event('test_layer', 'entry', event, report_kvs)
+
+      # String
+      report_kvs[:TestData] = 'test value'
+      result = Oboe::API.log_event('test_layer', 'entry', event, report_kvs)
+
+      # Symbol
+      report_kvs[:TestData] = :TestValue
+      result = Oboe::API.log_event('test_layer', 'entry', event, report_kvs)
+
+      # TrueClass
+      report_kvs[:TestData] = true
+      result = Oboe::API.log_event('test_layer', 'entry', event, report_kvs)
     end
   end
 
-  def validate_datatypes_support
-
-    Oboe::API.log_event('test_layer',
-  end
-  end
 end
