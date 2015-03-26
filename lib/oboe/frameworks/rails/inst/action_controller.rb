48247c35071fdafb7898538592663ed6d46452f7
Peter Giacomo Lombardo <pglombardo@gmail.com>
2015-03-26 18:11:36 +0100

    Basic code refactoring, documentation and clean up

--- lib/oboe/frameworks/rails/inst/action_controller.rb
+++ lib/oboe/frameworks/rails/inst/action_controller.rb
@@ -3,7 +3,75 @@
 
 module Oboe
   module Inst
-    module Rails3ActionController
+    #
+    # RailsBase
+    #
+    # This module contains the instrumentation code common to
+    # many Rails versions.
+    #
+    module RailsBase
+      #
+      # has_handler?
+      #
+      # Determins if <tt>exception</tt> has a registered
+      # handler via <tt>rescue_from</tt>
+      #
+      def has_handler?(exception)
+        # Don't log exceptions if they have a rescue handler set
+        has_handler = false
+        rescue_handlers.detect { | klass_name, handler |
+          # Rescue handlers can be specified as strings or constant names
+          klass = self.class.const_get(klass_name) rescue nil
+          klass ||= klass_name.constantize rescue nil
+          has_handler = exception.is_a?(klass) if klass
+        }
+        has_handler
+      end
+
+      #
+      # log_rails_error?
+      #
+      # Determins whether we should log a raised exception to the
+      # TraceView dashboard.  This is determined by whether the exception
+      # has a rescue handler setup and the value of
+      # Oboe::Config[:report_rescued_errors]
+      #
+      def log_rails_error?(exception)
+        has_handler = has_handler?(exception)
+
+        if !has_handler || (has_handler && Oboe::Config[:report_rescued_errors])
+          return true
+        end
+        false
+      end
+
+      #
+      # render_with_oboe
+      #
+      # Our render wrapper that just times and conditionally
+      # reports raised exceptions
+      #
+      def render_with_oboe(*args)
+        Oboe::API.log_entry('actionview')
+        render_without_oboe(*args)
+
+      rescue Exception => e
+        Oboe::API.log_exception(nil, e) if log_rails_error?(e)
+        raise
+      ensure
+        Oboe::API.log_exit('actionview')
+      end
+    end
+
+    #
+    # ActionController3
+    #
+    # This modules contains the instrumentation code specific
+    # to Rails v3
+    #
+    module ActionController3
+      include ::Oboe::Inst::RailsBase
+
       def self.included(base)
         base.class_eval do
           alias_method_chain :process, :oboe
@@ -31,15 +99,17 @@ module Oboe
         Oboe::API.log(nil, 'info', report_kvs)
         raise
       end
-
-      def render_with_oboe(*args)
-        Oboe::API.trace('actionview', {}) do
-          render_without_oboe *args
-        end
-      end
     end
 
-    module Rails4ActionController
+    #
+    # ActionController4
+    #
+    # This modules contains the instrumentation code specific
+    # to Rails v4
+    #
+    module ActionController4
+      include ::Oboe::Inst::RailsBase
+
       def self.included(base)
         base.class_eval do
           alias_method_chain :process_action, :oboe
@@ -59,25 +129,6 @@ module Oboe
           process_action_without_oboe(method_name, *args)
         end
       end
-
-      def render_with_oboe(*args)
-        Oboe::API.log_entry('actionview')
-        render_without_oboe(*args)
-
-      rescue StandardError => e
-        # Don't log exceptions if they have a rescue handler set
-        has_handler = false
-        rescue_handlers.detect { | klass_name, handler |
-          # Rescue handlers can be specified as strings or constant names
-          klass = self.class.const_get(klass_name) rescue nil
-          klass ||= klass_name.constantize rescue nil
-          has_handler = e.is_a?(klass) if klass
-        }
-        Oboe::API.log_exception(nil, e) unless has_handler
-        raise
-      ensure
-        Oboe::API.log_exit('actionview')
-      end
     end
   end
 end
@@ -86,13 +137,13 @@ if defined?(ActionController::Base) && Oboe::Config[:action_controller][:enabled
   if ::Rails::VERSION::MAJOR == 4
 
     class ActionController::Base
-      include Oboe::Inst::Rails4ActionController
+      include Oboe::Inst::ActionController4
     end
 
   elsif ::Rails::VERSION::MAJOR == 3
 
     class ActionController::Base
-      include Oboe::Inst::Rails3ActionController
+      include Oboe::Inst::ActionController3
     end
 
   elsif ::Rails::VERSION::MAJOR == 2
