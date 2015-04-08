bc8461b0e0bfd4952d5a946c9526b4f8c9db8adf
Peter Giacomo Lombardo <pglombardo@gmail.com>
2015-04-08 22:18:13 +0200

    Validate rest-client KVs in __Init

--- test/instrumentation/rest-client_test.rb
+++ test/instrumentation/rest-client_test.rb
@@ -21,6 +21,13 @@ if RUBY_VERSION >= '1.9.3'
       end
     end
 
+    it "should report rest-client version in __Init" do
+      init_kvs = ::Oboe::Util.build_init_report
+
+      init_kvs.key?('Ruby.RestClient.Version').must_equal true
+      init_kvs['Ruby.RestClient.Version'].must_equal "RestClient-#{::RestClient::VERSION}"
+    end
+
     it "should trace a request to an instr'd app" do
       response = nil
 
