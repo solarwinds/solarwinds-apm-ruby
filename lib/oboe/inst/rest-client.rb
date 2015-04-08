9c22c92830b304b6c41af529dac5855b460e3c77
Peter Giacomo Lombardo <pglombardo@gmail.com>
2015-04-08 20:36:32 +0200

    Assure KVs are captured in error conditions.  Refactor code.

--- lib/oboe/inst/rest-client.rb
+++ lib/oboe/inst/rest-client.rb
@@ -5,6 +5,35 @@ module Oboe
         ::Oboe::Util.method_alias(klass, :execute, ::RestClient::Request)
       end
 
+      ##
+      # oboe_collect
+      #
+      # Used to collect up KVs to be reported
+      # to the TraceView dashboard.
+      #
+      def oboe_collect
+        kvs = {}
+        uri = parse_url_with_auth(@url)
+
+        kvs['Backtrace'] = Oboe::API.backtrace if Oboe::Config[:rest_client][:collect_backtraces]
+        kvs['IsService'] = 1
+        kvs['RemoteProtocol'] = (uri.scheme == 'https') ? 'HTTPS' : 'HTTP'
+        kvs['RemoteHost'] = uri.host
+        kvs['RemotePort'] = uri.port
+        kvs['ServiceArg'] = uri.request_uri
+        kvs['HTTPMethod'] = @method.to_s.upcase
+        kvs
+      rescue => e
+        Oboe.logger.debug "[oboe/debug] Problem capturing rest-client KVs: #{e.message}"
+        Oboe.logger.debug e.backtrace if e.respond_to?(:backtrace) && Oboe::Config[:verbose]
+        return kvs
+      end
+
+      ##
+      # execute_with_oboe
+      #
+      # The wrapper method for RestClient::Request.execute
+      #
       def execute_with_oboe & block
         # If we're not tracing or if rest-client is doing
         # nested loops (following redirects), then just
@@ -13,58 +42,41 @@ module Oboe
           return execute_without_oboe(&block)
         end
 
-        Oboe::API.log_entry('rest-client')
-
-        blacklisted = Oboe::API.blacklisted?(@url)
-        context = Oboe::Context.toString
-        task_id = Oboe::XTrace.task_id(context)
-
-        # Avoid cross host tracing for blacklisted domains
-        # Conditionally add the X-Trace header to the outgoing request
-        @headers['X-Trace'] = context unless blacklisted
-
-        result = execute_without_oboe(&block)
-
-        kvs = {}
-        kvs[:HTTPStatus] = result.code
-        kvs['Backtrace'] = Oboe::API.backtrace if Oboe::Config[:rest_client][:collect_backtraces]
-
         begin
-          uri = parse_url_with_auth(@url)
+          Oboe::API.log_entry('rest-client')
 
-          kvs['IsService'] = 1
-          kvs['RemoteProtocol'] = (uri.scheme == 'https') ? 'HTTPS' : 'HTTP'
-          kvs['RemoteHost'] = uri.host
-          kvs['RemotePort'] = uri.port
-          kvs['ServiceArg'] = uri.request_uri
-          kvs['HTTPMethod'] = @method.to_s.upcase
-          kvs['Blacklisted'] = true if blacklisted
-        rescue => e
-          Oboe.logger.debug "[oboe/debug] Problem capturing rest-client KVs: #{e.message}"
-          Oboe.logger.debug e.backtrace if e.respond_to?(:backtrace) && Oboe::Config[:verbose]
-        end
+          blacklisted = Oboe::API.blacklisted?(@url)
+          start_xtrace = Oboe::Context.toString
 
-        # Re-attach net::http edge unless it's blacklisted or if we don't have a
-        # valid X-Trace header
-        unless blacklisted
-          xtrace = result.headers[:x_trace]
+          # Try to grab KVs as early as possible in case an exception
+          # is raised and we don't get a chance to.
+          kvs = oboe_collect
 
-          if Oboe::XTrace.valid?(xtrace) && Oboe.tracing?
-
-            # Assure that we received back a valid X-Trace with the same task_id
-            if task_id == Oboe::XTrace.task_id(xtrace)
-              Oboe::Context.fromString(xtrace)
-            else
-              Oboe.logger.debug "Mismatched returned X-Trace ID: #{xtrace}"
-            end
+          # Avoid cross host tracing for blacklisted domains
+          # Conditionally add the X-Trace header to the outgoing request
+          if blacklisted
+            kvs['Blacklisted'] = true
+          else
+            @headers['X-Trace'] = start_xtrace
           end
-        end
 
-        Oboe::API.log_exit('rest-client', kvs)
+          # The core rest-client call
+          result = execute_without_oboe(&block)
+
+          kvs[:HTTPStatus] = result.code
+
+          # Re-attach net::http edge unless it's blacklisted or if we don't have a
+          # valid X-Trace header.  pickup_context will validate values.
+          unless blacklisted
+            Oboe::XTrace.continue_service_context(start_xtrace, result.headers[:x_trace])
+          end
+        rescue => e
+          Oboe::API.log_exception('rest-client', e)
+          raise e
+        ensure
+          Oboe::API.log_exit('rest-client', kvs)
+        end
         result
-      rescue => e
-        Oboe::API.log_exception('rest-client', e)
-        raise e
       end
     end
   end
