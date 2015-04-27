1a32792c6f17d072729f8fa419f830d69d3a81c7
Peter Giacomo Lombardo <pglombardo@gmail.com>
2015-04-27 16:50:36 +0200

    Switch to Puma for background test stack

--- test/servers/rackapp_8101.rb
+++ test/servers/rackapp_8101.rb
@@ -1,17 +1,20 @@
 # Copyright (c) 2015 AppNeta, Inc.
 # All rights reserved.
 
+require 'rack/handler/puma'
+require 'oboe/inst/rack'
+
 Oboe.logger.info "[oboe/info] Starting background utility rack app on localhost:8101."
 
 Thread.new do
   app = Rack::Builder.new {
     use Oboe::Rack
     run Proc.new { |env|
-      [200, {"Content-Type" => "text/html"}, ['Hello, world!']]
+      [200, {"Content-Type" => "text/html"}, ['Hello TraceView!']]
     }
   }
 
-  Rack::Handler::Thin.run(app, :Port => 8101)
+  Rack::Handler::Puma.run(app, {:Host => '127.0.0.1', :Port => 8101})
 end
 
 # Allow Thin to boot.
