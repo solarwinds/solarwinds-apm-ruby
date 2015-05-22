73d363d7a94ac2dc37c9038baf31602fb2e74665
pglombardo <pglombardo@gmail.com>
2015-05-22 20:55:39 +0200

    Add backward compat support for OboeMethodProfiling

--- lib/oboe/backward_compatibility.rb
+++ lib/oboe/backward_compatibility.rb
@@ -1,7 +1,9 @@
 module Oboe
+  #
+  # Support for Oboe::API calls
+  #
   module API
     include TraceView::API
-
     def self.method_missing(sym, *args, &blk)
       TraceView.logger.warn "[traceview/warn] Note that Oboe::Config has been renamed to TraceView::Config. (#{sym}:#{args})"
       TraceView::API.send(sym, *args, &blk)
@@ -9,11 +11,28 @@ module Oboe
   end
 
 
+  #
+  # Support for Oboe::Config calls
+  #
   module Config
     def self.method_missing(sym, *args)
       TraceView.logger.warn "[traceview/warn] Note that Oboe::Config has been renamed to TraceView::Config. (#{sym}:#{args})"
       TraceView::Config.send(sym, *args)
     end
+  end
+end
+
+
+#
+# Support for OboeMethodProfiling
+#
+module OboeMethodProfiling
+  def self.included(klass)
+    klass.extend ClassMethods
+  end
+
+  module ClassMethods
+    include TraceViewMethodProfiling::ClassMethods
   end
 end
 
