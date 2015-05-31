ef5bf3d61d27ea6a83ce59c9be8f2e60e707ea04
pglombardo <pglombardo@gmail.com>
2015-05-31 20:32:26 +0200

    Add Oboe::Ruby to the backcompat support

--- lib/oboe/backward_compatibility.rb
+++ lib/oboe/backward_compatibility.rb
@@ -17,6 +17,7 @@ module Oboe
       unless @deprecated_notified
         TraceView.logger.warn "[traceview/warn] Note that Oboe::API has been renamed to TraceView::API. (#{sym}:#{args})"
         TraceView.logger.warn "[traceview/warn] Oboe::API will be deprecated in a future version."
+        TraceView.logger.warn "[traceview/warn] File:Line: #{Kernel.caller[0]}"
         @deprecated_notified = true
       end
       TraceView::API.send(sym, *args, &blk)
@@ -36,9 +37,29 @@ module Oboe
       unless @deprecated_notified
         TraceView.logger.warn "[traceview/warn] Note that Oboe::Config has been renamed to TraceView::Config. (#{sym}:#{args})"
         TraceView.logger.warn "[traceview/warn] Oboe::Config will be deprecated in a future version."
+        TraceView.logger.warn "[traceview/warn] File:Line: #{Kernel.caller[0]}"
         @deprecated_notified = true
       end
       TraceView::Config.send(sym, *args)
+    end
+  end
+
+  #
+  # Support for legacy Oboe::Ruby.load calls
+  #
+  module Ruby
+    extend ::TraceView::ThreadLocal
+    thread_local :deprecation_notified
+
+    def self.method_missing(sym, *args)
+      # Notify of deprecation only once
+      unless @deprecated_notified
+        TraceView.logger.warn "[traceview/warn] Note that Oboe::Ruby has been renamed to TraceView::Ruby. (#{sym}:#{args})"
+        TraceView.logger.warn "[traceview/warn] Oboe::Ruby will be deprecated in a future version."
+        TraceView.logger.warn "[traceview/warn] File:Line: #{Kernel.caller[0]}"
+        @deprecated_notified = true
+      end
+      TraceView::Ruby.send(sym, *args)
     end
   end
 end
