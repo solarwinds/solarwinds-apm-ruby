e7d4efefadd54b60b89bd29fe163ea5dc3ab4d11
pglombardo <pglombardo@gmail.com>
2015-06-03 17:23:05 +0200

    Have Sinatra tests validate legacy and new RUM helpers

--- test/frameworks/apps/sinatra_simple.rb
+++ test/frameworks/apps/sinatra_simple.rb
@@ -3,6 +3,19 @@ require 'sinatra'
 class SinatraSimple < Sinatra::Base
   set :reload, true
 
+  template :layout do
+    # Use both the legacy and new RUM helper
+    # oboe_rum_header + traceview_rum_footer
+    %q{
+<html>
+  <head><%= oboe_rum_header %></head>
+  <body>
+    <%= yield %>
+    <%= traceview_rum_footer %>
+  </body>
+</html>}
+  end
+
   get "/" do
     'The magick number is: 2767356926488785838763860464013972991031534522105386787489885890443740254365!' # Change only the number!!!
   end
