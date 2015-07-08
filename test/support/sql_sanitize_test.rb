01a948560ff0703b58cca7eeaf0f4e7967206d29
pglombardo <pglombardo@gmail.com>
2015-07-08 22:22:18 +0200

    Don't use setup/teardown as they seem to break in parallel.

--- test/support/sql_sanitize_test.rb
+++ test/support/sql_sanitize_test.rb
@@ -4,20 +4,14 @@
 require 'minitest_helper'
 
 class SQLSanitizeTest < Minitest::Test
-  def setup
-    @sanitize_sql = TraceView::Config[:sanitize_sql]
-  end
-
-  def teardown
-    TraceView::Config[:sanitize_sql] = @sanitize_sql
-  end
-
   def test_sanitize_sql1
     TraceView::Config[:sanitize_sql] = true
 
     sql = "INSERT INTO `queries` (`asdf_id`, `asdf_prices`, `created_at`, `updated_at`, `blue_pill`, `yearly_tax`, `rate`, `steam_id`, `red_pill`, `dimitri`, `origin`) VALUES (19231, 3, 'cat', 'dog', 111.0, 126.0, 116.0, 79.0, 72.0, 73.0, ?, 1, 3, 229.284, ?, ?, 100, ?, 0, 3, 1, ?, NULL, NULL, ?, 4, ?)"
     result = TraceView::Util.sanitize_sql(sql)
     result.must_equal "INSERT INTO `queries` (`asdf_id`, `asdf_prices`, `created_at`, `updated_at`, `blue_pill`, `yearly_tax`, `rate`, `steam_id`, `red_pill`, `dimitri`, `origin`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+
+    TraceView::Config[:sanitize_sql] = false
   end
 
   def test_sanitize_sql2
@@ -26,6 +20,8 @@ class SQLSanitizeTest < Minitest::Test
     sql = "SELECT \"game_types\".* FROM \"game_types\" WHERE \"game_types\".\"game_id\" IN (1162)"
     result = TraceView::Util.sanitize_sql(sql)
     result.must_equal "SELECT \"game_types\".* FROM \"game_types\" WHERE \"game_types\".\"game_id\" IN (?)"
+
+    TraceView::Config[:sanitize_sql] = false
   end
 
   def test_sanitize_sql3
@@ -34,6 +30,8 @@ class SQLSanitizeTest < Minitest::Test
     sql = "SELECT \"comments\".* FROM \"comments\" WHERE \"comments\".\"commentable_id\" = 2798 AND \"comments\".\"commentable_type\" = 'Video' AND \"comments\".\"parent_id\" IS NULL ORDER BY comments.created_at DESC"
     result = TraceView::Util.sanitize_sql(sql)
     result.must_equal "SELECT \"comments\".* FROM \"comments\" WHERE \"comments\".\"commentable_id\" = ? AND \"comments\".\"commentable_type\" = ? AND \"comments\".\"parent_id\" IS ? ORDER BY comments.created_at DESC"
+
+    TraceView::Config[:sanitize_sql] = false
   end
 
   def test_sanitize_sql4
@@ -42,6 +40,8 @@ class SQLSanitizeTest < Minitest::Test
     sql = "SELECT `assets`.* FROM `assets` WHERE `assets`.`type` IN ('Picture') AND (updated_at >= '2015-07-08 19:22:00') AND (updated_at <= '2015-07-08 19:23:00') LIMIT 31 OFFSET 0"
     result = TraceView::Util.sanitize_sql(sql)
     result.must_equal "SELECT `assets`.* FROM `assets` WHERE `assets`.`type` IN (?) AND (updated_at >= ?) AND (updated_at <= ?) LIMIT ? OFFSET ?"
+
+    TraceView::Config[:sanitize_sql] = false
   end
 
   def test_dont_sanitize
