97c33f066b122a8099c60b79ff1c7744298cbceb
pglombardo <pglombardo@gmail.com>
2015-07-13 18:17:11 +0200

    Bunny instrumentation: publish + wait_for_confirms

---
+++ lib/traceview/inst/bunny.rb
@@ -0,0 +1,68 @@
+# Copyright (c) 2015 AppNeta, Inc.
+# All rights reserved.
+
+module TraceView
+  module Inst
+    module BunnyExchange
+      def self.included(klass)
+        ::TraceView::Util.method_alias(klass, :publish, ::Bunny::Exchange)
+      end
+
+      def publish_with_traceview(payload, opts = {})
+        # If we're not tracing, just do a fast return.
+        return publish_without_traceview(payload, opts) if !TraceView.tracing?
+
+        begin
+          kvs = {}
+          kvs[:ExchangeAction] = :publish
+          kvs[:RemoteHost] = channel.connection.host
+          kvs[:RemotePort] = channel.connection.port
+          kvs[:VirtualHost] = channel.connection.vhost
+
+          TraceView::API.log_entry('rabbitmq')
+
+          publish_without_traceview(payload, opts)
+        rescue => e
+          TraceView::API.log_exception(nil, e)
+          raise e
+        ensure
+          TraceView::API.log_exit('rabbitmq', kvs)
+        end
+      end
+    end
+
+    module BunnyChannel
+      def self.included(klass)
+        ::TraceView::Util.method_alias(klass, :wait_for_confirms, ::Bunny::Channel)
+      end
+
+      def wait_for_confirms_with_traceview
+        # If we're not tracing, just do a fast return.
+        return wait_for_confirms_without_traceview if !TraceView.tracing?
+
+        begin
+          kvs = {}
+          kvs[:ExchangeAction] = :wait_for_confirms
+          kvs[:RemoteHost] = @connection.host
+          kvs[:RemotePort] = @connection.port
+          kvs[:VirtualHost] = @connection.vhost
+
+          TraceView::API.log_entry('rabbitmq')
+
+          wait_for_confirms_without_traceview
+        rescue => e
+          TraceView::API.log_exception(nil, e)
+          raise e
+        ensure
+          TraceView::API.log_exit('rabbitmq', kvs)
+        end
+      end
+    end
+  end
+end
+
+if TraceView::Config[:bunny][:enabled] && defined?(::Bunny)
+  ::TraceView.logger.info '[traceview/loading] Instrumenting bunny' if TraceView::Config[:verbose]
+  ::TraceView::Util.send_include(::Bunny::Exchange, ::TraceView::Inst::BunnyExchange)
+  ::TraceView::Util.send_include(::Bunny::Channel, ::TraceView::Inst::BunnyChannel)
+end
