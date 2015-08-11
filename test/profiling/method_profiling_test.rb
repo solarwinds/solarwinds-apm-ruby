28a53be89807e3c9bc7dd70616434794d5590715
pglombardo <pglombardo@gmail.com>
2015-08-12 00:05:44 +0200

    Add tests to validate default and optional arg/result reporting.

--- test/profiling/method_profiling_test.rb
+++ test/profiling/method_profiling_test.rb
@@ -64,9 +64,9 @@ describe "TraceViewMethodProfiling" do
 
     validate_event_keys(traces[1], kvs)
 
-    traces[1].has_key?("Layer").must_equal false
-    traces[1].has_key?("File").must_equal true
-    traces[1].has_key?("LineNumber").must_equal true
+    traces[1].key?("Layer").must_equal false
+    traces[1].key?("File").must_equal true
+    traces[1].key?("LineNumber").must_equal true
 
     kvs.clear
     kvs["Label"] = "profile_exit"
@@ -74,7 +74,7 @@ describe "TraceViewMethodProfiling" do
     kvs["ProfileName"] = "do_work"
 
     validate_event_keys(traces[2], kvs)
-    traces[2].has_key?("Layer").must_equal false
+    traces[2].key?("Layer").must_equal false
   end
 
   it 'should not double profile already profiled methods' do
@@ -144,9 +144,9 @@ describe "TraceViewMethodProfiling" do
 
     validate_event_keys(traces[1], kvs)
 
-    traces[1].has_key?("Layer").must_equal false
-    traces[1].has_key?("File").must_equal true
-    traces[1].has_key?("LineNumber").must_equal true
+    traces[1].key?("Layer").must_equal false
+    traces[1].key?("File").must_equal true
+    traces[1].key?("LineNumber").must_equal true
 
     kvs.clear
     kvs["Label"] = "profile_exit"
@@ -154,7 +154,7 @@ describe "TraceViewMethodProfiling" do
     kvs["ProfileName"] = "do_work"
 
     validate_event_keys(traces[2], kvs)
-    traces[2].has_key?("Layer").must_equal false
+    traces[2].key?("Layer").must_equal false
   end
 
   it 'should trace class private instance methods' do
@@ -190,9 +190,9 @@ describe "TraceViewMethodProfiling" do
 
     validate_event_keys(traces[1], kvs)
 
-    traces[1].has_key?("Layer").must_equal false
-    traces[1].has_key?("File").must_equal true
-    traces[1].has_key?("LineNumber").must_equal true
+    traces[1].key?("Layer").must_equal false
+    traces[1].key?("File").must_equal true
+    traces[1].key?("LineNumber").must_equal true
 
     kvs.clear
     kvs["Label"] = "profile_exit"
@@ -200,7 +200,7 @@ describe "TraceViewMethodProfiling" do
     kvs["ProfileName"] = "do_work"
 
     validate_event_keys(traces[2], kvs)
-    traces[2].has_key?("Layer").must_equal false
+    traces[2].key?("Layer").must_equal false
   end
 
   it 'should trace class private singleton methods' do
@@ -233,9 +233,9 @@ describe "TraceViewMethodProfiling" do
 
     validate_event_keys(traces[1], kvs)
 
-    traces[1].has_key?("Layer").must_equal false
-    traces[1].has_key?("File").must_equal true
-    traces[1].has_key?("LineNumber").must_equal true
+    traces[1].key?("Layer").must_equal false
+    traces[1].key?("File").must_equal true
+    traces[1].key?("LineNumber").must_equal true
 
     kvs.clear
     kvs["Label"] = "profile_exit"
@@ -243,7 +243,7 @@ describe "TraceViewMethodProfiling" do
     kvs["ProfileName"] = "do_work"
 
     validate_event_keys(traces[2], kvs)
-    traces[2].has_key?("Layer").must_equal false
+    traces[2].key?("Layer").must_equal false
   end
 
   it 'should trace module singleton methods' do
@@ -276,9 +276,9 @@ describe "TraceViewMethodProfiling" do
 
     validate_event_keys(traces[1], kvs)
 
-    traces[1].has_key?("Layer").must_equal false
-    traces[1].has_key?("File").must_equal true
-    traces[1].has_key?("LineNumber").must_equal true
+    traces[1].key?("Layer").must_equal false
+    traces[1].key?("File").must_equal true
+    traces[1].key?("LineNumber").must_equal true
 
     kvs.clear
     kvs["Label"] = "profile_exit"
@@ -286,7 +286,7 @@ describe "TraceViewMethodProfiling" do
     kvs["ProfileName"] = "do_work"
 
     validate_event_keys(traces[2], kvs)
-    traces[2].has_key?("Layer").must_equal false
+    traces[2].key?("Layer").must_equal false
   end
 
   it 'should trace module instance methods' do
@@ -324,9 +324,9 @@ describe "TraceViewMethodProfiling" do
 
     validate_event_keys(traces[1], kvs)
 
-    traces[1].has_key?("Layer").must_equal false
-    traces[1].has_key?("File").must_equal true
-    traces[1].has_key?("LineNumber").must_equal true
+    traces[1].key?("Layer").must_equal false
+    traces[1].key?("File").must_equal true
+    traces[1].key?("LineNumber").must_equal true
 
     kvs.clear
     kvs["Label"] = "profile_exit"
@@ -334,62 +334,113 @@ describe "TraceViewMethodProfiling" do
     kvs["ProfileName"] = "do_work"
 
     validate_event_keys(traces[2], kvs)
-    traces[2].has_key?("Layer").must_equal false
+    traces[2].key?("Layer").must_equal false
+  end
+
+  it 'should profile methods that use blocks' do
+    skip
+  end
+
+  it 'should profile methods with various argument types' do
+    skip
   end
 
   it 'should not store arguments and return value by default' do
-    skip
-    class TestWorker
-      def self.do_work(s, i, a, h)
-        sleep 1
-        return "the zebra is loose"
-      end
-
-      class << self
-        include TraceViewMethodProfiling
-        # Default call method
-        profile_method :do_work, 'do_work'
+    class TestKlass
+      def do_work(blah = {})
+        return 687
       end
     end
 
+    result = TraceView::API.profile_method(TestKlass, :do_work)
+    assert_equal true, result, "profile_method return value must be true"
+
+    result = nil
+
     ::TraceView::API.start_trace('method_profiling', '', {}) do
       # Call the profiled class method
-      TestWorker.do_work('String Argument', 203984, ["1", "2", 3], { :color => :black })
+      result = TestKlass.new.do_work(:ok => :blue)
     end
 
     traces = get_all_traces
     traces.count.must_equal 4
 
-    traces[1].has_key?("Args").must_equal false
-    traces[2].has_key?("ReturnValue").must_equal false
+    validate_outer_layers(traces, 'method_profiling')
+
+    result.must_equal 687
+
+    kvs = {}
+    kvs["Label"] = 'profile_entry'
+    kvs["Language"] = "ruby"
+    kvs["ProfileName"] = "do_work"
+    kvs["Class"] = "TestKlass"
+
+    validate_event_keys(traces[1], kvs)
+
+    traces[1].key?("Layer").must_equal false
+    traces[1].key?("File").must_equal true
+    traces[1].key?("LineNumber").must_equal true
+
+    kvs.clear
+    kvs["Label"] = "profile_exit"
+    kvs["Language"] = "ruby"
+    kvs["ProfileName"] = "do_work"
+
+    validate_event_keys(traces[2], kvs)
+    traces[2].key?("Layer").must_equal false
+
+    traces[2].key?("Arguments").must_equal false
+    traces[2].key?("ReturnValue").must_equal false
   end
 
   it 'should store arguments and return value when asked' do
-    skip
-    class TestWorker
-      def self.do_work(s, i, a, h)
-        sleep 1
-        return "the zebra is loose"
-      end
-
-      class << self
-        include TraceViewMethodProfiling
-        profile_method :do_work, 'do_work', true, true
+    class TestKlass
+      def do_work(blah = {})
+        return 687
       end
     end
 
+    result = TraceView::API.profile_method(TestKlass, :do_work, true, true)
+    assert_equal true, result, "profile_method return value must be true"
+
+    result = nil
+
     ::TraceView::API.start_trace('method_profiling', '', {}) do
       # Call the profiled class method
-      TestWorker.do_work('String Argument', 203984, ["1", "2", 3], { :color => :black })
+      result = TestKlass.new.do_work(:ok => :blue)
     end
 
     traces = get_all_traces
     traces.count.must_equal 4
 
-    traces[1].has_key?("Args").must_equal true
-    traces[1]["Args"].must_equal "\"String Argument\"\n203984\n[\"1\", \"2\", 3]\n{:color=>:black}\n"
+    validate_outer_layers(traces, 'method_profiling')
 
-    traces[2].has_key?("ReturnValue").must_equal true
-    traces[2]["ReturnValue"].must_equal "\"the zebra is loose\"\n"
+    result.must_equal 687
+
+    kvs = {}
+    kvs["Label"] = 'profile_entry'
+    kvs["Language"] = "ruby"
+    kvs["ProfileName"] = "do_work"
+    kvs["Class"] = "TestKlass"
+
+    validate_event_keys(traces[1], kvs)
+
+    traces[1].key?("Layer").must_equal false
+    traces[1].key?("File").must_equal true
+    traces[1].key?("LineNumber").must_equal true
+
+    kvs.clear
+    kvs["Label"] = "profile_exit"
+    kvs["Language"] = "ruby"
+    kvs["ProfileName"] = "do_work"
+
+    validate_event_keys(traces[2], kvs)
+    traces[2].key?("Layer").must_equal false
+
+    traces[2].key?("Arguments").must_equal true
+    traces[2]["Arguments"].must_equal "[{:ok=>:blue}]"
+
+    traces[2].key?("ReturnValue").must_equal true
+    traces[2]["ReturnValue"].must_equal 687
   end
 end
