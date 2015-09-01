e537c3f9ce810315ae77630b256f4ceef95537e3
pglombardo <pglombardo@gmail.com>
2015-09-01 23:01:19 +0200

    Test background jobs

---
+++ test/jobs/remote_call_worker_job.rb
@@ -0,0 +1,14 @@
+class RemoteCallWorkerJob
+  include Sidekiq::Worker
+
+  def perform(*args)
+    # Make some random Dalli (memcache) calls and top it
+    # off with an excon call to the background rack webserver.
+    @dc = Dalli::Client.new
+    @dc.get(rand(10).to_s)
+    Excon.get('http://127.0.0.1:8101/')
+    @dc.get(rand(10).to_s)
+    @dc.get(rand(10).to_s)
+    @dc.get_multi([:one, :two, :three, :four, :five, :six])
+  end
+end
