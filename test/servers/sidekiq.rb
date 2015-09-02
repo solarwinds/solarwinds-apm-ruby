1ddb35f2ffe30cfdd045be7aeb9481983701d288
pglombardo <pglombardo@gmail.com>
2015-09-02 22:35:54 +0200

    Boot sidekiq via system in a background thread.

--- test/servers/sidekiq.rb
+++ test/servers/sidekiq.rb
@@ -2,22 +2,30 @@ require 'sidekiq/cli'
 
 TraceView.logger.info "[traceview/servers] Starting up background Sidekiq."
 
-Thread.new do
-  cli = ::Sidekiq::CLI.instance
-  options = []
-  options << ["-r", Dir.pwd + "/test/jobs/db_worker_job.rb"]
-  options << ["-r", Dir.pwd + "/test/jobs/remote_call_worker_job.rb"]
-  options << ["-c", "2"]
-  options << ["-q", "critical,2", "-q", "default"]
-  options << ["-P", "/tmp/sidekiq_#{Process.pid}..pid"]
-  cli.parse(options.flatten)
+options = []
+arguments = ""
+options << ["-r", Dir.pwd + "/test/jobs/job_initializer.rb"]
+options << ["-c", "2"]
+options << ["-q", "critical,2", "-q", "default"]
+options << ["-P", "/tmp/sidekiq_#{Process.pid}.pid"]
 
-  ::Sidekiq.configure_server do |config|
-    config.server_middleware do |chain|
-      ::TraceView.logger.info '[traceview/loading] Adding Sidekiq worker middleware' if TraceView::Config[:verbose]
-      chain.add ::TraceView::SidekiqWorker
-    end
-  end
-
-  cli.run
+options.flatten.each do |x|
+  arguments += " #{x}"
 end
+
+TraceView.logger.debug "[traceview/servers] sidekiq #{arguments}"
+
+# Boot Sidekiq in a new thread
+Thread.new do
+  system("OBOE_GEM_TEST=true sidekiq #{arguments}")
+end
+
+# Allow Sidekiq to boot up
+sleep 2
+
+# Add a hook to shutdown sidekiq after Minitest finished running
+Minitest.after_run {
+  TraceView.logger.warn "[traceview/servers] Shutting down Sidekiq. (pid: #{@sidekiq_pid})"
+  pid = File.read("/tmp/sidekiq_#{Process.pid}.pid").chomp
+  Process.kill(:TERM, pid.to_i)
+}
