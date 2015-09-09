dc7457f5431a401b37e8b29236febd158337a197
pglombardo <pglombardo@gmail.com>
2015-09-09 22:32:25 +0200

    Add Job Spec KVs to instrumentation

--- lib/traceview/inst/sidekiq.rb
+++ lib/traceview/inst/sidekiq.rb
@@ -6,30 +6,23 @@ module TraceView
         # keep calm and instrument on.
 
         report_kvs = {}
+        _, msg, queue = args
+
         report_kvs['Backtrace'] = TV::API.backtrace if TV::Config[:sidekiq][:collect_backtraces]
-        report_kvs[:Op] = :perform
+
+        # Background Job Spec KVs
+        report_kvs[:Spec] = :job
+        report_kvs[:JobName] = msg['class']
+        report_kvs[:JobID] = msg['jid']
+        report_kvs[:Source] = msg['queue']
+        report_kvs[:Args] = msg['args'].to_s if TraceView::Config[:sidekiq][:log_args]
+
+        # Webserver Spec KVs
         report_kvs['HTTP-Host'] = Socket.gethostname
         report_kvs[:Method] = 'Worker'
-
-        if args.is_a?(Array) && args.count == 3
-          report_kvs['Args'] = args.to_s if TV::Config[:sidekiq][:log_args] && !args.empty?
-          report_kvs[:Controller] = "Sidekiq_#{args[2]}"
-          report_kvs[:Action] = args[1]['class'].to_s
-
-          report_kvs[:URL] = "/sidekiq/#{args[2]}/#{args[1]['class'].to_s}"
-          report_kvs[:Queue] = args[2].to_s
-
-          if TraceView::Config[:sidekiq][:log_args]
-            kv_args = args[1]['args'].to_s
-
-            # Limit the argument json string to 1024 bytes
-            if kv_args.length > 1024
-              report_kvs[:Args] = kv_args[0..1023] + '...[snipped]'
-            else
-              report_kvs[:Args] = kv_args
-            end
-          end
-        end
+        report_kvs[:Controller] = "Sidekiq_#{queue}"
+        report_kvs[:Action] = msg['class']
+        report_kvs[:URL] = "/sidekiq/#{args[2]}/#{args[1]['class'].to_s}"
       rescue => e
         TraceView.logger.warn "[traceview/sidekiq] Non-fatal error capturing KVs: #{e.message}"
       end
