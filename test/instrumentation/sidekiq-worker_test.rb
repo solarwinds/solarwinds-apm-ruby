b15924c2f31ddfe7b6eecd64ca4004691dcc3c77
pglombardo <pglombardo@gmail.com>
2015-09-09 23:48:23 +0200

    Validate all SRv1 KVs

--- test/instrumentation/sidekiq-worker_test.rb
+++ test/instrumentation/sidekiq-worker_test.rb
@@ -39,8 +39,9 @@ if RUBY_VERSION >= '2.0'
       valid_edges?(traces)
 
       # Validate entry layer KVs
-      assert_equal traces[0].key?('SampleRate'), true
-      assert_equal traces[0].key?('SampleSource'), true
+      assert_equal traces[0]['TraceOrigin'], 'always_sampled', "is always_sampled"
+      assert_equal traces[0].key?('SampleRate'), true, "has SampleRate KV"
+      assert_equal traces[0].key?('SampleSource'), true, "has SampleSource KV"
 
       # Validate Webserver Spec KVs
       assert_equal Socket.gethostname, traces[0]['HTTP-Host']
