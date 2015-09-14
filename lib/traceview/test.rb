8c7b75b7055e051c43017dfed46acf20f0a7257b
pglombardo <pglombardo@gmail.com>
2015-09-14 23:17:45 +0200

    Only conditionally load test jobs (if sidekiq or resque loaded)

--- lib/traceview/test.rb
+++ lib/traceview/test.rb
@@ -5,11 +5,14 @@ module TraceView
   module Test
     class << self
       def load_extras
-        # Load all of the test workers
-        pattern = File.join(File.dirname(__FILE__), '../../test/jobs/', '*.rb')
-        Dir.glob(pattern) do |f|
-          TV.logger.debug "Loading test job file: #{f}"
-          require f
+        # If we're using the libraries gemfile (with sidekiq and resque)
+        if File.basename(ENV['BUNDLE_GEMFILE']) =~ /libraries/
+          # Load all of the test workers
+          pattern = File.join(File.dirname(__FILE__), '../../test/jobs/', '*.rb')
+          Dir.glob(pattern) do |f|
+            TV.logger.debug "Loading test job file: #{f}"
+            require f
+          end
         end
       end
     end
