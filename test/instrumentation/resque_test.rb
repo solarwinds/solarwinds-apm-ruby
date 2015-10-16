391e16ba62625815f9da15aaba51bc09d5f7f132
pglombardo <pglombardo@gmail.com>
2015-10-16 21:16:34 +0200

    Add log_args and backtrace validation tests.

--- test/instrumentation/resque_test.rb
+++ test/instrumentation/resque_test.rb
@@ -72,79 +72,84 @@ if RUBY_VERSION >= '1.9.3'
     end
 
     def test_obey_collect_backtraces_when_false
-      skip
       TraceView::Config[:resqueclient][:collect_backtraces] = false
 
       # Queue up a job to be run
-      ::TraceView::API.start_trace(:enqueue_test) do
-        resque::Client.push('queue' => 'critical', 'class' => ::RemoteCallWorkerJob, 'args' => [1, 2, 3], 'retry' => false)
+      ::TraceView::API.start_trace('resque-client_test') do
         Resque.enqueue(ResqueRemoteCallWorkerJob, [1, 2, 3])
       end
 
-      # Allow the job to be run
-      sleep 5
-
       traces = get_all_traces
-      assert_equal 23, traces.count, "Trace count"
-      valid_edges?(traces)
-      assert_equal 'resque-client',   traces[1]['Layer']
-      assert_equal false,              traces[1].key?('Backtrace')
+      assert_equal 6, traces.count, "trace count"
+      validate_outer_layers(traces, 'resque-client_test')
+
+      assert_equal false,           traces[1].key?('Backtrace')
+
+      assert_equal "resque-client", traces[1]['Layer'], "entry event layer name"
+      assert_equal "entry",         traces[1]['Label'], "entry event label"
+      assert_equal "resque-client", traces[4]['Layer'], "exit event layer name"
+      assert_equal "exit",         traces[4]['Label'], "exit event label"
     end
 
     def test_obey_collect_backtraces_when_true
-      skip
       TraceView::Config[:resqueclient][:collect_backtraces] = true
 
       # Queue up a job to be run
-      ::TraceView::API.start_trace(:enqueue_test) do
+      ::TraceView::API.start_trace('resque-client_test') do
         Resque.enqueue(ResqueRemoteCallWorkerJob, [1, 2, 3])
       end
 
-      # Allow the job to be run
-      sleep 5
-
       traces = get_all_traces
-      assert_equal 23, traces.count, "Trace count"
-      valid_edges?(traces)
-      assert_equal 'resque-client',   traces[1]['Layer']
-      assert_equal true,               traces[1].key?('Backtrace')
+      assert_equal 6, traces.count, "trace count"
+      validate_outer_layers(traces, 'resque-client_test')
+
+      assert_equal true,           traces[1].key?('Backtrace')
+
+      assert_equal "resque-client", traces[1]['Layer'], "entry event layer name"
+      assert_equal "entry",         traces[1]['Label'], "entry event label"
+      assert_equal "resque-client", traces[4]['Layer'], "exit event layer name"
+      assert_equal "exit",         traces[4]['Label'], "exit event label"
     end
 
     def test_obey_log_args_when_false
-      skip
       TraceView::Config[:resqueclient][:log_args] = false
 
       # Queue up a job to be run
-      ::TraceView::API.start_trace(:enqueue_test) do
-        Resque::Client.push('queue' => 'critical', 'class' => ::RemoteCallWorkerJob, 'args' => [1, 2, 3], 'retry' => false)
+      ::TraceView::API.start_trace('resque-client_test') do
+        Resque.enqueue(ResqueRemoteCallWorkerJob, [1, 2, 3])
       end
 
-      # Allow the job to be run
-      sleep 5
-
       traces = get_all_traces
-      assert_equal 23, traces.count, "Trace count"
-      valid_edges?(traces)
-      assert_equal false, traces[1].key?('Args')
+      assert_equal 6, traces.count, "trace count"
+      validate_outer_layers(traces, 'resque-client_test')
+
+      assert_equal false,           traces[1].key?('Args')
+
+      assert_equal "resque-client", traces[1]['Layer'], "entry event layer name"
+      assert_equal "entry",         traces[1]['Label'], "entry event label"
+      assert_equal "resque-client", traces[4]['Layer'], "exit event layer name"
+      assert_equal "exit",          traces[4]['Label'], "exit event label"
     end
 
     def test_obey_log_args_when_true
-      skip
       TraceView::Config[:resqueclient][:log_args] = true
 
       # Queue up a job to be run
-      ::TraceView::API.start_trace(:enqueue_test) do
-        Resque::Client.push('queue' => 'critical', 'class' => ::RemoteCallWorkerJob, 'args' => [1, 2, 3], 'retry' => false)
+      ::TraceView::API.start_trace('resque-client_test') do
+        Resque.enqueue(ResqueRemoteCallWorkerJob, 1, 2, 3)
       end
 
-      # Allow the job to be run
-      sleep 5
-
       traces = get_all_traces
-      assert_equal 23, traces.count, "Trace count"
-      valid_edges?(traces)
-      assert_equal true,         traces[1].key?('Args')
-      assert_equal '[1, 2, 3]',  traces[1]['Args']
+      assert_equal 6, traces.count, "trace count"
+      validate_outer_layers(traces, 'resque-client_test')
+
+      assert_equal true,            traces[1].key?('Args')
+      assert_equal "[1,2,3]",     traces[1]['Args']
+
+      assert_equal "resque-client", traces[1]['Layer'], "entry event layer name"
+      assert_equal "entry",         traces[1]['Label'], "entry event label"
+      assert_equal "resque-client", traces[4]['Layer'], "exit event layer name"
+      assert_equal "exit",          traces[4]['Label'], "exit event label"
     end
   end
 end
