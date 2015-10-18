dc9398f5378878688fa843a6619ee900df1844ea
pglombardo <pglombardo@gmail.com>
2015-10-18 18:31:27 +0200

    Update import paths to load all test jobs

--- lib/traceview/test.rb
+++ lib/traceview/test.rb
@@ -8,9 +8,9 @@ module TraceView
         # If we're using the libraries gemfile (with sidekiq and resque)
         if File.basename(ENV['BUNDLE_GEMFILE']) =~ /libraries/
           # Load all of the test workers
-          pattern = File.join(File.dirname(__FILE__), '../../test/jobs/', '*.rb')
+          pattern = File.join(File.dirname(__FILE__), '../../test/jobs/**/', '*.rb')
           Dir.glob(pattern) do |f|
-            TV.logger.debug "Loading test job file: #{f}"
+            TV.logger.debug "Loading test job file: #{File.basename(f)}"
             require f
           end
         end
