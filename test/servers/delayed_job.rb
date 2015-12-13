351467d2ce95988d964a05d7a3f830e61f0643e4
pglombardo <pglombardo@gmail.com>
2015-12-13 18:17:32 +0100

    Setup DB for DelayedJob background server

--- test/servers/delayed_job.rb
+++ test/servers/delayed_job.rb
@@ -1,7 +1,23 @@
 # Copyright (c) 2015 AppNeta, Inc.
 # All rights reserved.
 
-TraceView.logger.info "[traceview/servers] Starting up background DelayedJob."
+require "rails"
+require "delayed_job"
+
+TraceView.logger.level = Logger::DEBUG
+
+if ENV.key?('TRAVIS_PSQL_PASS')
+  DJ_DB_URL = "postgres://postgres:#{ENV['TRAVIS_PSQL_PASS']}@127.0.0.1:5432/travis_ci_test"
+else
+  DJ_DB_URL = 'postgres://postgres@127.0.0.1:5432/travis_ci_test'
+end
+
+ActiveRecord::Base.establish_connection(DJ_DB_URL)
+
+unless ActiveRecord::Base.connection.table_exists? :delayed_jobs
+  TraceView.logger.info "[traceview/servers] Creating DelayedJob DB table."
+  ActiveRecord::Migration.run(CreateDelayedJobs)
+end
 
 @worker_options = {
   :min_priority => ENV['MIN_PRIORITY'],
@@ -12,6 +28,8 @@ TraceView.logger.info "[traceview/servers] Starting up background DelayedJob."
 
 @worker_options[:sleep_delay] = ENV['SLEEP_DELAY'].to_i if ENV['SLEEP_DELAY']
 @worker_options[:read_ahead] = ENV['READ_AHEAD'].to_i if ENV['READ_AHEAD']
+
+TraceView.logger.info "[traceview/servers] Starting up background DelayedJob."
 
 Thread.new do
   Delayed::Worker.new(@worker_options).start
