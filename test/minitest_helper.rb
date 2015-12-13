3cb0d9e1b2a0bf97f615b7da225ffb837f38d528
pglombardo <pglombardo@gmail.com>
2015-12-13 20:38:35 +0100

    Fix quoting style

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -56,10 +56,10 @@ require './test/servers/rackapp_8101'
 #
 case File.basename(ENV['BUNDLE_GEMFILE'])
 when /rails4/
-  require "./test/servers/rails4x_8140"
+  require './test/servers/rails4x_8140'
   require './test/servers/delayed_job'
 when /rails3/
-  require "./test/servers/rails3x_8140"
+  require './test/servers/rails3x_8140'
   require './test/servers/delayed_job'
 when /frameworks/
 when /libraries/
