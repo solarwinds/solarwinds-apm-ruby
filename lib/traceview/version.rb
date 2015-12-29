1e2899cea22d575ffe3622cbbe4a104108b99f59
pglombardo <pglombardo@gmail.com>
2015-12-29 12:05:45 +0100

    Bump gem version to 3.4.0

--- lib/traceview/version.rb
+++ lib/traceview/version.rb
@@ -7,8 +7,8 @@ module TraceView
   # traceview.gemspec during gem build process
   module Version
     MAJOR = 3
-    MINOR = 3
-    PATCH = 3
+    MINOR = 4
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
