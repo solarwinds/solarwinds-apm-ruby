5a9b599a9fef47b0eddde214a4aebf0a51dec181
pglombardo <pglombardo@gmail.com>
2016-01-28 00:48:24 +0100

    Tests to validate init reports

---
+++ test/support/init_report_test.rb
@@ -0,0 +1,25 @@
+# Copyright (c) 2015 AppNeta, Inc.
+# All rights reserved.
+
+require 'minitest_helper'
+
+class InitReportTest  < Minitest::Test
+  def test_report_format
+    init_kvs = ::TraceView::Util.build_init_report
+    init_kvs.is_a?(Hash)
+  end
+
+  def test_report_kvs
+    init_kvs = ::TraceView::Util.build_init_report
+    init_kvs.has_key?("__Init").must_equal true
+    init_kvs.has_key?("Force").must_equal true
+    init_kvs.has_key?("Ruby.AppContainer.Version").must_equal true
+    init_kvs["Ruby.TraceView.Version"].must_equal TraceView::Version::STRING
+    init_kvs["Ruby.TraceMode.Version"].must_equal TraceView::Config[:tracing_mode]
+  end
+
+  def test_legacy_report_format
+    init_kvs = ::TraceView::Util.legacy_build_init_report
+    init_kvs.is_a?(Hash)
+  end
+end
