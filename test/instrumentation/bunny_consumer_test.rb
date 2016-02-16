87d89d43077db391bbaa0ae4f7dd10be768c1c0f
pglombardo <pglombardo@gmail.com>
2016-02-17 00:06:00 +0100

    Test for and validate SourceTrace

--- test/instrumentation/bunny_consumer_test.rb
+++ test/instrumentation/bunny_consumer_test.rb
@@ -37,29 +37,31 @@ unless defined?(JRUBY_VERSION)
         http.get('/?q=1').read_body
       end
 
-      @exchange.publish("The Tortoise and the Hare", :routing_key => @queue.name)
+      TraceView::API.start_trace('bunny_consume_test') do
+        @exchange.publish("The Tortoise and the Hare", :routing_key => @queue.name)
+      end
 
       sleep 1
 
       traces = get_all_traces
-      traces.count.must_equal 8
 
-      validate_outer_layers(traces, "rabbitmq-consumer")
+      traces.count.must_equal 12
       valid_edges?(traces)
 
-      traces[1]['Layer'].must_equal "net-http"
-      traces[1]['Label'].must_equal "entry"
-      traces[6]['Layer'].must_equal "net-http"
-      traces[6]['Label'].must_equal "exit"
+      traces[5]['Layer'].must_equal "net-http"
+      traces[5]['Label'].must_equal "entry"
+      traces[10]['Layer'].must_equal "net-http"
+      traces[10]['Label'].must_equal "exit"
 
-      traces[0]['Spec'].must_equal "job"
-      traces[0]['Flavor'].must_equal "rabbitmq"
-      traces[0]['Queue'].must_equal "tv.ruby.consumer.test"
-      traces[0]['RemoteHost'].must_equal @connection_params[:host]
-      traces[0]['RemotePort'].must_equal @connection_params[:port].to_i
-      traces[0]['VirtualHost'].must_equal @connection_params[:vhost]
-      traces[0]['RoutingKey'].must_equal "tv.ruby.consumer.test"
-      traces[0].key?('Backtrace').must_equal false
+      traces[4]['Spec'].must_equal "job"
+      traces[4]['Flavor'].must_equal "rabbitmq"
+      traces[4]['Queue'].must_equal "tv.ruby.consumer.test"
+      traces[4]['RemoteHost'].must_equal @connection_params[:host]
+      traces[4]['RemotePort'].must_equal @connection_params[:port].to_i
+      traces[4]['VirtualHost'].must_equal @connection_params[:vhost]
+      traces[4]['RoutingKey'].must_equal "tv.ruby.consumer.test"
+      traces[4].key?('SourceTrace').must_equal true
+      traces[4].key?('Backtrace').must_equal false
 
       @conn.close
     end
