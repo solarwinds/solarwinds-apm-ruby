ca0363d9f5fd1ba7399a1434f66d341751027b19
pglombardo <pglombardo@gmail.com>
2016-03-09 19:22:52 +0100

    Fix a Ruby warning.

--- lib/traceview/base.rb
+++ lib/traceview/base.rb
@@ -224,7 +224,7 @@ module TraceViewBase
         Pry.commands.alias_command 'n', 'next'
         Pry.commands.alias_command 'f', 'finish'
 
-        Pry::Commands.command /^$/, 'repeat last command' do
+        Pry::Commands.command(/^$/, 'repeat last command') do
           _pry_.run_command Pry.history.to_a.last
         end
       end
