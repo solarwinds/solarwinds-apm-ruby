1975dfa8c755642dedcb4295bb6b529dbecee7b2
pglombardo <pglombardo@gmail.com>
2016-03-13 21:06:30 +0100

    Updated ActionController 2 instrumentation

--- lib/traceview/frameworks/rails/inst/action_controller2.rb
+++ lib/traceview/frameworks/rails/inst/action_controller2.rb
@@ -12,14 +12,14 @@ module TraceView
     module ActionController
       include ::TraceView::Inst::RailsBase
 
-      def self.included(base)
-        alias :perform_action_without_traceview :perform_action
-        alias :rescue_action_without_traceview :rescue_action
-        alias :process_without_traceview :process
-        alias :render_without_traceview :render
+      def self.included(klass)
+        ::TraceView::Util.method_alias(klass, :perform_action)
+        ::TraceView::Util.method_alias(klass, :rescue_action)
+        ::TraceView::Util.method_alias(klass, :process)
+        ::TraceView::Util.method_alias(klass, :render)
       end
 
-      def process(*args)
+      def process_with_traceview(*args)
         TraceView::API.log_entry('rails')
         process_without_traceview(*args)
 
@@ -30,7 +30,7 @@ module TraceView
         TraceView::API.log_exit('rails')
       end
 
-      def perform_action(*arguments)
+      def perform_action_with_traceview(*arguments)
         report_kvs = {
           :Controller  => @_request.path_parameters['controller'],
           :Action      => @_request.path_parameters['action']
@@ -39,12 +39,12 @@ module TraceView
         perform_action_without_traceview(*arguments)
       end
 
-      def rescue_action(exn)
+      def rescue_action_with_traceview(exn)
         TraceView::API.log_exception(nil, exn) if log_rails_error?(exn)
         rescue_action_without_traceview(exn)
       end
 
-      def render(options = nil, extra_options = {}, &block)
+      def render_with_traceview(options = nil, extra_options = {}, &block)
         TraceView::API.log_entry('actionview')
         render_without_traceview(options, extra_options, &block)
 
