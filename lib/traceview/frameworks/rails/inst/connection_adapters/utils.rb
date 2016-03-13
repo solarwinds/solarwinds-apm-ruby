d9f12654f35a92df6710036b1054b490a4b16090
pglombardo <pglombardo@gmail.com>
2016-03-13 18:33:55 +0100

    Better QueryArg collection

--- lib/traceview/frameworks/rails/inst/connection_adapters/utils.rb
+++ lib/traceview/frameworks/rails/inst/connection_adapters/utils.rb
@@ -8,44 +8,41 @@ module TraceView
 
         def extract_trace_details(sql, name = nil, binds = [])
           opts = {}
-
-          begin
-            if TraceView::Config[:sanitize_sql]
-              # Sanitize SQL and don't report binds
-              opts[:Query] = TraceView::Util.sanitize_sql(sql)
-            else
-              # Report raw SQL and any binds if they exist
-              opts[:Query] = sql.to_s
-              opts[:QueryArgs] = binds.map { |col, val| type_cast(val, col) } unless binds.empty?
-            end
-
-            opts[:Name] = name.to_s if name
-            opts[:Backtrace] = TraceView::API.backtrace if TraceView::Config[:active_record][:collect_backtraces]
-
-            if ::Rails::VERSION::MAJOR == 2
-              config = ::Rails.configuration.database_configuration[::Rails.env]
-            else
-              config = ActiveRecord::Base.connection.instance_variable_get(:@config)
-            end
-
-            if config
-              opts[:Database]   = config['database'] if config.key?('database')
-              opts[:RemoteHost] = config['host']     if config.key?('host')
-              adapter_name = config[:adapter]
-
-              case adapter_name
-              when /mysql/i
-                opts[:Flavor] = 'mysql'
-              when /postgres/i
-                opts[:Flavor] = 'postgresql'
-              end
-            end
-          rescue StandardError => e
-            TraceView.logger.debug "Exception raised capturing ActiveRecord KVs: #{e.inspect}"
-            TraceView.logger.debug e.backtrace.join('\n')
+          if TraceView::Config[:sanitize_sql]
+            # Sanitize SQL and don't report binds
+            opts[:Query] = TraceView::Util.sanitize_sql(sql)
+          else
+            # Report raw SQL and any binds if they exist
+            opts[:Query] = sql.to_s
+            opts[:QueryArgs] = binds.map { |col, val| [col.name, val.to_s] } unless binds.empty?
           end
 
-          return opts || {}
+          opts[:Name] = name.to_s if name
+          opts[:Backtrace] = TraceView::API.backtrace if TraceView::Config[:active_record][:collect_backtraces]
+
+          if ::Rails::VERSION::MAJOR == 2
+            config = ::Rails.configuration.database_configuration[::Rails.env]
+          else
+            config = ActiveRecord::Base.connection.instance_variable_get(:@config)
+          end
+
+          if config
+            opts[:Database]   = config['database'] if config.key?('database')
+            opts[:RemoteHost] = config['host']     if config.key?('host')
+            adapter_name = config[:adapter]
+
+            case adapter_name
+            when /mysql/i
+              opts[:Flavor] = 'mysql'
+            when /postgres/i
+              opts[:Flavor] = 'postgresql'
+            end
+          end
+        rescue StandardError => e
+          TraceView.logger.debug "Exception raised capturing ActiveRecord KVs: #{e.inspect}"
+          TraceView.logger.debug e.backtrace.join('\n')
+        ensure
+          return opts
         end
 
         # We don't want to trace framework caches.  Only instrument SQL that
@@ -110,10 +107,7 @@ module TraceView
 
         def begin_db_transaction_with_traceview
           if TraceView.tracing?
-            opts = {}
-
-            opts[:Query] = 'BEGIN'
-            TraceView::API.trace('activerecord', opts || {}) do
+            TraceView::API.trace('activerecord', { :Query => 'BEGIN' }) do
               begin_db_transaction_without_traceview
             end
           else
