5c20c6693a9ace5dd70661aa41001366024d4da9
Maia Engeli <maia@prettycoders.com>
2017-10-02 11:41:31 -0700

    add outbound xtraces to rest-client
    #5

--- lib/traceview/inst/rest-client.rb
+++ lib/traceview/inst/rest-client.rb
@@ -13,18 +13,28 @@ module TraceView
       #
       # The wrapper method for RestClient::Request.execute
       #
-      def execute_with_traceview & block
+      def execute_with_traceview(&block)
+        blacklisted = TraceView::API.blacklisted?(uri)
+
+        unless TraceView.tracing?
+          xtrace = TraceView::Context.toString
+          @processed_headers = make_headers('X-Trace' => xtrace) if TraceView::XTrace.valid?(xtrace) && !blacklisted
+          return execute_without_traceview(&block)
+        end
+
         kvs = {}
         kvs[:Backtrace] = TraceView::API.backtrace if TraceView::Config[:rest_client][:collect_backtraces]
-        TraceView::API.log_entry(:'rest-client', kvs)
+        TraceView::API.log_entry('rest-client', kvs)
+
+        @processed_headers = make_headers('X-Trace' => TraceView::Context.toString) unless blacklisted
 
         # The core rest-client call
         execute_without_traceview(&block)
       rescue => e
-        TraceView::API.log_exception(:'rest-client', e)
+        TraceView::API.log_exception('rest-client', e)
         raise e
       ensure
-        TraceView::API.log_exit(:'rest-client')
+        TraceView::API.log_exit('rest-client')
       end
     end
   end
