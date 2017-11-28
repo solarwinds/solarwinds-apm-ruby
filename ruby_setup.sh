16743f5f3d83bb17a51ac344f338a81d5821bda3
Maia Engeli <maia@prettycoders.com>
2017-11-28 10:34:31 -0800

    tweaks to the docker setup
    - add quota, try version "2", install less
    - automatically find gem to upload to package cloud
    - remove containers

--- ruby_setup.sh
+++ ruby_setup.sh
@@ -3,7 +3,7 @@
 # used by run_tests_docker.sh
 # call with:
 # docker-compose run --service-ports ruby_appoptics /code/ruby-appoptics/ruby_setup.sh <ruby-version> <gemfile> <true|false>
-# docker-compose run --service-ports ruby_appoptics /code/ruby-appoptics/ruby_setup.sh 1.9.3 gemfiles/libraries.gemfile
+# docker-compose run --service-ports ruby_appoptics /code/ruby-appoptics/ruby_setup.sh 2.4.1 gemfiles/libraries.gemfile
 
 
 cd /code/ruby-appoptics/
