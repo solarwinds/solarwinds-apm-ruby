a294592b1e367b2533cf7b6065ca661009631812
Maia Engeli <maia@prettycoders.com>
2017-12-07 16:38:27 -0800

    add mini benchmark WIP setup including README
    #20

--- test/benchmark/logging_bench.rb
+++ test/benchmark/logging_bench.rb
@@ -8,30 +8,31 @@ require_relative '../minitest_helper'
 # compare logging when testing for loaded versus tracing?
 ENV['APPOPTICS_GEM_VERBOSE'] = 'false'
 
-n = 50000
+n = 1000
 
 Benchmark.ips do |x|
   x.config(:time => 10, :warmup => 2)
 
-  x.report('tracing_f') do
-    AppOptics.loaded = false
-    AppOptics::Config[:tracing_mode] = 'never'
-    AppOptics::Context.fromString('2B7435A9FE510AE4533414D425DADF4E180D2B4E3649E60702469DB05F00')
-    n.times do
-      AppOptics.tracing?
-    end
-  end
-  x.report('tracing_n') do
-    AppOptics.loaded = true
-    AppOptics::Config[:tracing_mode] = 'never'
-    AppOptics::Context.fromString('2B7435A9FE510AE4533414D425DADF4E180D2B4E3649E60702469DB05F00')
-    n.times do
-      AppOptics.tracing?
-    end
-  end
+  # x.report('tracing_f') do
+  #   AppOptics.loaded = false
+  #   AppOptics::Config[:tracing_mode] = 'never'
+  #   AppOptics::Context.fromString('2B7435A9FE510AE4533414D425DADF4E180D2B4E3649E60702469DB05F00')
+  #   n.times do
+  #     AppOptics.tracing?
+  #   end
+  # end
+  # x.report('tracing_n') do
+  #   AppOptics.loaded = true
+  #   AppOptics::Config[:tracing_mode] = 'never'
+  #   AppOptics::Context.fromString('2B7435A9FE510AE4533414D425DADF4E180D2B4E3649E60702469DB05F00')
+  #   n.times do
+  #     AppOptics.tracing?
+  #   end
+  # end
+
   x.report('tracing_tf') do
     AppOptics.loaded = true
-    AppOptics::Config[:tracing_mode] = 'never'
+    AppOptics::Config[:tracing_mode] = 'always'
     AppOptics::Context.fromString('2B7435A9FE510AE4533414D425DADF4E180D2B4E3649E60702469DB05F00')
     n.times do
       AppOptics.tracing?
@@ -39,9 +40,10 @@ Benchmark.ips do |x|
   end
   x.report('tracing_tt') do
     AppOptics.loaded = true
-    AppOptics::Config[:tracing_mode] = 'never'
+    AppOptics::Config[:tracing_mode] = 'always'
     AppOptics::Context.fromString('2B7435A9FE510AE4533414D425DADF4E180D2B4E3649E60702469DB05F01')
     n.times do
+      AppOptics.tracing?
       AppOptics.tracing?
     end
   end
