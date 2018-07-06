e9fc839f42f4450a3b8ec59be444cb29cf30a118
Maia Engeli <maia@prettycoders.com>
2018-07-06 14:29:02 -0700

    remove old examples, they will be recreated (most of them)

--- examples/instrumenting_metal_controller.rb
+++
@@ -1,8 +0,0 @@
-class MetalController < ActionController::Metal
-  def index
-    self.response_body = 'Hello Metal!'
-  end
-
-  include AppOpticsAPMMethodProfiling
-  profile_method :index, 'metal-index'
-end
