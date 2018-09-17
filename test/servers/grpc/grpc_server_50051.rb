211939c0a32d5599257b7d04bbb743ab9cf6a3ea
Maia Engeli <maia@prettycoders.com>
2018-09-17 17:18:41 +0200

    [grpc_client/test] stop grpc server gracefully, so that tests ...
    ... pass in travis. Aborting the server created an error code that
    was picked up by travis and made the test suite fail.
    
    [AO-7401]

--- test/servers/grpc/grpc_server_50051.rb
+++ test/servers/grpc/grpc_server_50051.rb
@@ -147,23 +147,3 @@ class AddressService < Grpctest::TestService::Service
     raise StandardError
   end
 end
-
-class AddressServer
-  class << self
-    def start
-      start_grpc_server
-    end
-
-    private
-    def start_grpc_server
-      @server = GRPC::RpcServer.new
-      @server.add_http2_port("0.0.0.0:50051", :this_port_is_insecure)
-      @server.handle(AddressService)
-      @server.run_till_terminated
-    end
-  end
-end
-
-Thread.new do
-  AddressServer.start
-end
