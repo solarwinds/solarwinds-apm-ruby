d127d104c8c21b69d82ef17b958f3add246e8f0b
Maia Engeli <maia@prettycoders.com>
2018-10-29 09:52:07 -0700

    [test] enhance valid_edges? check, simplify SDK

--- lib/appoptics_apm/sdk.rb
+++ lib/appoptics_apm/sdk.rb
@@ -178,7 +178,8 @@ module AppOpticsAPM
           return result
         end
 
-        transaction_name_from_opts(opts)
+        # :TransactionName and 'TransactionName' need to be removed from opts
+        AppOpticsAPM.transaction_name = opts.delete('TransactionName') || opts.delete(:TransactionName)
 
         AppOpticsAPM::API.log_start(span, xtrace, opts)
         # AppOpticsAPM::Event.startTrace creates an Event without an Edge
@@ -293,6 +294,7 @@ module AppOpticsAPM
       #   end
       #
       def appoptics_ready?(wait_milliseconds = 3000)
+        return false unless AppOpticsAPM.loaded
         # These codes are returned by isReady:
         # OBOE_SERVER_RESPONSE_UNKNOWN 0
         # OBOE_SERVER_RESPONSE_OK 1
@@ -301,21 +303,6 @@ module AppOpticsAPM
         # OBOE_SERVER_RESPONSE_INVALID_API_KEY 4
         # OBOE_SERVER_RESPONSE_CONNECT_ERROR 5
         AppopticsAPM::Context.isReady(wait_milliseconds) == 1
-      end
-
-      private
-      # private method
-      #
-      # This should only be called at the beginning of a transaction,
-      # when AppOpticsAPM.transaction_name needs to be set to nil
-      # or whatever is provided in the opts
-      def transaction_name_from_opts(opts)
-        # :TransactionName and 'TransactionName' need to be removed from opts
-        # :TransactionName should only be sent after it is set by send_metrics
-        transaction_name = opts.delete('TransactionName')
-        transaction_name = opts.delete(:TransactionName) || transaction_name
-
-        AppOpticsAPM.transaction_name = transaction_name
       end
     end
 
