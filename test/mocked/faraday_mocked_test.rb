17e2d9c2341636b6ba3358df9f2e9e61a991e35c
Maia Engeli <maia@prettycoders.com>
2018-11-01 10:11:25 -0700

    [faraday] fix faraday so that edges are properly continued
    - when the faraday adapter/middleware is instrumented the adapter
    (default net-http) will take care of adding the x-trace header and create
    the kvs for the rsc. When the adapter is not instrumented then it is
    faraday who does the rsc and adds kvs and x-trace header
    
    - added tests for cases where middleware and/or remote service are not
    instrumented (patron is an adapter without instrumentation)
    
    - removed the info event, the kvs are recorded on exit
    
    - adapted tests, because there are less info events now
    
    - adapted  tests to test for the updated set of kvs
    
    - added a uninstrumented rack service on 127.0.0.1:8110
    
    [AO-10414][AO-10487]

--- test/mocked/faraday_mocked_test.rb
+++ test/mocked/faraday_mocked_test.rb
@@ -32,7 +32,7 @@ if !defined?(JRUBY_VERSION)
       end
 
       assert_requested :get, "http://127.0.0.1:8101/", times: 1
-      assert_requested :get, "http://127.0.0.1:8101/", headers: {'X-Trace'=>/^2B[0-9,A-F]*01$/}, times: 1
+      refute_requested :get, "http://127.0.0.1:8101/", headers: {'X-Trace'=>/^2B[0-9,A-F]*01$/}, times: 1
       refute AppOpticsAPM::Context.isValid
     end
 
@@ -50,8 +50,8 @@ if !defined?(JRUBY_VERSION)
       end
 
       assert_requested :get, "http://127.0.0.12:8101/", times: 1
-      assert_requested :get, "http://127.0.0.12:8101/", headers: {'X-Trace'=>/^2B[0-9,A-F]*00$/}, times: 1
-      assert_not_requested :get, "http://127.0.0.12:8101/", headers: {'X-Trace'=>/^2B0*$/}
+      refute_requested :get, "http://127.0.0.12:8101/", headers: {'X-Trace'=>/^2B[0-9,A-F]*00$/}, times: 1
+      refute_requested :get, "http://127.0.0.12:8101/", headers: {'X-Trace'=>/^2B0*$/}
       refute AppOpticsAPM::Context.isValid
     end
 
@@ -64,7 +64,7 @@ if !defined?(JRUBY_VERSION)
       conn.get
 
       assert_requested :get, "http://127.0.0.3:8101/", times: 1
-      assert_not_requested :get, "http://127.0.0.3:8101/", headers: {'X-Trace'=>/^.*$/}
+      refute_requested :get, "http://127.0.0.3:8101/", headers: {'X-Trace'=>/^.*$/}
     end
 
     def test_blacklisted
@@ -81,9 +81,74 @@ if !defined?(JRUBY_VERSION)
       end
 
       assert_requested :get, "http://127.0.0.4:8101/", times: 1
-      assert_not_requested :get, "http://127.0.0.4:8101/", headers: {'X-Trace'=>/^.*$/}
+      refute_requested :get, "http://127.0.0.4:8101/", headers: {'X-Trace'=>/^.*$/}
       refute AppOpticsAPM::Context.isValid
     end
 
+    ##### with uninstrumented middleware #####
+
+    def test_tracing_sampling_patron
+      stub_request(:get, "http://127.0.0.1:8101/")
+
+      AppOpticsAPM::API.start_trace('faraday_test') do
+        conn = Faraday.new(:url => 'http://127.0.0.1:8101') do |faraday|
+          faraday.adapter :patron # use an uninstrumented middleware
+        end
+        conn.get
+      end
+
+      assert_requested :get, "http://127.0.0.1:8101/", times: 1
+      assert_requested :get, "http://127.0.0.1:8101/", headers: {'X-Trace'=>/^2B[0-9,A-F]*01$/}, times: 1
+      refute AppOpticsAPM::Context.isValid
+    end
+
+    def test_tracing_not_sampling_patron
+      stub_request(:get, "http://127.0.0.12:8101/")
+
+      AppOpticsAPM.config_lock.synchronize do
+        AppOpticsAPM::Config[:sample_rate] = 0
+        AppOpticsAPM::API.start_trace('faraday_test') do
+          conn = Faraday.new(:url => 'http://127.0.0.12:8101') do |faraday|
+            faraday.adapter :patron # use an uninstrumented middleware
+          end
+          conn.get
+        end
+      end
+
+      assert_requested :get, "http://127.0.0.12:8101/", times: 1
+      assert_requested :get, "http://127.0.0.12:8101/", headers: {'X-Trace'=>/^2B[0-9,A-F]*00$/}, times: 1
+      assert_not_requested :get, "http://127.0.0.12:8101/", headers: {'X-Trace'=>/^2B0*$/}
+      refute AppOpticsAPM::Context.isValid
+    end
+
+    def test_no_xtrace_patron
+      stub_request(:get, "http://127.0.0.3:8101/")
+
+      conn = Faraday.new(:url => 'http://127.0.0.3:8101') do |faraday|
+        faraday.adapter :patron # use an uninstrumented middleware
+      end
+      conn.get
+
+      assert_requested :get, "http://127.0.0.3:8101/", times: 1
+      assert_not_requested :get, "http://127.0.0.3:8101/", headers: {'X-Trace'=>/^.*$/}
+    end
+
+    def test_blacklisted_patron
+      stub_request(:get, "http://127.0.0.4:8101/")
+
+      AppOpticsAPM.config_lock.synchronize do
+        AppOpticsAPM::Config.blacklist << '127.0.0.4'
+        AppOpticsAPM::API.start_trace('faraday_test') do
+          conn = Faraday.new(:url => 'http://127.0.0.4:8101') do |faraday|
+            faraday.adapter :patron # use an uninstrumented middleware
+          end
+          conn.get
+        end
+      end
+
+      assert_requested :get, "http://127.0.0.4:8101/", times: 1
+      assert_not_requested :get, "http://127.0.0.4:8101/", headers: {'X-Trace'=>/^.*$/}
+      refute AppOpticsAPM::Context.isValid
+    end
   end
 end
--- test/servers/rackapp_8101.rb
+++ test/servers/rackapp_8101.rb
@@ -24,6 +24,22 @@ Thread.new do
   Rack::Handler::Puma.run(app, {:Host => '127.0.0.1', :Port => 8101})
 end
 
+AppOpticsAPM.logger.info "[appoptics_apm/info] Starting UNINSTRUMENTED background utility rack app on localhost:8110."
+
+Thread.new do
+  app = Rack::Builder.new {
+    map "/" do
+      run Proc.new { [200, {"Content-Type" => "text/html"}, ['Hello AppOpticsAPM!']] }
+    end
+
+    map "/redirectme" do
+      run Proc.new { [301, {"Location" => "/", "Content-Type" => "text/html"}, ['']] }
+    end
+  }
+
+  Rack::Handler::Puma.run(app, {:Host => '127.0.0.1', :Port => 8110})
+end
+
 # Allow Thin to boot.
 sleep(2)
 
