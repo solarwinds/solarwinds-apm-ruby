fc48eb3af9cbe6b0d90f585d34632e11218a6106
Maia Engeli <maia@prettycoders.com>
2018-11-13 16:09:32 -0800

    [version] update gem version to 4.4.0, oboe version to 3.3.0
    new features: CustomMetrics, Disable oboe logging

--- lib/appoptics_apm/version.rb
+++ lib/appoptics_apm/version.rb
@@ -7,8 +7,8 @@ module AppOpticsAPM
   # appoptics_apm.gemspec during gem build process
   module Version
     MAJOR = 4
-    MINOR = 3
-    PATCH = 1
+    MINOR = 4
+    PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
