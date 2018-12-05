941d596bc6e3d77e11bef8a7ec842aa0c10ee92d
Maia Engeli <maia@prettycoders.com>
2018-12-05 15:16:41 -0800

    [::] remove 2 more unnecessary `::`
    [AO-10473]

--- lib/appoptics_apm/frameworks/rails/inst/action_controller.rb
+++ lib/appoptics_apm/frameworks/rails/inst/action_controller.rb
@@ -88,7 +88,7 @@ if defined?(ActionController::Base) && AppOpticsAPM::Config[:action_controller][
   AppOpticsAPM.logger.info '[appoptics_apm/loading] Instrumenting actioncontroller' if AppOpticsAPM::Config[:verbose]
   require "appoptics_apm/frameworks/rails/inst/action_controller#{Rails::VERSION::MAJOR}"
   if Rails::VERSION::MAJOR >= 5
-    ::ActionController::Base.send(:prepend, AppOpticsAPM::Inst::ActionController)
+    ActionController::Base.send(:prepend, ::AppOpticsAPM::Inst::ActionController)
   else
     AppOpticsAPM::Util.send_include(::ActionController::Base, AppOpticsAPM::Inst::ActionController)
   end
@@ -98,7 +98,7 @@ end
 if defined?(ActionController::API) && AppOpticsAPM::Config[:action_controller_api][:enabled]
   AppOpticsAPM.logger.info '[appoptics_apm/loading] Instrumenting actioncontroller api' if AppOpticsAPM::Config[:verbose]
   require "appoptics_apm/frameworks/rails/inst/action_controller_api"
-  ::ActionController::API.send(:prepend, AppOpticsAPM::Inst::ActionControllerAPI)
+  ActionController::API.send(:prepend, ::AppOpticsAPM::Inst::ActionControllerAPI)
 end
 
 # vim:set expandtab:tabstop=2
