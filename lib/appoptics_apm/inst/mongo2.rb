d22dd43c71d3ba9a1d7530532845237fa85204d4
Maia Engeli <maia@prettycoders.com>
2018-12-05 11:19:32 -0800

    [backtraces] fix and revert some mongo stuff
    [AO-9540]

--- lib/appoptics_apm/inst/mongo2.rb
+++ lib/appoptics_apm/inst/mongo2.rb
@@ -51,6 +51,7 @@ if AppOpticsAPM::Config[:mongo][:enabled]
           end
 
           kvs[:RemoteHost] = @database.client.cluster.addresses.first.to_s
+          kvs[:Backtrace] = AppOpticsAPM::API.backtrace if AppOpticsAPM::Config[:mongo][:collect_backtraces]
         rescue => e
           AppOpticsAPM.logger.debug "[appoptics_apm/debug] #{__method__}:#{File.basename(__FILE__)}:#{__LINE__}: #{e.message}" if AppOpticsAPM::Config[:verbose]
         ensure
@@ -71,7 +72,6 @@ if AppOpticsAPM::Config[:mongo][:enabled]
 
               kvs = collect_kvs(m, args)
               AppOpticsAPM::API.log_entry(:mongo, kvs)
-              kvs[:Backtrace] = AppOpticsAPM::API.backtrace if AppOpticsAPM::Config[:mongo][:collect_backtraces]
 
               send("#{m}_without_appoptics", *args)
             rescue => e
@@ -128,6 +128,7 @@ if AppOpticsAPM::Config[:mongo][:enabled]
             end
 
             kvs[:RemoteHost] = @collection.database.client.cluster.addresses.first.to_s
+            kvs[:Backtrace] = AppOpticsAPM::API.backtrace if AppOpticsAPM::Config[:mongo][:collect_backtraces]
           rescue => e
             AppOpticsAPM.logger.debug "[appoptics_apm/debug] #{__method__}:#{File.basename(__FILE__)}:#{__LINE__}: #{e.message}" if AppOpticsAPM::Config[:verbose]
           ensure
@@ -148,7 +149,6 @@ if AppOpticsAPM::Config[:mongo][:enabled]
 
                 kvs = collect_kvs(m, args)
                 AppOpticsAPM::API.log_entry(:mongo, kvs)
-                kvs[:Backtrace] = AppOpticsAPM::API.backtrace if AppOpticsAPM::Config[:mongo][:collect_backtraces]
 
                 send("#{m}_without_appoptics", *args)
               rescue => e
