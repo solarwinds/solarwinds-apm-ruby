80530f442fac8483f6fe3d95e3cea6cdf784ace5
Maia Engeli <maia@prettycoders.com>
2019-03-27 15:23:17 -0700

    change version to 4.6.0

--- lib/appoptics_apm/version.rb
+++ lib/appoptics_apm/version.rb
@@ -7,8 +7,8 @@ module AppOpticsAPM
   # appoptics_apm.gemspec during gem build process
   module Version
     MAJOR = 4
-    MINOR = 5
-    PATCH = 2
+    MINOR = 6
+    PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
