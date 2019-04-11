fffde5b37a58d10efdf895f27365b3c4af8b2433
Maia Engeli <maia@prettycoders.com>
2019-04-11 15:45:32 +0200

    [ruby loggers] add traceId do logs for Logger, Logging and Lumberjack
    - includes tests
    
    [AO-12305]

---
+++ lib/appoptics_apm/inst/logger_formatter.rb
@@ -0,0 +1,49 @@
+# Copyright (c) 2019 SolarWinds, LLC.
+# All rights reserved.
+
+require 'logger'
+
+module AppOpticsAPM
+  module Logger
+    module Formatter
+
+      def call(severity, time, progname, msg)
+        return super if AppOpticsAPM::Config[:log_traceId] == :never
+
+        msg = insert_trace_id(msg)
+        super
+      end
+
+      private
+
+      def insert_trace_id(msg)
+        return msg if msg =~ /ao(=>{:|\.){1}traceId/
+
+        current_trace = AppOpticsAPM::SDK.current_trace
+        if current_trace.log?
+          case msg
+          when ::String
+            insert_before_empty_lines(msg, current_trace.for_log)
+          when ::Exception
+            # conversion to String copied from Logger::Formatter private method #msg2str
+            "#{msg.message} (#{msg.class}) #{current_trace.for_log}\n" <<
+              (msg.backtrace || []).join("\n")
+          end
+        end
+      end
+
+      def insert_before_empty_lines(msg, for_log)
+        stripped = msg.rstrip
+        "#{stripped} #{for_log}#{msg[stripped.length..-1]}"
+      end
+    end
+  end
+end
+
+if AppOpticsAPM.loaded
+  class Logger
+    class Formatter
+      prepend AppOpticsAPM::Logger::Formatter
+    end
+  end
+end
---
+++ test/instrumentation/logger_formatter_test.rb
@@ -0,0 +1,23 @@
+require 'minitest_helper'
+
+describe "Logger::Formatter " do
+  let(:msg) { Logger::Formatter.new.call('error', Time.now, 'test', 'Message.') }
+  let (:exc_message) { Logger::Formatter.new.call('error', Time.now, 'test', StandardError.new) }
+
+  load File.join(File.dirname(__FILE__), 'logger_formatter_helper.rb')
+end
+
+describe "Lumberjack::Formatter " do
+  let(:msg) { Lumberjack::Formatter.new.call('error', Time.now, 'test', 'Message.') }
+  let (:exc_message) { Lumberjack::Formatter.new.call('error', Time.now, 'test', StandardError.new) }
+
+  load File.join(File.dirname(__FILE__), 'logger_formatter_helper.rb')
+end
+
+describe "Logging::LogEvent " do
+  let(:msg) { Logging::LogEvent.new('error', Time.now, 'test', 'Message.').data }
+  let (:exc_message) { Logging::LogEvent.new('error', Time.now, 'test', StandardError.new).data }
+
+  load File.join(File.dirname(__FILE__), 'logger_formatter_helper.rb')
+end
+
