6a29600ef1db10265c9cc9c871a764210b92d313
Maia Engeli <maia@prettycoders.com>
2019-09-17 17:02:55 +0200

    [noop] remove assignment to unused var

--- ext/oboe_metal/noop/noop.c
+++ ext/oboe_metal/noop/noop.c
@@ -3,5 +3,6 @@
 /* ruby calls this to load the extension */
 void Init_oboe_noop(void) {
   /* assume we haven't yet defined Hola */
-  VALUE klass = rb_define_class("OboeNoop", rb_cObject);
+//  VALUE klass = rb_define_class("OboeNoop", rb_cObject);
+  rb_define_class("OboeNoop", rb_cObject);
 }
