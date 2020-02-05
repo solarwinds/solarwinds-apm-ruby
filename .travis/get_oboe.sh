9c19f98e328d9d8f84d2464c2a42b4cd0cc45a91
Maia Engeli <maia@prettycoders.com>
2020-02-05 15:52:06 -0800

    [travis] tweaks to get_oboe script
    AO-15107

--- .travis/get_oboe.sh
+++ .travis/get_oboe.sh
@@ -5,11 +5,11 @@ set -ev # (exit immediatly on failure and print script lines before execution)
 CURRENT_RUBY=`rvm current`
 # set ruby to 2.5.3 (pre-installed on travis)
 rvm 2.5.3
-bundle
+bundle update --jobs=3 --retry=3
 bundle exec rake clean fetch
 # restore previous ruby
 rvm $CURRENT_RUBY
 rm gemfiles/*.lock
-bundle
+bundle update --jobs=3 --retry=3
 bundle exec rake compile
 
