fba1a7484af1943238447c508f306d898ab564d7
Maia Engeli <maia@prettycoders.com>
2020-02-06 12:12:26 -0800

    [travis] change to `rake compile` with test version ruby
    AO-15107

--- .travis/get_oboe.sh
+++ .travis/get_oboe.sh
@@ -2,15 +2,15 @@
 
 set -e # (exit immediatly on failure)
 source ~/.profile
-# store current ruby
+
 CURRENT_RUBY=`rvm current`
 CURRENT_GEMFILE=$BUNDLE_GEMFILE
 # set ruby to 2.5.3 (pre-installed on travis)
 rvm 2.5.3
 unset BUNDLE_GEMFILE
 bundle update --jobs=3 --retry=3
-bundle exec rake clean fetch compile
+bundle exec rake distclean fetch
 # restore previous ruby
 rvm $CURRENT_RUBY
 export BUNDLE_GEMFILE=$CURRENT_GEMFILE
-
+bundle exec rake compile
