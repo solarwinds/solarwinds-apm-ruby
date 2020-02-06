db88136c5d2398654936c5a036a2427d33c5300c
Maia Engeli <maia@prettycoders.com>
2020-02-05 16:26:22 -0800

    [travis] enforce older bundler
    AO-15107

--- .travis/get_oboe.sh
+++ .travis/get_oboe.sh
@@ -6,11 +6,12 @@ source ~/.profile
 CURRENT_RUBY=`rvm current`
 # set ruby to 2.5.3 (pre-installed on travis)
 rvm 2.5.3
+gem uninstall bundler --quiet -x && gem install bundler -v 1.17.3
 bundle update --jobs=3 --retry=3
 bundle exec rake clean fetch
 # restore previous ruby
 rvm $CURRENT_RUBY
-rm gemfiles/*.lock
+rm -f gemfiles/*.lock
 bundle update --jobs=3 --retry=3
 bundle exec rake compile
 
