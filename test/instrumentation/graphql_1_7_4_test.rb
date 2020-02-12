cb2d74c649cd4088819ca4e20eef6eb8205d1bee
Maia Engeli <maia@prettycoders.com>
2020-02-12 12:04:12 -0800

    remove commented out stuff
    AO-15541

--- test/instrumentation/graphql_1_7_4_test.rb
+++ test/instrumentation/graphql_1_7_4_test.rb
@@ -426,7 +426,6 @@ describe GraphQL::Tracing::AppOpticsTracing do
     # GraphQL::Tracing::AppOpticsTracing class
     # from the graphql gem will be loaded first
     it 'uses the newer version of AppOpticsTracing from the appoptics_apm gem' do
-      # Kernel.silence_warnings do # silence warning about re-initializing a const
         load 'test/instrumentation/graphql/appoptics_tracing_older.rb'
         load 'lib/appoptics_apm/inst/graphql.rb'
 
@@ -434,11 +433,9 @@ describe GraphQL::Tracing::AppOpticsTracing do
                      GraphQL::Tracing::AppOpticsTracing.new.method(:metadata).source_location[0]
         assert_match 'lib/appoptics_apm/inst/graphql.rb',
                      GraphQL::Tracing::AppOpticsTracing.new.method(:platform_trace).source_location[0]
-      # end
     end
 
     it 'uses the newer version of AppOpticsTracing from the graphql gem' do
-      # Kernel.silence_warnings do # silence warning about re-initializing a const
         load 'test/instrumentation/graphql/appoptics_tracing_newer.rb'
         load 'lib/appoptics_apm/inst/graphql.rb'
 
@@ -446,7 +443,6 @@ describe GraphQL::Tracing::AppOpticsTracing do
                      GraphQL::Tracing::AppOpticsTracing.new.method(:metadata).source_location[0]
         assert_match 'graphql/appoptics_tracing_newer.rb',
                      GraphQL::Tracing::AppOpticsTracing.new.method(:platform_trace).source_location[0]
-      # end
     end
 
   end
