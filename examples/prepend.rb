85cc338c4b7fae94564c1a3715a6eac44cc22244
Maia Engeli <maia@prettycoders.com>
2020-03-11 12:21:16 -0700

    prepend example for mmbeale's blog post

---
+++ examples/prepend.rb
@@ -0,0 +1,13 @@
+require 'appoptics_apm'
+
+module Measurements
+  def request(*args, &block)
+    req = args.first
+    AppOpticsAPM::SDK.summary_metric("request_size", req.to_hash.to_s.size)
+    resp = super
+    AppOpticsAPM::SDK.summary_metric("response_size", resp.to_hash.to_s.size)
+    return resp
+  end
+end
+
+Net::HTTP.send(:prepend, :Measurements)
