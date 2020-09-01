dc69e90f8bd121fed2dd6d419817fee6d15c307c
Maia Engeli <maia@prettycoders.com>
2020-09-01 16:02:55 -0700

    [profiling] clean up ruby code
    - there isn't much needed in the Ruby profiling class
    
    AO-12790

--- lib/appoptics_apm/support/profiling.rb
+++ lib/appoptics_apm/support/profiling.rb
@@ -1,49 +1,18 @@
 # Copyright (c) 2020 SolarWinds, LLC.
 # All rights reserved.
 
-# require 'rack-mini-profiler'
-
 module AppOpticsAPM
   class Profiling
-    AOProfiler.set_interval(AppOpticsAPM::Config.profiling_interval)
+    CProfiler.set_interval(AppOpticsAPM::Config.profiling_interval)
 
     def self.run
      return yield unless AppOpticsAPM::Config.profiling == :enabled && AppOpticsAPM.tracing?
 
-     AOProfiler.run(Thread.current) do
+     CProfiler.run(Thread.current) do
         # for some reason `return` is needed here
         # this is yielded by c-code, but why it needs `return` ... ????
         return yield
       end
-    end
-
-    # for testing
-    def self.interval=(interval)
-      AOProfiler.set_interval(interval)
-    end
-
-    def self.interval
-      AOProfiler.get_rb_interval
-    end
-
-    def self.running?
-      AOProfiler.running?
-    end
-
-    def self.getTid
-      AOProfiler.getTid
-    end
-
-    def self.stack
-      my_iseq = nil
-      ObjectSpace.each_iseq { |iseq| my_iseq = iseq }
-      # stack = caller
-      # stack.delete_if { |ele| ele =~ /block /}
-      # stack = stack[1..-1] if stack[0] =~ /eval:1/
-      #
-      # puts "**********************************************"
-      # puts stack
-      # stack
     end
   end
 end
