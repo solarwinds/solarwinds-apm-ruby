ad046961b5b1fc3ee6a2929fb0c4e047224b1621
Maia Engeli <maia@prettycoders.com>
2021-02-17 12:35:23 -0800

    use prepend to instrument http, alias chain creates conflict with airbrake
    AO-18679

--- test/mocked/http_mocked_test.rb
+++ test/mocked/http_mocked_test.rb
@@ -8,9 +8,15 @@ unless defined?(JRUBY_VERSION)
 
   class HTTPMockedTest < Minitest::Test
 
-    class MockResponse
-      def get_fields(x); nil; end
-      def code; 200; end
+    # prepend HttpMock to check if appoptics is in the ancestors chain
+    # resorting to this solution because a method instrumented by using :prepend
+    # can't be mocked with mocha
+    module HttpMock
+      def request(*args, &block)
+        res = super
+        res['ancestors'] = self.class.ancestors
+        return res
+      end
     end
 
     def setup
@@ -23,69 +29,86 @@ unless defined?(JRUBY_VERSION)
       AppOpticsAPM::Config[:sample_rate] = 1000000
       AppOpticsAPM::Config[:tracing_mode] = :enabled
       AppOpticsAPM::Config[:blacklist] = []
+
+      # prepending outside of tests didn't work when using run_tests.sh
+      Net::HTTP.prepend(HTTPMockedTest::HttpMock)
     end
 
-    # webmock not working, interferes with instrumentation
     def test_tracing_sampling
-      Net::HTTP.any_instance.expects(:request_without_appoptics).with do |req, _|
-        !req.to_hash['x-trace'].nil? &&
-            req.to_hash['x-trace'].first =~ /^2B[0-9,A-F]*01$/
-      end.returns(MockResponse.new)
-
       AppOpticsAPM::API.start_trace('net_http_test') do
         uri = URI('http://127.0.0.1:8101/?q=1')
         Net::HTTP.start(uri.host, uri.port) do |http|
+          xt = AppOpticsAPM::Context.toString
           request = Net::HTTP::Get.new(uri)
-          http.request(request)
+          refute request['x-trace']
+
+          res = http.request(request)
+          # did we instrument?
+          assert res['ancestors'].include?('AppOpticsAPM::Inst::NetHttp')
+          # is the x-trace correct, it does not change during this call
+          assert request['x-trace']
+          assert res['x-trace']
+          assert AppOpticsAPM::XTrace.sampled?(res['x-trace'])
+          refute_equal xt, request['x-trace']
+          refute_equal xt, res['x-trace']
         end
       end
       refute AppOpticsAPM::Context.isValid
     end
 
     def test_tracing_not_sampling
-      Net::HTTP.any_instance.expects(:request_without_appoptics).with do |req, _, _|
-        !req.to_hash['x-trace'].nil? &&
-            req.to_hash['x-trace'].first =~ /^2B[0-9,A-F]*00$/ &&
-            req.to_hash['x-trace'].first !~ /^2B0*$/
-      end.returns(MockResponse.new)
-
       AppOpticsAPM.config_lock.synchronize do
         AppOpticsAPM::Config[:sample_rate] = 0
         AppOpticsAPM::API.start_trace('Net::HTTP_test') do
           uri = URI('http://127.0.0.1:8101/?q=1')
           Net::HTTP.start(uri.host, uri.port) do |http|
+            xt = AppOpticsAPM::Context.toString
+
             request = Net::HTTP::Get.new(uri)
-            http.request(request) # Net::HTTPResponse object
+            refute request['x-trace']
+
+            res = http.request(request) # Net::HTTPResponse object
+            # did we instrument?
+            assert res['ancestors'].include?('AppOpticsAPM::Inst::NetHttp')
+            # is the x-trace correct, it does not change during this call
+            assert request['x-trace']
+            assert res['x-trace']
+            refute AppOpticsAPM::XTrace.sampled?(res.to_hash['x-trace'])
+            assert_equal xt, request['x-trace']
+            assert_equal xt, res['x-trace']
           end
+
         end
       end
       refute AppOpticsAPM::Context.isValid
     end
 
     def test_no_xtrace
-      Net::HTTP.any_instance.expects(:request_without_appoptics).with do |req, _, _|
-        req.to_hash['x-trace'].nil?
-      end.returns(MockResponse.new)
-
       uri = URI('http://127.0.0.1:8101/?q=1')
       Net::HTTP.start(uri.host, uri.port) do |http|
         request = Net::HTTP::Get.new(uri)
-        http.request(request) # Net::HTTPResponse object
+        res = http.request(request) # Net::HTTPResponse object
+
+        assert res['ancestors'].include?('AppOpticsAPM::Inst::NetHttp')
+        refute request['x-trace']
+        # the result may have an x-trace from the outbound call and that is ok
       end
     end
 
     def test_blacklisted
-      Net::HTTP.any_instance.expects(:request_without_appoptics).with do |req, _, _|
-        req.to_hash['x-trace'].nil?
-      end.returns(MockResponse.new)
-
       AppOpticsAPM.config_lock.synchronize do
         AppOpticsAPM::Config.blacklist << '127.0.0.1'
         AppOpticsAPM::API.start_trace('Net::HTTP_tests') do
           uri = URI('http://127.0.0.1:8101/?q=1')
           Net::HTTP.start(uri.host, uri.port) do |http|
             request = Net::HTTP::Get.new(uri)
-            http.request(request) # Net::HTTPResponse object
+            res = http.request(request) # Net::HTTPResponse object
+            # did we instrument?
+            assert res['ancestors'].include?('AppOpticsAPM::Inst::NetHttp')
+            # is the x-trace correct, it does not change during this call
+            refute request['x-trace']
+            # the result should either not have an x-trace or
+            refute res['x-trace']
           end
         end
       end
@@ -93,10 +116,6 @@ unless defined?(JRUBY_VERSION)
     end
 
     def test_not_sampling_blacklisted
-      Net::HTTP.any_instance.expects(:request_without_appoptics).with do |req, _, _|
-        req.to_hash['x-trace'].nil?
-      end.returns(MockResponse.new)
-
       AppOpticsAPM.config_lock.synchronize do
         AppOpticsAPM::Config[:sample_rate] = 0
         AppOpticsAPM::Config.blacklist << '127.0.0.1'
@@ -104,26 +123,33 @@ unless defined?(JRUBY_VERSION)
           uri = URI('http://127.0.0.1:8101/?q=1')
           Net::HTTP.start(uri.host, uri.port) do |http|
             request = Net::HTTP::Get.new(uri)
-            http.request(request) # Net::HTTPResponse object
+            res = http.request(request) # Net::HTTPResponse object
+            # did we instrument?
+            assert res['ancestors'].include?('AppOpticsAPM::Inst::NetHttp')
+            # is the x-trace correct, it does not change during this call
+            refute request['x-trace']
+            # the result should either not have an x-trace or
+            refute res['x-trace']
           end
         end
       end
       refute AppOpticsAPM::Context.isValid
     end
 
-    # ========== make sure headers are preserved =============================
+    # ========== make sure request headers are preserved =============================
     def test_preserves_custom_headers
-      Net::HTTP.any_instance.expects(:request_without_appoptics).with do |req, _|
-        assert req.to_hash['custom'], "Custom header missing"
-        assert_match /specialvalue/, req.to_hash['custom'].first
-      end.returns(MockResponse.new)
-
       AppOpticsAPM::API.start_trace('Net::HTTP_tests') do
         uri = URI('http://127.0.0.1:8101/?q=1')
         Net::HTTP.start(uri.host, uri.port) do |http|
           request = Net::HTTP::Get.new(uri)
           request['Custom'] = 'specialvalue'
-          http.request(request) # Net::HTTPResponse object
+          res = http.request(request) # Net::HTTPResponse object
+          # did we instrument?
+          assert res['ancestors'].include?('AppOpticsAPM::Inst::NetHttp')
+          # is the x-trace correct, it does not change during this call
+          assert request['x-trace']
+          assert res['x-trace']
+          assert_equal 'specialvalue', request['Custom']
         end
       end
       refute AppOpticsAPM::Context.isValid
