03866197f50268b5b0703fa8529ab134a41cc778
Maia Engeli <maia@prettycoders.com>
2021-03-03 12:15:09 -0800

    report sha when building gem and clean up
    AO-18648

--- build_gem.sh
+++ build_gem.sh
@@ -3,14 +3,18 @@
 
 # we currently only build for MRI, no JRuby
 echo -e "\n=== building for MRI ===\n"
-rm -f Gemfile.lock
-bundle install --without development --without test
-bundle exec rake distclean
-bundle exec rake fetch_ext_deps
+# rm -f Gemfile.lock
+# bundle install --without development --without test
+# bundle exec rake distclean
+# bundle exec rake fetch_ext_deps
 gem build appoptics_apm.gemspec
 mv appoptics_apm*.gem builds
 
-echo -e "\n=== built gems ===\n"
-ls -lart builds/appoptics_apm*.gem
+echo -e "\n=== last 5 built gems ===\n"
+ls -lart builds/appoptics_apm*.gem | tail -n 5
+
+echo -e "\n=== SHA256 ===\n"
+gem=`ls -dt1 builds/appoptics_apm-[^pre]*.gem | head -1`
+echo `shasum -a256 $gem`
 
 echo -e "\n=== publish to rubygems via: gem push <gem> ===\n"
