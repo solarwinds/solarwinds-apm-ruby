849846f237d7674526a701cfc4d79c7f6a138861
Maia Engeli <maia@prettycoders.com>
2021-03-08 09:08:37 -0800

    apply mysql port from env var, add dbtype to matrix
    AO-18649

--- lib/appoptics_apm/test.rb
+++ lib/appoptics_apm/test.rb
@@ -73,7 +73,11 @@ module AppOpticsAPM
         if ENV.key?('TRAVIS_MYSQL_PASS')
           ENV['DATABASE_URL'] = "mysql://root:#{ENV['TRAVIS_MYSQL_PASS']}@127.0.0.1:3306/travis_ci_test"
         elsif ENV.key?('DOCKER_MYSQL_PASS')
-          ENV['DATABASE_URL'] = "mysql://root:#{ENV['DOCKER_MYSQL_PASS']}@#{ENV['MYSQL_HOST']}:3306/travis_ci_test"
+          if ENV.key?('MYSQL_PORT')
+            ENV['DATABASE_URL'] = "mysql://root:#{ENV['DOCKER_MYSQL_PASS']}@#{ENV['MYSQL_HOST']}:#{ENV['MYSQL_PORT']}/travis_ci_test"
+          else
+            ENV['DATABASE_URL'] = "mysql://root:#{ENV['DOCKER_MYSQL_PASS']}@#{ENV['MYSQL_HOST']}:3306/travis_ci_test"
+          end
         else
           ENV['DATABASE_URL'] = 'mysql://root@127.0.0.1:3306/travis_ci_test'
         end
