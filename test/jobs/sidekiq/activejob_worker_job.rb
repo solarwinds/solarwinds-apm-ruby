8e0eeb5ef5ed7063eaa7e0552bb0cc56dcc33da4
Maia Engeli <maia@prettycoders.com>
2021-03-08 15:01:28 -0800

    more redis config adpatations
    AO-18649

--- test/jobs/sidekiq/activejob_worker_job.rb
+++ test/jobs/sidekiq/activejob_worker_job.rb
@@ -1,10 +1,7 @@
 require "active_job/railtie"
 
 Sidekiq.configure_server do |config|
-  config.redis ||= {}
-  # if ENV.key?('REDIS_PASSWORD')
-    config.redis << { :password => ENV['REDIS_PASSWORD'] | 'secret_pass'}
-  # end
+    config.redis = { :password => ENV['REDIS_PASSWORD'] | 'secret_pass'}
   if ENV.key?('REDIS_HOST')
     config.redis << { :url => "redis://#{ENV['REDIS_HOST']}:6379" }
   end
--- test/servers/sidekiq_initializer.rb
+++ test/servers/sidekiq_initializer.rb
@@ -7,10 +7,7 @@
 ENV['BUNDLE_GEMFILE'] = Dir.pwd + "/gemfiles/libraries.gemfile"
 
 Sidekiq.configure_server do |config|
-  # config.redis ||= {}
-  # if ENV.key?('REDIS_PASSWORD')
   config.redis = { :password => ENV['REDIS_PASSWORD'] || 'secret_pass' }
-  # end
   if ENV.key?('REDIS_HOST')
     config.redis << { :url => "redis://#{ENV['REDIS_HOST']}:6379" }
   end
