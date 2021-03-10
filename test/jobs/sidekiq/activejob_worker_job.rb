bda62c44f9b6517313500d051a5bf1541766335c
Maia Engeli <maia@prettycoders.com>
2021-03-09 18:19:42 -0800

    fix redis config
    AO-18649

--- test/jobs/sidekiq/activejob_worker_job.rb
+++ test/jobs/sidekiq/activejob_worker_job.rb
@@ -1,7 +1,7 @@
 require "active_job/railtie"
 
 Sidekiq.configure_server do |config|
-    config.redis = { :password => ENV['REDIS_PASSWORD'] | 'secret_pass'}
+    config.redis = { :password => ENV['REDIS_PASSWORD'] || 'secret_pass' }
   if ENV.key?('REDIS_HOST')
     config.redis << { :url => "redis://#{ENV['REDIS_HOST']}:6379" }
   end
