2a0d92ab269fc4fe5dcd8c79298dda2c4ae3153f
Maia Engeli <maia@prettycoders.com>
2021-03-09 18:59:04 -0800

    more tweaks to get test suites running with ruby 3
    AO-18649

--- test/servers/rails5x_8140.rb
+++ test/servers/rails5x_8140.rb
@@ -5,6 +5,7 @@
 #  This is a Rails stack that launches on a background
 #  thread and listens on port 8140.
 
+require "rails/all"
 require "active_record"
 require "action_controller" # require more rails if needed
 require 'rack/handler/puma'
