913e894597f5eeb97bf4313684ecdbf8ec3ee124
Maia Engeli <maia@prettycoders.com>
2021-03-19 13:12:49 -0700

    reset the version and set the trigger for releasing to workflow_dispatch
    AO-18649

--- lib/appoptics_apm/version.rb
+++ lib/appoptics_apm/version.rb
@@ -6,10 +6,10 @@ module AppOpticsAPM
   # The current version of the gem.  Used mainly by
   # appoptics_apm.gemspec during gem build process
   module Version
-    MAJOR = 0  # breaking,
-    MINOR = 1 # feature,
-    PATCH = 0  # fix => BFF
-    PRE   = "ghtest9"
+    MAJOR = 4  # breaking,
+    MINOR = 12 # feature,
+    PATCH = 2  # fix => BFF
+    PRE   = nil
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
