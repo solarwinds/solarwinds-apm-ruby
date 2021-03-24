223b09293d2133f1baa1ecd0b81e8e54461b84a0
Maia Engeli <maia@prettycoders.com>
2021-03-24 16:17:44 -0700

    stop trying to republish on package cloud
    AO-18649

--- lib/appoptics_apm/version.rb
+++ lib/appoptics_apm/version.rb
@@ -9,7 +9,7 @@ module AppOpticsAPM
     MAJOR = 0  # breaking,
     MINOR = 1 # feature,
     PATCH = 0  # fix => BFF
-    PRE   = "ghtest13"
+    PRE   = "ghtest14"
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
