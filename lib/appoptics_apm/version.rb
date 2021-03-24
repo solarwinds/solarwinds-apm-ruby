440db816a6daec899b8a3952512355dd13cf27a9
Maia Engeli <maia@prettycoders.com>
2021-03-24 10:20:11 -0700

    try with a new gem version, doesn't seem to replace the old one on packagecloud
    AO-18649

--- lib/appoptics_apm/version.rb
+++ lib/appoptics_apm/version.rb
@@ -9,7 +9,7 @@ module AppOpticsAPM
     MAJOR = 0  # breaking,
     MINOR = 1 # feature,
     PATCH = 0  # fix => BFF
-    PRE   = "ghtest11"
+    PRE   = "ghtest12"
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
