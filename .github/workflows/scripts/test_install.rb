a30c2c8dad3561e5b614db7e0e34f5b33e1530bd
Maia Engeli <maia@prettycoders.com>
2021-08-20 12:39:40 -0700

    revert commit accidentally committed to master
    This reverts commit f550d1e1

--- .github/workflows/scripts/test_install.rb
+++ .github/workflows/scripts/test_install.rb
@@ -5,19 +5,8 @@
 # the collector
 # requires env vars:
 # - APPOPTICS_SERVICE_KEY
-# - APPOPTICS_COLLECTOR (optional if the key is for production)
+# - APPOPTICS_COLLECTOR (optional if the kye is for production)
 
 require 'appoptics_apm'
 AppOpticsAPM.support_report
 exit 1 unless AppOpticsAPM.reporter
-
-AppOpticsAPM::Config[:profiling] = :enabled
-
-AppOpticsAPM::SDK.start_trace("install_test_profiling") do
-  AppOpticsAPM::Profiling.run do
-    10.times do
-      [9, 6, 12, 2, 7, 1, 9, 3, 4, 14, 5, 8].sort
-      sleep 0.2
-    end
-  end
-end
