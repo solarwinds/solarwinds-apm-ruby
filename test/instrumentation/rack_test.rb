79dc6ab1fdc3a6d9d3a07ff613b412b42c374fa2
Maia Engeli <maia@prettycoders.com>
2021-09-16 12:59:13 +0200

    comment out more tests to get to bottom of flakyness

--- test/instrumentation/rack_test.rb
+++ test/instrumentation/rack_test.rb
@@ -320,40 +320,40 @@ describe "Rack: " do
     #   end
     # end
 
-    it 'should sample but not do metrics' do
-      AppOpticsAPM::API.start_trace(:other) do
-        check_w_context_01_110
-      end
-    end
-
-    it 'should log an error' do
-      AppOpticsAPM::API.expects(:log_start)
-      AppOpticsAPM::API.expects(:log_exception)
-      AppOpticsAPM::API.expects(:log_exit)
-      AppOpticsAPM::Span.expects(:createHttpSpan).never
-
-      AppOpticsAPM::Context.fromString('2B7435A9FE510AE4533414D425DADF4E180D2B4E3649E60702469DB05F01')
-
-      assert_raises StandardError do
-        def @app.call(_); raise StandardError; end
-        @rack.call({})
-      end
-    end
-
-    it 'returns a non sampling header when there is a non-sampling context' do
-      AppOpticsAPM::Context.fromString('2B7435A9FE510AE4533414D425DADF4E180D2B4E3649E60702469DB05F00')
-
-      check_w_context_00_000
-    end
-
-    it 'does not trace or send metrics when there is a non-sampling context' do
-      AppOpticsAPM::Context.fromString('2B7435A9FE510AE4533414D425DADF4E180D2B4E3649E60702469DB05F00')
-
-      AppOpticsAPM::API.expects(:log_start).never
-      AppOpticsAPM::Span.expects(:createHttpSpan).never
-
-      @rack.call({})
-    end
+    # it 'should sample but not do metrics' do
+    #   AppOpticsAPM::API.start_trace(:other) do
+    #     check_w_context_01_110
+    #   end
+    # end
+    #
+    # it 'should log an error' do
+    #   AppOpticsAPM::API.expects(:log_start)
+    #   AppOpticsAPM::API.expects(:log_exception)
+    #   AppOpticsAPM::API.expects(:log_exit)
+    #   AppOpticsAPM::Span.expects(:createHttpSpan).never
+    #
+    #   AppOpticsAPM::Context.fromString('2B7435A9FE510AE4533414D425DADF4E180D2B4E3649E60702469DB05F01')
+    #
+    #   assert_raises StandardError do
+    #     def @app.call(_); raise StandardError; end
+    #     @rack.call({})
+    #   end
+    # end
+    #
+    # it 'returns a non sampling header when there is a non-sampling context' do
+    #   AppOpticsAPM::Context.fromString('2B7435A9FE510AE4533414D425DADF4E180D2B4E3649E60702469DB05F00')
+    #
+    #   check_w_context_00_000
+    # end
+    #
+    # it 'does not trace or send metrics when there is a non-sampling context' do
+    #   AppOpticsAPM::Context.fromString('2B7435A9FE510AE4533414D425DADF4E180D2B4E3649E60702469DB05F00')
+    #
+    #   AppOpticsAPM::API.expects(:log_start).never
+    #   AppOpticsAPM::Span.expects(:createHttpSpan).never
+    #
+    #   @rack.call({})
+    # end
   end
 
   describe 'F - asset?' do
