e259a18dc9411fcd47c60f215c72513924a1ef92
Maia Engeli <maia@prettycoders.com>
2021-09-21 13:25:12 +0200

    require xtrace.rb when in noop

--- lib/appoptics_apm.rb
+++ lib/appoptics_apm.rb
@@ -61,6 +61,7 @@ begin
     require 'appoptics_apm/noop/context'
     require 'appoptics_apm/noop/metadata'
     require 'appoptics_apm/noop/profiling'
+    require 'appoptics_apm/support/xtrace'
   end
 
   # Load Ruby module last.  If there is no framework detected,
