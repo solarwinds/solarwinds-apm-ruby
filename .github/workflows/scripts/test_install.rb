76cf5b5c32ce00bed4eecd9c59b1b83effb5aeab
Maia Engeli <maia.engeli@solarwinds.com>
2022-04-29 10:38:09 -0700

    fix env vars for post release test script plus tweaks

--- .github/workflows/scripts/test_install.rb
+++ .github/workflows/scripts/test_install.rb
@@ -8,16 +8,23 @@
 # - SW_APM_COLLECTOR (optional if the key is for production)
 
 require 'solarwinds_apm'
-SolarWindsAPM.support_report
-exit 1 unless SolarWindsAPM.reporter
 
-SolarWindsAPM::Config[:profiling] = :enabled
+unless SolarWindsAPM::SDK.solarwinds_ready?(10_000)
+  puts "aborting!!! Agent not ready after 10 seconds"
+  exit false
+end
+
+SolarWindsAPM.support_report
+
+# no profiling yet for NH, but it shouldn't choke on Profiling.run
+SolarWindsAPM::Config[:profiling] = :disabled
 
 SolarWindsAPM::SDK.start_trace("install_test_profiling") do
   SolarWindsAPM::Profiling.run do
     10.times do
       [9, 6, 12, 2, 7, 1, 9, 3, 4, 14, 5, 8].sort
-      sleep 0.2
+      # sleep 0.2 # maybe turn back on when profiling
     end
+    puts "Looking good so far :)"  # this will show up in the log of github actions
   end
 end
--- lib/solarwinds_apm/version.rb
+++ lib/solarwinds_apm/version.rb
@@ -6,10 +6,10 @@ module SolarWindsAPM
   # The current version of the gem. Used mainly by
   # solarwinds_apm.gemspec during gem build process
   module Version
-    MAJOR  = 5 # breaking,
+    MAJOR  = 0 # breaking,
     MINOR  = 0 # feature,
     PATCH  = 0 # fix => BFF
-    PRE    = nil # for pre-releases into packagecloud,
+    PRE    = 0 # for pre-releases into packagecloud,
                # set to nil for production releases into rubygems
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
