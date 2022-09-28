309cbee3b48d65562d3cabc992751c4ef8dfc1a3
xuan-cao-swi <xuan.cao@solarwinds.com>
2022-09-28 13:27:43 -0400

    [skip actions] test for prod

--- lib/solarwinds_apm/version.rb
+++ lib/solarwinds_apm/version.rb
@@ -9,7 +9,7 @@ module SolarWindsAPM
     MAJOR  = 5 # breaking,
     MINOR  = 0 # feature,
     PATCH  = 0 # fix => BFF
-    PRE    = 6 # for pre-releases into packagecloud,
+    PRE    = 7 # for pre-releases into packagecloud,
                # set to nil for production releases into rubygems
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
