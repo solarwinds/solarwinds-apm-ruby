ed6f871f870801166e333381c585bb6e36303f03
xuan-cao-swi <xuan.cao@solarwinds.com>
2022-10-07 15:35:18 -0400

    NH-20932: [skip actions] minor fix on collector env as nil

--- lib/solarwinds_apm/version.rb
+++ lib/solarwinds_apm/version.rb
@@ -8,7 +8,7 @@ module SolarWindsAPM
   module Version
     MAJOR  = 5 # breaking,
     MINOR  = 1 # feature,
-    PATCH  = 0 # fix => BFF
+    PATCH  = 1 # fix => BFF
     PRE    = nil # for pre-releases into packagecloud, set to nil for production releases into rubygems
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
