1e6763218c5798fb7402c50c2897072be518d9fe
xuan-cao-swi <xuan.cao@solarwinds.com>
2023-01-10 14:57:57 -0500

    NH-27837: revision

--- .github/workflows/scripts/_helper_run_install_tests.sh
+++ .github/workflows/scripts/_helper_run_install_tests.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env sh
 
-# Helper script to set up dependencies for the install tests, then runs the tests.
-# Accounts for:
-#   * Alpine not having bash nor agent install deps
-#   * Amazon Linux not having agent install deps
-#   * CentOS 8 being at end-of-life and needing a mirror re-point
-#   * Ubuntu not having agent install deps
-#
-# Note: centos8 can only install Python 3.8, 3.9
-
 # stop on error
 set -e
 
@@ -18,14 +9,12 @@ echo "Install solarwinds_apm version: $SOLARWINDS_APM_VERSION"
 {
     if grep Alpine /etc/os-release; then
         # test deps
-        apk add bash
-        apk --update add ruby ruby-dev build-base nodejs tzdata postgresql-dev postgresql-client libxslt-dev libxml2-dev imagemagick
-    
+        apk add bash && apk --update add ruby ruby-dev build-base
+
     elif grep Ubuntu /etc/os-release; then
         ubuntu_version=$(grep VERSION_ID /etc/os-release | sed 's/VERSION_ID="//' | sed 's/"//')
         if [ "$ubuntu_version" = "18.04" ] || [ "$ubuntu_version" = "20.04" ]; then
-            apt-get update -y
-            apt install -y ruby ruby-dev libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
+            apt update && apt install -y ruby ruby-dev build-essential
         else
             echo "ERROR: Testing on Ubuntu <18.04 not supported."
             exit 1
@@ -33,13 +22,11 @@ echo "Install solarwinds_apm version: $SOLARWINDS_APM_VERSION"
     elif  grep Debian /etc/os-release; then
         debain_version=$(grep VERSION_ID /etc/os-release | sed 's/VERSION_ID="//' | sed 's/"//')
         if [ "$debain_version" = "11" ] || [ "$debain_version" = "12" ]; then
-            apt-get update -y
-            apt install -y ruby ruby-dev libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
+            apt update && apt install -y ruby ruby-dev build-essential
         else
             echo "ERROR: Testing on Debian < 11 not supported."
             exit 1
         fi
-
     fi
 } >/dev/null
 
@@ -51,11 +38,10 @@ elif [ "$MODE" = "packagecloud" ]; then
     gem install solarwinds_apm -v "$SOLARWINDS_APM_VERSION" --source https://packagecloud.io/solarwinds/solarwinds-apm-ruby/
 fi
 
-if [ "$ARITCH" = "AMD" ]; then
+if [ "$ARCHITECTURE" = "AMD" ]; then
     echo "AMD"
     ruby ./scripts/test_install.rb
-elif [ "$ARITCH" = "ARM" ]; then
+elif [ "$ARCHITECTURE" = "ARM" ]; then
     echo "ARM"
     ruby ./.github/workflows/scripts/test_install.rb
 fi
-
