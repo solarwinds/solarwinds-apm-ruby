5ecac5baef3c72a1f8ff34faf8e3e5f431eb84b1
xuan-cao-swi <xuan.cao@solarwinds.com>
2023-01-12 16:10:54 -0500

    [skip actions] NH-27837: verify install with internal images

--- .github/workflows/scripts/_helper_run_install_tests.sh
+++ .github/workflows/scripts/_helper_run_install_tests.sh
@@ -5,32 +5,30 @@ set -e
 
 echo "Install solarwinds_apm version: $SOLARWINDS_APM_VERSION"
 
-# setup dependencies quietly
-# {
-#     if grep Alpine /etc/os-release; then
-#         # test deps
-#         apk add bash && apk --update add ruby ruby-dev build-base
+{
+    if grep rhel /etc/os-release; then
+        # for special ubi now. next liboboe version we will build our own rhel image
+        yum update -y && yum install -y ruby-devel git-core gcc gcc-c++ make perl zlib-devel bzip2 openssl-devel
 
-#     elif grep Ubuntu /etc/os-release; then
-#         ubuntu_version=$(grep VERSION_ID /etc/os-release | sed 's/VERSION_ID="//' | sed 's/"//')
-#         if [ "$ubuntu_version" = "18.04" ] || [ "$ubuntu_version" = "20.04" ]; then
-#             apt update && apt install -y ruby ruby-dev build-essential
-#         else
-#             echo "ERROR: Testing on Ubuntu <18.04 not supported."
-#             exit 1
-#         fi
-#     elif  grep Debian /etc/os-release; then
-#         debain_version=$(grep VERSION_ID /etc/os-release | sed 's/VERSION_ID="//' | sed 's/"//')
-#         if [ "$debain_version" = "11" ] || [ "$debain_version" = "12" ]; then
-#             apt update && apt install -y ruby ruby-dev build-essential
-#         else
-#             echo "ERROR: Testing on Debian < 11 not supported."
-#             exit 1
-#         fi
-#     fi
-# } >/dev/null
+        git clone https://github.com/rbenv/rbenv.git ~/.rbenv \
+             && git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build \
+             && git clone https://github.com/rbenv/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems \
+             && echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile \
+             && echo 'eval "$(rbenv init -)"' >> ~/.profile \
+             && echo 'eval "$(rbenv init -)"' >> ~/.bashrc \
+             && echo 'bundler' > ~/.rbenv/default-gems
 
-rbenv versions
+        echo 'alias be="bundle exec"' >> ~/.bashrc
+        echo 'alias be="bundle exec"' >> ~/.profile
+
+        . ~/.profile \
+            && cd /root/.rbenv/plugins/ruby-build && git pull && cd - \
+            && rbenv install $RUBY_VERSION
+
+        rbenv global $RUBY_VERSION
+    fi
+} >/dev/null
+
 rbenv local $RUBY_VERSION
 
 if [ "$MODE" = "RubyGem" ]; then
@@ -42,11 +40,3 @@ elif [ "$MODE" = "packagecloud" ]; then
 fi
 
 ruby ./home/.github/workflows/scripts/test_install.rb
-
-# if [ "$ARCHITECTURE" = "AMD" ]; then
-#     echo "AMD"
-#     ruby ./scripts/test_install.rb
-# elif [ "$ARCHITECTURE" = "ARM" ]; then
-#     echo "ARM"
-#     ruby ./.github/workflows/scripts/test_install.rb
-# fi
